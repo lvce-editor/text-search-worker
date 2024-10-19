5ab14a986bd6d1de7a34286a7995e5639d00d6b5
Le Vivilet <levivlet@pm.me>
2024-10-19 20:52:28 +0200

    feature: add idb for file handles

---
+++ src/parts/IndexedDbState/IndexedDbState.ts
@@ -0,0 +1,6 @@
+export const state = {
+  databases: Object.create(null),
+  eventId: 0,
+  dbVersion: 1,
+  cachedDb: undefined as any,
+}
--- src/parts/PersistentFileHandle/PersistentFileHandle.ts
+++ src/parts/PersistentFileHandle/PersistentFileHandle.ts
@@ -1,11 +1,11 @@
-import * as Command from '../Command/Command.ts'
+import * as IndexedDb from '../IndexedDb/IndexedDb.ts'
 import { VError } from '../VError/VError.ts'
 
 export const getHandle = async (uri: string) => {
   try {
     // TODO retrieve handle from state or from indexeddb
     // TODO if not found, throw error
-    const handle = await Command.execute('IndexedDb.getHandle', uri)
+    const handle = await IndexedDb.getHandle(uri)
     return handle
   } catch (error) {
     throw new VError(error, 'Failed to get handle')
