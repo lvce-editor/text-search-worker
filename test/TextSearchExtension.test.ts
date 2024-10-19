c7a6cf845ead8f57cf10249b6504ed5f5ad28ee3
Le Vivilet <levivlet@pm.me>
2024-10-19 22:29:03 +0200

    feature: add text search tests

---
+++ test/TextSearchExtension.test.ts
@@ -0,0 +1,51 @@
+import { beforeEach, expect, jest, test } from '@jest/globals'
+
+beforeEach(() => {
+  jest.resetAllMocks()
+})
+
+jest.unstable_mockModule('../src/parts/ExtensionHost/ExtensionHostTextSearch.js', () => {
+  return {
+    executeTextSearchProvider: jest.fn(() => {
+      throw new Error('not implemented')
+    }),
+  }
+})
+
+const TextSearchExtension = await import('../src/parts/TextSearch/TextSearchExtension.ts')
+
+test.skip('textSearch - extension search - error', async () => {
+  // @ts-ignore
+  ExtensionHostTextSearch.executeTextSearchProvider.mockImplementation(() => {
+    throw new TypeError('x is not a function')
+  })
+  await expect(TextSearchExtension.textSearch('xyz', 'xyz://', 'abc')).rejects.toThrow(new TypeError('x is not a function'))
+})
+
+test.skip('textSearch - extension search', async () => {
+  // @ts-ignore
+  ExtensionHostTextSearch.executeTextSearchProvider.mockImplementation(() => {
+    return [
+      [
+        './index.txt',
+        {
+          absoluteOffset: 208,
+          preview: '    <title>Document</title>\n',
+        },
+      ],
+    ]
+  })
+  expect(await TextSearchExtension.textSearch('xyz', 'xyz://', 'abc')).toEqual([
+    [
+      './index.txt',
+      {
+        absoluteOffset: 208,
+        preview: '    <title>Document</title>\n',
+      },
+    ],
+  ])
+  // @ts-ignore
+  expect(ExtensionHostTextSearch.executeTextSearchProvider).toHaveBeenCalledTimes(1)
+  // @ts-ignore
+  expect(ExtensionHostTextSearch.executeTextSearchProvider).toHaveBeenCalledWith('xyz', 'abc')
+})
---
+++ test/TextSearchWeb.test.ts
@@ -0,0 +1,24 @@
+import { beforeEach, expect, test } from '@jest/globals'
+import * as FileSystemWeb from '../src/parts/FileSystem/FileSystemWeb.js'
+import * as TextSearchWeb from '../src/parts/TextSearch/TextSearchWeb.ts'
+
+beforeEach(() => {
+  FileSystemWeb.state.files = Object.create(null)
+})
+
+test('textSearch', async () => {
+  FileSystemWeb.state.files = {
+    './index.txt': 'value',
+  }
+  expect(TextSearchWeb.textSearch('', '', 'val')).toEqual([
+    [
+      './index.txt',
+      [
+        {
+          absoluteOffset: 0,
+          preview: 'value',
+        },
+      ],
+    ],
+  ])
+})
