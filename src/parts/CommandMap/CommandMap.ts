f8108937c96554834cafecf13b9b0c56a074cfc4
Le Vivilet <levivlet@pm.me>
2024-10-22 19:24:11 +0200

    feature: add search focus functions to text search worker to keep text search state in text search worker

--- src/parts/CommandMap/CommandMap.ts
+++ src/parts/CommandMap/CommandMap.ts
@@ -1,5 +1,17 @@
+import * as SearchFocus from '../SearchFocus/SearchFocus.ts'
 import * as TextSearch from '../TextSearch/TextSearch.ts'
 
 export const commandMap = {
+  'TextSearch.focusMatchCase': SearchFocus.focusMatchCase,
+  'TextSearch.focusMatchCasePrevious': SearchFocus.focusMatchCasePrevious,
+  'TextSearch.focusMatchWholeWord': SearchFocus.focusMatchWholeWord,
+  'TextSearch.focusPreserveCase': SearchFocus.focusPreserveCase,
+  'TextSearch.focusRegex': SearchFocus.focusRegex,
+  'TextSearch.focusRegexNext': SearchFocus.focusRegexNext,
+  'TextSearch.focusReplaceAll': SearchFocus.focusReplaceAll,
+  'TextSearch.focusReplaceValue': SearchFocus.focusReplaceValue,
+  'TextSearch.focusReplaceValueNext': SearchFocus.focusReplaceValueNext,
+  'TextSearch.focusSearchValue': SearchFocus.focusSearchValue,
+  'TextSearch.focusSearchValueNext': SearchFocus.focusSearchValueNext,
   'TextSearch.textSearch': TextSearch.textSearch,
 }
