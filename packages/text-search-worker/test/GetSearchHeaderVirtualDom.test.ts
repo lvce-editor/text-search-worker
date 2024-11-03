1a515bdab3e10f3b5dc4084e222c6acc91505755
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-03 15:38:41 +0100

    feature: add getSearchHeaderVirtualDom test (#26)

---
+++ packages/text-search-worker/test/GetSearchHeaderVirtualDom.test.ts
@@ -0,0 +1,113 @@
+import { expect, test } from '@jest/globals'
+import * as GetSearchHeaderVirtualDom from '../src/parts/GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.ts'
+
+test('getSearchHeaderVirtualDom', () => {
+  const replaceExpanded = false
+  const matchCase = false
+  const matchWholeWord = false
+  const useRegularExpression = false
+  const detailsExpanded = false
+  expect(
+    GetSearchHeaderVirtualDom.getSearchHeaderVirtualDom(replaceExpanded, matchCase, matchWholeWord, useRegularExpression, detailsExpanded),
+  ).toEqual([
+    {
+      childCount: 2,
+      className: 'SearchHeader',
+      onClick: 'handleHeaderClick',
+      onFocusIn: 'handleHeaderFocusIn',
+      role: 'none',
+      type: 4,
+    },
+    {
+      childCount: 2,
+      className: 'SearchHeaderTop',
+      role: 'none',
+      type: 4,
+    },
+    {
+      ariaExpanded: false,
+      ariaLabel: 'Toggle Replace',
+      childCount: 1,
+      className: 'IconButton SearchToggleButton ',
+      'data-command': 'toggleReplace',
+      title: 'Toggle Replace',
+      type: 1,
+    },
+    {
+      childCount: 0,
+      className: 'MaskIcon MaskIconChevronRight',
+      type: 4,
+    },
+    {
+      childCount: 1,
+      className: 'SearchHeaderTopRight',
+      role: 'none',
+      type: 4,
+    },
+    {
+      childCount: 2,
+      className: 'SearchField',
+      role: 'none',
+      type: 4,
+    },
+    {
+      autocapitalize: 'off',
+      autocorrect: 'off',
+      childCount: 0,
+      className: 'MultilineInputBox',
+      name: 'search-value',
+      onFocus: '',
+      onInput: 'handleInput',
+      placeholder: 'Search',
+      spellcheck: false,
+      type: 62,
+    },
+    {
+      childCount: 3,
+      className: 'SearchFieldButtons',
+      type: 4,
+    },
+    {
+      ariaChecked: false,
+      childCount: 1,
+      className: 'SearchFieldButton ',
+      role: 'checkbox',
+      tabIndex: 0,
+      title: 'Match Case',
+      type: 4,
+    },
+    {
+      childCount: 0,
+      className: 'MaskIcon MaskIconCaseSensitive',
+      type: 4,
+    },
+    {
+      ariaChecked: false,
+      childCount: 1,
+      className: 'SearchFieldButton ',
+      role: 'checkbox',
+      tabIndex: 0,
+      title: 'Match Whole Word',
+      type: 4,
+    },
+    {
+      childCount: 0,
+      className: 'MaskIcon MaskIconWholeWord',
+      type: 4,
+    },
+    {
+      ariaChecked: false,
+      childCount: 1,
+      className: 'SearchFieldButton ',
+      role: 'checkbox',
+      tabIndex: 0,
+      title: 'Use Regular Expression',
+      type: 4,
+    },
+    {
+      childCount: 0,
+      className: 'MaskIcon MaskIconRegex',
+      type: 4,
+    },
+  ])
+})
