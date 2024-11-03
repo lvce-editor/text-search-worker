7982765ba4a9f2555e90090d55d40219279514d5
Le Vivilet <levivlet@pm.me>
2024-11-03 14:24:00 +0100

    build

--- packages/build/src/bundleJs.js
+++ packages/build/src/bundleJs.js
@@ -9,7 +9,7 @@ import { root } from './root.js'
  * @type {import('rollup').RollupOptions}
  */
 const options = {
-  input: join(root, 'src/textSearchWorkerMain.ts'),
+  input: join(root, 'packages/text-search-worker/src/textSearchWorkerMain.ts'),
   preserveEntrySignatures: 'strict',
   treeshake: {
     propertyReadSideEffects: false,
