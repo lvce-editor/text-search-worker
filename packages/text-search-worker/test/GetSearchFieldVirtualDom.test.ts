eaa57caaf5e6d6d9fa63eb82bffc26d854ac6490
Le Vivilet <levivlet@pm.me>
2024-11-03 15:33:48 +0100

    feature: add search field test

---
+++ packages/text-search-worker/test/GetSearchFieldVirtualDom.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@jest/globals'
+import * as GetSearchFieldVirtualDom from '../src/parts/GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.ts'
+
+test('NotReadableError', () => {
+  const name = ''
+  const placeholder = ''
+  const onInput = 'handleInput'
+  const insideButtons: any = []
+  const outsideButtons: any = []
+  const onFocus = ''
+  expect(GetSearchFieldVirtualDom.getSearchFieldVirtualDom(name, placeholder, onInput, insideButtons, outsideButtons, onFocus)).toEqual([
+    {
+      childCount: 2,
+      className: 'SearchField',
+      role: 'none',
+      type: 4,
+    },
+    {
+      autocapitalize: 'off',
+      autocorrect: 'off',
+      childCount: 0,
+      className: 'MultilineInputBox',
+      name: '',
+      onFocus: '',
+      onInput: 'handleInput',
+      placeholder: '',
+      spellcheck: false,
+      type: 62,
+    },
+    {
+      childCount: 0,
+      className: 'SearchFieldButtons',
+      type: 4,
+    },
+  ])
+})
