e2182a3a7dc1fb3bd0871273365f6c3a9d0566b3
Le Vivilet <levivlet@pm.me>
2024-11-09 10:55:03 +0100

    feature: add more search strings tests

--- packages/text-search-worker/test/SearchStrings.test.ts
+++ packages/text-search-worker/test/SearchStrings.test.ts
@@ -19,3 +19,34 @@ test('manyResultsInManyFiles', () => {
   const fileResultCount = 2
   expect(SearchStrings.manyResultsInManyFiles(resultCount, fileResultCount)).toBe('2 results in 2 files')
 })
+
+test('confirmReplaceOneOccurrenceInOneFile', () => {
+  const replacement = 'b'
+  expect(SearchStrings.confirmReplaceOneOccurrenceInOneFile(replacement)).toBe("Replace 1 occurrence across 1 file with 'b'")
+})
+
+test('confirmReplaceManyOccurrencesInOneFile', () => {
+  const matchCount = 2
+  const replacement = 'b'
+  expect(SearchStrings.confirmReplaceManyOccurrencesInOneFile(matchCount, replacement)).toBe("Replace 2 occurrences across 1 file with 'b'")
+})
+
+test('confirmReplaceManyOccurrencesInOneFileNoValue', () => {
+  const matchCount = 2
+  expect(SearchStrings.confirmReplaceManyOccurrencesInOneFileNoValue(matchCount)).toBe('Replace 2 occurrences across 1 file')
+})
+
+test('confirmReplaceManyOccurrencesInManyFiles', () => {
+  const matchCount = 2
+  const fileCount = 2
+  const replacement = 'b'
+  expect(SearchStrings.confirmReplaceManyOccurrencesInManyFiles(matchCount, fileCount, replacement)).toBe(
+    "Replace 2 occurrences across 2 files with 'b'",
+  )
+})
+
+test('confirmReplaceManyOccurrencesInManyFilesNoValue', () => {
+  const matchCount = 2
+  const fileCount = 2
+  expect(SearchStrings.confirmReplaceManyOccurrencesInManyFilesNoValue(matchCount, fileCount)).toBe('Replace 2 occurrences across 2 files')
+})
