2c49cd1dacbdc24602e546358cc74a33643ccdc0
Le Vivilet <levivlet@pm.me>
2024-11-10 10:31:52 +0100

    feature: add static build

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,16 +1,23 @@
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.js'
+import { cp } from 'node:fs/promises'
 
 const sharedProcessPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
 
 const sharedProcessUrl = pathToFileURL(sharedProcessPath).toString()
 
 const sharedProcess = await import(sharedProcessUrl)
-console.log({ sharedProcess })
 
+process.env.PATH_PREFIX = '/text-search-worker'
 const { commitHash } = await sharedProcess.exportStatic({
   root,
+  extensionPath: '',
 })
 
-console.log({ commitHash })
+await cp(
+  join(root, '.tmp', 'dist', 'dist', 'textSearchWorkerMain.js'),
+  join(root, 'dist', commitHash, 'packages', 'text-search-worker', 'dist', 'textSearchWorkerMain.js'),
+)
+
+await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
