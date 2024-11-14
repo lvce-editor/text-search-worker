33ca5a4bde7e519f84e0dcb395bdd0bfacd2136b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-14 23:47:04 +0100

    feature: add getActionsVirtualDom test (#115)
    * feature: add getActionsVirtualDom test
    
    * test
    
    * lint
    
    * test

---
+++ packages/text-search-worker/test/GetActionsVirtualDom.test.ts
@@ -0,0 +1,66 @@
+import { test, expect } from '@jest/globals'
+import * as GetActionsVirtualDom from '../src/parts/GetActionsVirtualDom/GetActionsVirtualDom.ts'
+import type { Action } from '../src/parts/Action/Action.ts'
+
+test('getActionsVirtualDom - should return correct virtual dom structure', () => {
+  const actions: readonly Action[] = [
+    {
+      id: 'searchInFiles',
+      icon: 'Search in Files',
+      command: 'Search.searchInFiles',
+      type: 1,
+    },
+    {
+      id: 'findInFile',
+      icon: 'Find in File',
+      command: 'Search.findInFile',
+      type: 1,
+    },
+  ]
+
+  expect(GetActionsVirtualDom.getActionsVirtualDom(actions)).toEqual([
+    {
+      childCount: 2,
+      className: 'Actions',
+      role: 'toolbar',
+      type: 4,
+    },
+    {
+      childCount: 1,
+      className: 'IconButton',
+      'data-command': 'Search.searchInFiles',
+      title: 'searchInFiles',
+      type: 1,
+    },
+    {
+      childCount: 0,
+      className: 'MaskIcon MaskIconSearch in Files',
+      role: 'none',
+      type: 4,
+    },
+    {
+      childCount: 1,
+      className: 'IconButton',
+      'data-command': 'Search.findInFile',
+      title: 'findInFile',
+      type: 1,
+    },
+    {
+      childCount: 0,
+      className: 'MaskIcon MaskIconFind in File',
+      role: 'none',
+      type: 4,
+    },
+  ])
+})
+
+test('getActionsVirtualDom - should handle empty actions array', () => {
+  expect(GetActionsVirtualDom.getActionsVirtualDom([])).toEqual([
+    {
+      type: 4,
+      className: 'Actions',
+      role: 'toolbar',
+      childCount: 0,
+    },
+  ])
+})
