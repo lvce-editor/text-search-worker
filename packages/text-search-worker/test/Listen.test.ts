0069deb765527518da2a7cc87e7892d7ef67b551
Le Vivilet <levivlet@pm.me>
2024-11-14 23:23:21 +0100

    test

--- packages/text-search-worker/test/Listen.test.ts
+++ packages/text-search-worker/test/Listen.test.ts
@@ -1,7 +1,6 @@
-import { expect, test, jest } from '@jest/globals'
+import { expect, jest, test } from '@jest/globals'
 import * as CommandMap from '../src/parts/CommandMap/CommandMap.ts'
 import * as IpcChildType from '../src/parts/IpcChildType/IpcChildType.ts'
-import { listen } from '../src/parts/Listen/Listen.ts'
 
 jest.unstable_mockModule('../src/parts/Command/Command.ts', () => {
   return {
@@ -31,13 +30,14 @@ const Command = await import('../src/parts/Command/Command.ts')
 const IpcChild = await import('../src/parts/IpcChild/IpcChild.ts')
 const HandleIpc = await import('../src/parts/HandleIpc/HandleIpc.ts')
 const Rpc = await import('../src/parts/Rpc/Rpc.ts')
+const Listen = await import('../src/parts/Listen/Listen.ts')
 
 test('listen registers commands and sets up IPC', async () => {
   const mockIpc: any = {}
   // @ts-ignore
   IpcChild.listen.mockResolvedValue(mockIpc)
 
-  await listen()
+  await Listen.listen()
 
   expect(Command.register).toHaveBeenCalledWith(CommandMap.commandMap)
   expect(IpcChild.listen).toHaveBeenCalledWith({ method: IpcChildType.Auto() })
