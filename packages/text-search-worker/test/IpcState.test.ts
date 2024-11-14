5f9d557e48f851325ecfec0d7382ad1626507504
Le Vivilet <levivlet@pm.me>
2024-11-14 23:13:25 +0100

    feature: add ipcState test

---
+++ packages/text-search-worker/test/IpcState.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@jest/globals'
+import * as IpcState from '../src/parts/IpcState/IpcState.ts'
+
+test('initial state', () => {
+  expect(IpcState.get()).toBeUndefined()
+})
+
+test('set and get', () => {
+  const mockIpc = {
+    send: () => {},
+    receive: () => {},
+  }
+  IpcState.set(mockIpc)
+  expect(IpcState.get()).toBe(mockIpc)
+})
+
+test('set overwrites previous value', () => {
+  const mockIpc1 = {
+    id: 1,
+  }
+  const mockIpc2 = {
+    id: 2,
+  }
+  IpcState.set(mockIpc1)
+  IpcState.set(mockIpc2)
+  expect(IpcState.get()).toBe(mockIpc2)
+})
+
+test('set with undefined', () => {
+  const mockIpc = {
+    send: () => {},
+  }
+  IpcState.set(mockIpc)
+  IpcState.set(undefined)
+  expect(IpcState.get()).toBeUndefined()
+})
