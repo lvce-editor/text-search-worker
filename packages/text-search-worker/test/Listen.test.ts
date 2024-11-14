023771c40e32b66600fcd0d00daff50c7ab29381
Le Vivilet <levivlet@pm.me>
2024-11-14 23:02:34 +0100

    feature: add listen test

---
+++ packages/text-search-worker/test/Listen.test.ts
@@ -0,0 +1,46 @@
+import { expect, test, jest } from '@jest/globals'
+import * as CommandMap from '../src/parts/CommandMap/CommandMap.ts'
+import * as IpcChildType from '../src/parts/IpcChildType/IpcChildType.ts'
+import { listen } from '../src/parts/Listen/Listen.ts'
+
+jest.unstable_mockModule('../src/parts/Command/Command.ts', () => {
+  return {
+    register: jest.fn(),
+  }
+})
+
+jest.unstable_mockModule('../src/parts/IpcChild/IpcChild.ts', () => {
+  return {
+    listen: jest.fn(),
+  }
+})
+
+jest.unstable_mockModule('../src/parts/HandleIpc/HandleIpc.ts', () => {
+  return {
+    handleIpc: jest.fn(),
+  }
+})
+
+jest.unstable_mockModule('../src/parts/Rpc/Rpc.ts', () => {
+  return {
+    listen: jest.fn(),
+  }
+})
+
+const Command = await import('../src/parts/Command/Command.ts')
+const IpcChild = await import('../src/parts/IpcChild/IpcChild.ts')
+const HandleIpc = await import('../src/parts/HandleIpc/HandleIpc.ts')
+const Rpc = await import('../src/parts/Rpc/Rpc.ts')
+
+test('listen registers commands and sets up IPC', async () => {
+  const mockIpc: any = {}
+  // @ts-ignore
+  IpcChild.listen.mockResolvedValue(mockIpc)
+
+  await listen()
+
+  expect(Command.register).toHaveBeenCalledWith(CommandMap.commandMap)
+  expect(IpcChild.listen).toHaveBeenCalledWith({ method: IpcChildType.Auto() })
+  expect(HandleIpc.handleIpc).toHaveBeenCalledWith(mockIpc)
+  expect(Rpc.listen).toHaveBeenCalledWith(mockIpc)
+})
