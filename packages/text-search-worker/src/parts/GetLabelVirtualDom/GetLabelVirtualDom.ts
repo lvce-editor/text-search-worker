d9965da590b72099fc2a4a6add54232ba31e1649
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-14 21:08:31 +0100

    feature: update label dom code to reuse objects (#86)

--- packages/text-search-worker/src/parts/GetLabelVirtualDom/GetLabelVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetLabelVirtualDom/GetLabelVirtualDom.ts
@@ -1,6 +1,6 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
@@ -22,27 +22,31 @@ const highlighted: VirtualDomNode = {
   childCount: 1,
 }
 
+const label1: VirtualDomNode = {
+  type: VirtualDomElements.Div,
+  className: MergeClassNames.mergeClassNames(ClassNames.Label, ClassNames.Grow),
+  childCount: 1,
+}
+
+const label3: VirtualDomNode = {
+  ...label1,
+  childCount: 3,
+}
+
+const label4: VirtualDomNode = {
+  ...label1,
+  childCount: 4,
+}
+
 export const getLabelVirtualDom = (displayText: string, matchLength: number, matchStart: number, replacement: string): readonly VirtualDomNode[] => {
-  const dom: VirtualDomNode[] = []
-  const label = {
-    type: VirtualDomElements.Div,
-    className: MergeClassNames.mergeClassNames(ClassNames.Label, ClassNames.Grow),
-    childCount: 1,
-  }
-  dom.push(label)
   if (matchLength) {
     const before = displayText.slice(0, matchStart)
     const highlight = displayText.slice(matchStart, matchStart + matchLength)
     const after = displayText.slice(matchStart + matchLength)
     if (replacement) {
-      dom.push(text(before), deleted, text(highlight), inserted, text(replacement), text(after))
-      label.childCount = 4
-    } else {
-      label.childCount = 3
-      dom.push(text(before), highlighted, text(highlight), text(after))
+      return [label4, text(before), deleted, text(highlight), inserted, text(replacement), text(after)]
     }
-  } else {
-    dom.push(text(displayText))
+    return [label3, text(before), highlighted, text(highlight), text(after)]
   }
-  return dom
+  return [label1, text(displayText)]
 }
