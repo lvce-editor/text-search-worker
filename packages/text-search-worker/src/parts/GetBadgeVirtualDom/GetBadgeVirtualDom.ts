e84c08d3da9692d51f7483411de10e6424ecfc19
Le Vivilet <levivlet@pm.me>
2024-11-14 20:56:00 +0100

    feature: use classnames enum more often

--- packages/text-search-worker/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
@@ -1,4 +1,6 @@
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
@@ -6,7 +8,7 @@ export const getBadgeVirtualDom = (className: string, count: number): readonly V
   return [
     {
       type: VirtualDomElements.Div,
-      className: `Badge ${className}`,
+      className: MergeClassNames.mergeClassNames(ClassNames.Badge, className),
       childCount: 1,
     },
     text(`${count}`),
