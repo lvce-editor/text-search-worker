e6c80561ba7210697c2f85e7d097268e1c50a1a6
Le Vivilet <levivlet@pm.me>
2024-11-14 20:29:49 +0100

    feature: add eslint config

---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+import eslint from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    ignores: [
+      'dist',
+      '.tmp',
+      'build',
+      'coverage',
+      'scripts',
+      'rollup.config.js',
+      'eslint.config.js',
+      'packages/text-search-worker/src/textSearchWorkerMain.ts',
+    ],
+  },
+  {
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      'no-case-declarations': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+    },
+  },
+)
