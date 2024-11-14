7cc357ce7c093d7a286aa56e9a508a79e2be54d6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-14 23:44:43 +0100

    feature: add ipcChildModule test (#113)
    * feature: add ipcChildModule test
    
    * tsc

---
+++ packages/text-search-worker/test/ipcChildModule.test.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@jest/globals'
+import { getModule } from '../src/parts/IpcChildModule/IpcChildModule.ts'
+import * as IpcChildType from '../src/parts/IpcChildType/IpcChildType.ts'
+// @ts-ignore
+import { IpcChildWithModuleWorker, IpcChildWithModuleWorkerAndMessagePort } from '@lvce-editor/ipc/dist/browser.js'
+
+test('getModule - should return IpcChildWithModuleWorker for ModuleWorker type', () => {
+  expect(getModule(IpcChildType.ModuleWorker)).toEqual(IpcChildWithModuleWorker)
+})
+
+test('getModule - should return IpcChildWithModuleWorkerAndMessagePort for ModuleWorkerAndMessagePort type', () => {
+  expect(getModule(IpcChildType.ModuleWorkerAndMessagePort)).toEqual(IpcChildWithModuleWorkerAndMessagePort)
+})
+
+test('getModule - should throw error for unknown type', () => {
+  expect(() => getModule('unknown')).toThrow('unexpected ipc type')
+})
