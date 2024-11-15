b87616184848cc2cc8980c9e4e5b74b1fc4512da
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-15 20:22:06 +0100

    feature: add context menu test (#124)
    * feature: add context menu test
    
    * rename
    
    * test

---
+++ packages/text-search-worker/test/ViewletSearchHandleContextMenuKeyBoard.test.ts
@@ -0,0 +1,24 @@
+import { expect, jest, test } from '@jest/globals'
+import * as Create from '../src/parts/Create/Create.ts'
+import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
+
+const mockContextMenu = {
+  show: jest.fn(),
+}
+
+jest.unstable_mockModule('../src/parts/ContextMenu/ContextMenu.ts', () => mockContextMenu)
+
+test('handleContextMenuKeyboard', async () => {
+  const { handleContextMenuKeyboard } = await import('../src/parts/ViewletSearchHandleContextMenuKeyBoard/ViewletSearchHandleContextMenuKeyBoard.ts')
+
+  const state: SearchState = {
+    ...Create.create(0, 0, 0, 0, 0, '', ''),
+    x: 100,
+    y: 200,
+  }
+
+  const result = await handleContextMenuKeyboard(state)
+
+  expect(result).toBe(state)
+  expect(mockContextMenu.show).toHaveBeenCalledWith(100, 200, 18)
+})
