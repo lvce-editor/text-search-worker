e80c93698188c80ad1dc0f60c73cb3a3c5b133fc
Le Vivilet <levivlet@pm.me>
2024-11-15 20:52:29 +0100

    feature: add focus test

---
+++ packages/text-search-worker/test/FocusSearchValueNext.test.ts
@@ -0,0 +1,36 @@
+import { expect, test } from '@jest/globals'
+import * as Create from '../src/parts/Create/Create.ts'
+import { focusSearchValueNext } from '../src/parts/FocusSearchValueNext/FocusSearchValueNext.ts'
+import * as WhenExpression from '../src/parts/WhenExpression/WhenExpression.ts'
+import * as InputSource from '../src/parts/InputSource/InputSource.ts'
+import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
+
+test('focusSearchValueNext - with replace expanded', () => {
+  const state: SearchState = {
+    ...Create.create(0, 0, 0, 0, 0, '', ''),
+    replaceExpanded: true,
+  }
+
+  const result = focusSearchValueNext(state)
+
+  expect(result).toEqual({
+    ...state,
+    focus: WhenExpression.FocusSearchReplaceInput,
+    focusSource: InputSource.Script,
+  })
+})
+
+test('focusSearchValueNext - without replace expanded', () => {
+  const state: SearchState = {
+    ...Create.create(0, 0, 0, 0, 0, '', ''),
+    replaceExpanded: false,
+  }
+
+  const result = focusSearchValueNext(state)
+
+  expect(result).toEqual({
+    ...state,
+    focus: WhenExpression.FocusSearchMatchCase,
+    focusSource: InputSource.Script,
+  })
+})
