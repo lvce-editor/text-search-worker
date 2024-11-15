c2e95b2363402dc417480f0f84bd59a7bf5d1d3d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-15 21:36:21 +0100

    feature: add browserErrorTypes test (#149)
    * feature: add browserErrorTypes test
    
    * test

---
+++ packages/text-search-worker/test/BrowserErrorTypes.test.ts
@@ -0,0 +1,32 @@
+import { expect, test } from '@jest/globals'
+import * as BrowserErrorTypes from '../src/parts/BrowserErrorTypes/BrowserErrorTypes.ts'
+import * as DomExceptionType from '../src/parts/DomExceptionType/DomExceptionType.ts'
+
+test('isNotReadableError - with NotReadableError', () => {
+  const error = {
+    name: DomExceptionType.NotReadableError,
+  }
+  expect(BrowserErrorTypes.isNotReadableError(error)).toBe(true)
+})
+
+test('isNotReadableError - with different error', () => {
+  const error = {
+    name: 'TypeError',
+  }
+  expect(BrowserErrorTypes.isNotReadableError(error)).toBe(false)
+})
+
+test('isNotReadableError - with undefined', () => {
+  expect(BrowserErrorTypes.isNotReadableError(undefined)).toBe(undefined)
+})
+
+test('isNotReadableError - with null', () => {
+  expect(BrowserErrorTypes.isNotReadableError(null)).toBe(null)
+})
+
+test('isNotReadableError - with non-error object', () => {
+  const nonError = {
+    message: 'test',
+  }
+  expect(BrowserErrorTypes.isNotReadableError(nonError)).toBe(false)
+})
