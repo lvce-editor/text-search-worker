b37820fa97ce479496c8e8bd5843e0c168c1efdb
Le Vivilet <levivlet@pm.me>
2024-11-16 19:44:22 +0100

    feature: use rpc dependency

--- packages/text-search-worker/src/parts/Listen/Listen.ts
+++ packages/text-search-worker/src/parts/Listen/Listen.ts
@@ -1,13 +1,10 @@
-import * as Command from '../Command/Command.ts'
+import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as HandleIpc from '../HandleIpc/HandleIpc.ts'
-import * as IpcChild from '../IpcChild/IpcChild.ts'
-import * as IpcChildType from '../IpcChildType/IpcChildType.ts'
-import * as Rpc from '../Rpc/Rpc.ts'
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
 export const listen = async (): Promise<void> => {
-  Command.register(CommandMap.commandMap)
-  const ipc = await IpcChild.listen({ method: IpcChildType.Auto() })
-  HandleIpc.handleIpc(ipc)
-  Rpc.listen(ipc)
+  const rpc = WebWorkerRpcClient.create({
+    commandMap: CommandMap.commandMap,
+  })
+  ParentRpc.setRpc(rpc)
 }
