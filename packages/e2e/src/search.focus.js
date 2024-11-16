c48b3908533c6a9f2eb66fcb4abb9ce430e31e13
Le Vivilet <levivlet@pm.me>
2024-11-16 17:50:00 +0100

    feature: skip e2e tests for now

--- packages/e2e/src/search.focus.js
+++ packages/e2e/src/search.focus.js
@@ -1,5 +1,7 @@
 export const name = 'search.focus'
 
+export const skip = true
+
 export const test = async ({ FileSystem, Workspace, Main, Editor, Locator, expect }) => {
   // TODO add search focus test
 }
