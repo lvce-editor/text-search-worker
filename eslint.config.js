435012d1da7bc8b4e94af54ec94a0099c91a04c8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-16 10:34:13 +0100

    feature: add e2e tests (#153)
    * feature: add e2e tests
    
    * Discard changes to package-lock.json
    
    * Discard changes to package.json
    
    * ignore e2e for now

--- eslint.config.js
+++ eslint.config.js
@@ -18,6 +18,7 @@ export default tseslint.config(
       '.tmp',
       '**/build/**',
       '**/coverage/**',
+      '**/e2e/**',
       'scripts',
       'rollup.config.js',
       'eslint.config.js',
