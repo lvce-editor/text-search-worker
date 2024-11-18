b08d7ed27eaf3cb6f254f692d323ef6e4d270bc1
Le Vivilet <levivlet@pm.me>
2024-11-18 22:32:49 +0100

    add back functions

--- packages/text-search-worker/src/parts/SearchFocus/SearchFocus.ts
+++ packages/text-search-worker/src/parts/SearchFocus/SearchFocus.ts
@@ -6,6 +6,38 @@ export const focusSearchValue = (state: SearchState): SearchState => {
   return FocusElement.focusElement(state, WhenExpression.FocusSearchInput)
 }
 
+export const focusSearchValueNext = (state: SearchState): SearchState => {
+  const { replaceExpanded } = state
+  if (replaceExpanded) {
+    return focusReplaceValue(state)
+  }
+  return focusMatchCase(state)
+}
+
+export const focusMatchCasePrevious = (state: SearchState): SearchState => {
+  const { replaceExpanded } = state
+  if (replaceExpanded) {
+    return focusReplaceValue(state)
+  }
+  return focusSearchValue(state)
+}
+
+export const focusReplaceValuePrevious = (state: SearchState): SearchState => {
+  return focusSearchValue(state)
+}
+
+export const focusReplaceValueNext = (state: SearchState): SearchState => {
+  return focusMatchCase(state)
+}
+
+export const focusRegexNext = (state: SearchState): SearchState => {
+  return focusPreserveCase(state)
+}
+
+export const focusPreserveCasePrevious = (state: SearchState): SearchState => {
+  return focusRegex(state)
+}
+
 export const focusReplaceValue = (state: SearchState): SearchState => {
   return FocusElement.focusElement(state, WhenExpression.FocusSearchReplaceInput)
 }
@@ -16,6 +48,10 @@ export const focusMatchCase = (state: SearchState): SearchState => {
 
 export const focusPreserveCase = (state: SearchState): SearchState => {
   return FocusElement.focusElement(state, WhenExpression.FocusSearchPreserveCase)
+}
+
+export const focusMatchWholeWord = (state: SearchState): SearchState => {
+  return FocusElement.focusElement(state, WhenExpression.FocusSearchWholeWord)
 }
 
 export const focusRegex = (state: SearchState): SearchState => {
