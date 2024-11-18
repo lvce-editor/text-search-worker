e1fbc9995bdd8692bd2628822d6596b1961e0eea
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-18 23:59:57 +0100

    feature: simplify chevron virtual dom (#203)
    * feature: simplify search field dom function
    
    * feature: simplify chevron virtual dom
    
    * coverage

--- packages/text-search-worker/src/parts/GetChevronVirtualDom/GetChevronVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetChevronVirtualDom/GetChevronVirtualDom.ts
@@ -1,20 +1,24 @@
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
-export const getChevronDownVirtualDom = (extraClassName = ''): VirtualDomNode => {
-  return {
-    type: VirtualDomElements.Div,
-    className: MergeClassNames.mergeClassNames(ClassNames.Chevron, ClassNames.MaskIconChevronDown, extraClassName),
-    childCount: 0,
-  }
+export const chevronDownVirtualDom: VirtualDomNode = {
+  type: VirtualDomElements.Div,
+  className: MergeClassNames.mergeClassNames(ClassNames.Chevron, ClassNames.MaskIconChevronDown),
+  childCount: 0,
 }
 
-export const getChevronRightVirtualDom = (extraClassName = ''): VirtualDomNode => {
-  return {
-    type: VirtualDomElements.Div,
-    className: MergeClassNames.mergeClassNames(ClassNames.Chevron, ClassNames.MaskIconChevronRight, extraClassName),
-    childCount: 0,
-  }
+export const chevronRightVirtualDom: VirtualDomNode = {
+  type: VirtualDomElements.Div,
+  className: MergeClassNames.mergeClassNames(ClassNames.Chevron, ClassNames.MaskIconChevronRight),
+  childCount: 0,
+}
+
+export const getChevronDownVirtualDom = (): VirtualDomNode => {
+  return chevronDownVirtualDom
+}
+
+export const getChevronRightVirtualDom = (): VirtualDomNode => {
+  return chevronRightVirtualDom
 }
