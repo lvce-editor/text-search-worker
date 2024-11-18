d0be657ab6f36be12756e198b276bf0257af236f
Le Vivilet <levivlet@pm.me>
2024-11-18 22:27:12 +0100

    feature: simplify search focus functions

--- packages/text-search-worker/src/parts/SearchFocus/SearchFocus.ts
+++ packages/text-search-worker/src/parts/SearchFocus/SearchFocus.ts
@@ -1,106 +1,27 @@
-import * as Focus from '../Focus/Focus.ts'
-import * as GetSearchFocusKey from '../GetSearchFocusKey/GetSearchFocusKey.ts'
+import * as FocusElement from '../FocusElement/FocusElement.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
-import * as InputSource from '../InputSource/InputSource.ts'
 
 export const focusSearchValue = (state: SearchState): SearchState => {
-  return {
-    ...state,
-    focus: WhenExpression.FocusSearchInput,
-    focusSource: InputSource.Script,
-  }
-}
-
-export const focusSearchValueNext = (state: SearchState): SearchState => {
-  const { replaceExpanded } = state
-  if (replaceExpanded) {
-    return focusReplaceValue(state)
-  }
-  return focusMatchCase(state)
-}
-
-export const focusMatchCasePrevious = (state: SearchState): SearchState => {
-  const { replaceExpanded } = state
-  if (replaceExpanded) {
-    return focusReplaceValue(state)
-  }
-  return focusSearchValue(state)
-}
-
-export const focusReplaceValuePrevious = (state: SearchState): SearchState => {
-  return focusSearchValue(state)
-}
-
-export const focusReplaceValueNext = (state: SearchState): SearchState => {
-  return focusMatchCase(state)
-}
-
-export const focusRegexNext = (state: SearchState): SearchState => {
-  return focusPreserveCase(state)
-}
-
-export const focusPreserveCasePrevious = (state: SearchState): SearchState => {
-  return focusRegex(state)
+  return FocusElement.focusElement(state, WhenExpression.FocusSearchInput)
 }
 
 export const focusReplaceValue = (state: SearchState): SearchState => {
-  return {
-    ...state,
-    focus: WhenExpression.FocusSearchReplaceInput,
-    focusSource: InputSource.Script,
-  }
+  return FocusElement.focusElement(state, WhenExpression.FocusSearchReplaceInput)
 }
 
 export const focusMatchCase = (state: SearchState): SearchState => {
-  return {
-    ...state,
-    focus: WhenExpression.FocusSearchMatchCase,
-    focusSource: InputSource.Script,
-  }
+  return FocusElement.focusElement(state, WhenExpression.FocusSearchMatchCase)
 }
 
 export const focusPreserveCase = (state: SearchState): SearchState => {
-  return {
-    ...state,
-    focus: WhenExpression.FocusSearchPreserveCase,
-    focusSource: InputSource.Script,
-  }
-}
-
-export const focusMatchWholeWord = (state: SearchState): SearchState => {
-  return {
-    ...state,
-    focus: WhenExpression.FocusSearchWholeWord,
-    focusSource: InputSource.Script,
-  }
+  return FocusElement.focusElement(state, WhenExpression.FocusSearchPreserveCase)
 }
 
 export const focusRegex = (state: SearchState): SearchState => {
-  return {
-    ...state,
-    focus: WhenExpression.FocusSearchRegex,
-    focusSource: InputSource.Script,
-  }
+  return FocusElement.focusElement(state, WhenExpression.FocusSearchRegex)
 }
 
 export const focusReplaceAll = (state: SearchState): SearchState => {
-  return {
-    ...state,
-    focus: WhenExpression.FocusSearchReplaceAll,
-    focusSource: InputSource.Script,
-  }
-}
-
-export const handleFocusIn = (state: SearchState, key: any): SearchState => {
-  const focusKey = GetSearchFocusKey.getSearchFocusKey(key)
-  if (state.focus === focusKey) {
-    return state
-  }
-  void Focus.setFocus(focusKey)
-  return {
-    ...state,
-    focus: focusKey,
-    focusSource: InputSource.User,
-  }
+  return FocusElement.focusElement(state, WhenExpression.FocusSearchReplaceAll)
 }
