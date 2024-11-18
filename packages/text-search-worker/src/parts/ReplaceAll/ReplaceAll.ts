bfe5deaf2ae220c6764315ec5c106a101b648a15
Le Vivilet <levivlet@pm.me>
2024-11-19 00:10:23 +0100

    feature: add bulk replacement types

--- packages/text-search-worker/src/parts/ReplaceAll/ReplaceAll.ts
+++ packages/text-search-worker/src/parts/ReplaceAll/ReplaceAll.ts
@@ -1,8 +1,14 @@
+import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
 import * as GetBulkReplacementEdits from '../GetBulkReplacementEdits/GetBulkReplacementEdits.ts'
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
 
-export const replaceAll = async (state: SearchState, workspacePath: string, items: readonly any[], replacement: string): Promise<SearchState> => {
+export const replaceAll = async (
+  state: SearchState,
+  workspacePath: string,
+  items: readonly DisplaySearchResult[],
+  replacement: string,
+): Promise<SearchState> => {
   const { files, ranges } = GetBulkReplacementEdits.getBulkReplacementEdits(workspacePath, items)
   await Rpc.invoke('BulkReplacement.applyBulkReplacement', files, ranges, replacement)
   return state
--- packages/text-search-worker/test/GetBulkReplacementEdits.test.ts
+++ packages/text-search-worker/test/GetBulkReplacementEdits.test.ts
@@ -1,13 +1,26 @@
 import { expect, test } from '@jest/globals'
 import * as GetBulkReplacementEdits from '../src/parts/GetBulkReplacementEdits/GetBulkReplacementEdits.ts'
 import * as TextSearchResultType from '../src/parts/TextSearchResultType/TextSearchResultType.ts'
+import { DisplaySearchResult } from '../src/parts/DisplaySearchResult/DisplaySearchResult.ts'
 
 test('getBulkReplacementEdits', () => {
   const workspacePath = '/test'
-  const matches = [
+  const matches: readonly DisplaySearchResult[] = [
     {
       type: TextSearchResultType.File,
       text: './file.txt',
+      lineNumber: 0,
+      matchLength: 0,
+      matchStart: 0,
+      title: '',
+      depth: 0,
+      focused: false,
+      icon: '',
+      matchCount: 0,
+      posInSet: 0,
+      replacement: '',
+      setSize: 0,
+      top: 0,
     },
     {
       type: TextSearchResultType.Match,
@@ -15,6 +28,15 @@ test('getBulkReplacementEdits', () => {
       lineNumber: 1,
       matchLength: 1,
       matchStart: 0,
+      depth: 0,
+      focused: false,
+      icon: '',
+      matchCount: 0,
+      posInSet: 0,
+      replacement: '',
+      setSize: 0,
+      top: 0,
+      text: '',
     },
   ]
   expect(GetBulkReplacementEdits.getBulkReplacementEdits(workspacePath, matches)).toEqual({ files: ['/test/file.txt'], ranges: [4, 0, 0, 0, 1] })
