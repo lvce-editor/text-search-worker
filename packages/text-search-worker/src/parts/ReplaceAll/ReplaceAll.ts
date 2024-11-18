538649a9416c7a67d728eb9cc3fe50161c16db61
Le Vivilet <levivlet@pm.me>
2024-11-18 23:34:10 +0100

    feature: improve replaceAll function

--- packages/text-search-worker/src/parts/ReplaceAll/ReplaceAll.ts
+++ packages/text-search-worker/src/parts/ReplaceAll/ReplaceAll.ts
@@ -1,7 +1,9 @@
 import * as GetBulkReplacementEdits from '../GetBulkReplacementEdits/GetBulkReplacementEdits.ts'
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
+import type { SearchState } from '../SearchState/SearchState.ts'
 
-export const replaceAll = async (workspacePath: string, items: readonly any[], replacement: string): Promise<void> => {
+export const replaceAll = async (state: SearchState, workspacePath: string, items: readonly any[], replacement: string): Promise<SearchState> => {
   const { files, ranges } = GetBulkReplacementEdits.getBulkReplacementEdits(workspacePath, items)
   await Rpc.invoke('BulkReplacement.applyBulkReplacement', files, ranges, replacement)
+  return state
 }
