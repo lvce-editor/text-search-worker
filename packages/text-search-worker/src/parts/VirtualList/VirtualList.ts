3f3f8e26edc726bb5d4d2a2ba6d6d9e652126f4c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-18 22:05:06 +0100

    Discard changes to packages/text-search-worker/src/parts/VirtualList/VirtualList.ts

--- packages/text-search-worker/src/parts/VirtualList/VirtualList.ts
+++ packages/text-search-worker/src/parts/VirtualList/VirtualList.ts
@@ -1,28 +1,13 @@
-import type { SearchList } from '../SearchList/SearchList.ts'
-import * as InputSource from '../InputSource/InputSource.ts'
-
 export const create = ({
   itemHeight,
   headerHeight = 0,
   minimumSliderSize = 20,
-  x,
-  y,
-  width,
-  height,
-  workspacePath,
-  assetDir,
 }: {
   itemHeight: number
   headerHeight: number
   minimumSliderSize: number
-  x: number
-  y: number
-  width: number
-  height: number
-  workspacePath: string
-  assetDir: string
-}): SearchList => {
-  const state: SearchList = {
+}): any => {
+  return {
     deltaY: 0,
     minLineY: 0,
     maxLineY: 0,
@@ -37,27 +22,5 @@ export const create = ({
     touchDifference: 0,
     scrollBarHeight: 0,
     scrollBarActive: false,
-    searchResults: [],
-    stats: {},
-    searchId: -1,
-    fileCount: 0,
-    x,
-    y,
-    width,
-    height,
-    threads: 0,
-    matchCount: 0,
-    listFocused: false,
-    listFocusedIndex: -1,
-    workspacePath: workspacePath,
-    loaded: false,
-    message: '',
-    collapsedPaths: [],
-    icons: [],
-    assetDir,
-    handleOffset: 0,
-    disposed: false,
-    inputSource: InputSource.User,
   }
-  return state
 }
