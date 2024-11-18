746767e2b8d51d93e50e20ce44e30be8e2c4363b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-18 21:30:21 +0100

    feature: add handleReplaceInput function (#177)
    * feature: add handleReplaceInput function
    
    * coverage

--- packages/text-search-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/text-search-worker/src/parts/CommandMap/CommandMap.ts
@@ -12,6 +12,7 @@ import * as GetSearchDisplayResults from '../GetSearchDisplayResults/GetSearchDi
 import * as GetSearchVirtualDom from '../GetSearchVirtualDom/GetSearchVirtualDom.ts'
 import * as HandleIconThemeChange from '../HandleIconThemeChange/HandleIconThemeChange.ts'
 import * as HandleInput from '../HandleInput/HandleInput.ts'
+import * as HandleReplaceInput from '../HandleReplaceInput/HandleReplaceInput.ts'
 import * as ListFocusFirst from '../ListFocusFirst/ListFocusFirst.ts'
 import * as ListFocusIndex from '../ListFocusIndex/ListFocusIndex.ts'
 import * as ListFocusLast from '../ListFocusLast/ListFocusLast.ts'
@@ -78,6 +79,7 @@ export const commandMap = {
   'TextSearch.replaceAll': WrapCommand.wrapCommand(ReplaceAll.replaceAll),
   'TextSearch.submit': WrapCommand.wrapCommand(Submit.submit),
   'TextSearch.toggleReplace': WrapCommand.wrapCommand(ToggleReplace.toggleReplace),
+  'TextSearch.handleReplaceInput': WrapCommand.wrapCommand(HandleReplaceInput.handleReplaceInput),
 
   // not wrapped
   'TextSearch.create': Create.create,
