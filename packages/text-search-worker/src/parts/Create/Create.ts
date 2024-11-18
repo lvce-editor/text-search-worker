1bc0e579c877f6c16f667a3c6a364bccc5120e48
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-18 22:04:40 +0100

    Discard changes to packages/text-search-worker/src/parts/Create/Create.ts

--- packages/text-search-worker/src/parts/Create/Create.ts
+++ packages/text-search-worker/src/parts/Create/Create.ts
@@ -1,25 +1,56 @@
-import * as CreateHeader from '../CreateHeader/CreateHeader.ts'
 import * as Height from '../Height/Height.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 import * as MinimumSliderSize from '../MinimumSliderSize/MinimumSliderSize.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
 import * as SearchViewStates from '../SearchViewStates/SearchViewStates.ts'
 import * as VirtualList from '../VirtualList/VirtualList.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const create = (uid: number, x: number, y: number, width: number, height: number, workspacePath: string, assetDir: string): SearchState => {
   const state: SearchState = {
     uid,
+    searchResults: [],
+    stats: {},
+    searchId: -1,
+    value: '',
+    disposed: false,
+    fileCount: 0,
+    x,
+    y,
+    width,
+    height,
     ...VirtualList.create({
       itemHeight: Height.ListItem,
       minimumSliderSize: MinimumSliderSize.minimumSliderSize,
       headerHeight: 61, // TODO
-      x,
-      y,
-      width,
-      height,
-      workspacePath,
-      assetDir,
     }),
-    ...CreateHeader.createHeader(),
+    threads: 0,
+    replaceExpanded: false,
+    useRegularExpression: false,
+    matchCase: false,
+    matchWholeWord: false,
+    replacement: '',
+    matchCount: 0,
+    listFocused: false,
+    listFocusedIndex: -1,
+    inputSource: InputSource.User,
+    workspacePath: workspacePath,
+    includeValue: '',
+    excludeValue: '',
+    detailsExpanded: false,
+    focus: WhenExpression.Empty,
+    loaded: false,
+    message: '',
+    collapsedPaths: [],
+    icons: [],
+    assetDir,
+    deltaY: 0,
+    finalDeltaY: 0,
+    focused: false,
+    focusedIndex: -1,
+    handleOffset: 0,
+    focusSource: InputSource.User,
+    preserveCase: false,
   }
   SearchViewStates.set(uid, state, state)
   return state
