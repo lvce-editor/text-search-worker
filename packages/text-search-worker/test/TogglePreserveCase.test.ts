cfe3aa7d4ab73fe52162b90ef39e17d43b395eb1
Le Vivilet <levivlet@pm.me>
2024-11-19 22:39:39 +0100

    test

--- packages/text-search-worker/test/TogglePreserveCase.test.ts
+++ packages/text-search-worker/test/TogglePreserveCase.test.ts
@@ -2,12 +2,13 @@ import { expect, test } from '@jest/globals'
 import * as Create from '../src/parts/Create/Create.ts'
 import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
 import * as TogglePreserveCase from '../src/parts/TogglePreserveCase/TogglePreserveCase.ts'
+import * as SearchFlags from '../src/parts/SearchFlags/SearchFlags.ts'
 
 test('togglePreserveCase', () => {
   const initialState: SearchState = {
     ...Create.create(0, 0, 0, 0, 0, '', ''),
-    preserveCase: false,
+    flags: 0,
   }
   const newState = TogglePreserveCase.togglePreserveCase(initialState)
-  expect(newState.preserveCase).toBe(true)
+  expect(SearchFlags.hasPreserveCase(newState.flags)).toBe(true)
 })
