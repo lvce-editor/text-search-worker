1d3a97276a5978cee51437db56847f9998a72f2c
Le Vivilet <levivlet@pm.me>
2024-11-19 20:50:00 +0100

    tsc

--- packages/text-search-worker/test/SaveState.test.ts
+++ packages/text-search-worker/test/SaveState.test.ts
@@ -1,9 +1,12 @@
 import { expect, test } from '@jest/globals'
+import * as Create from '../src/parts/Create/Create.ts'
 import { saveState } from '../src/parts/SaveState/SaveState.ts'
+import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
 import * as SearchViewStates from '../src/parts/SearchViewStates/SearchViewStates.ts'
 
 test('saveState', () => {
-  const state = {
+  const state: SearchState = {
+    ...Create.create(0, 0, 0, 0, 0, '', ''),
     value: 'test-value',
     replaceExpanded: true,
   }
