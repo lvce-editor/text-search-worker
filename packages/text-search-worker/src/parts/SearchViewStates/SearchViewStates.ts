41b28a2c4b06fe66d48b7dc911a95e05df72319f
Le Vivilet <levivlet@pm.me>
2024-11-19 20:47:55 +0100

    feature: add types for searchState

--- packages/text-search-worker/src/parts/SearchViewStates/SearchViewStates.ts
+++ packages/text-search-worker/src/parts/SearchViewStates/SearchViewStates.ts
@@ -1,10 +1,17 @@
+import type { SearchState } from '../SearchState/SearchState.ts'
+
 const states = Object.create(null)
 
-export const get = (uid: number): any => {
+export const get = (
+  uid: number,
+): {
+  oldState: SearchState
+  newState: SearchState
+} => {
   return states[uid]
 }
 
-export const set = (uid: number, oldState: any, newState: any): void => {
+export const set = (uid: number, oldState: SearchState, newState: SearchState): void => {
   states[uid] = {
     oldState,
     newState,
