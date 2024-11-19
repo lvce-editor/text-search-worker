c5d42068964a049aa9d4068902b116ee57cfd899
Le Vivilet <levivlet@pm.me>
2024-11-19 22:52:55 +0100

    simplify restore

--- packages/text-search-worker/src/parts/RestoreState/RestoreState.ts
+++ packages/text-search-worker/src/parts/RestoreState/RestoreState.ts
@@ -16,57 +16,18 @@ const getSavedReplacement = (savedState: unknown): string => {
 }
 
 const getSavedFlags = (savedState: unknown): number => {
-  if (savedState && typeof savedState === 'object') {
-    let flags = 0
-    if ('preserveCase' in savedState && savedState.preserveCase === true) {
-      flags |= SearchFlags.PreserveCase
-    }
-    if ('useRegularExpression' in savedState && savedState.useRegularExpression === true) {
-      flags |= SearchFlags.UseRegularExpression
-    }
-    if ('replaceExpanded' in savedState && savedState.replaceExpanded === true) {
-      flags |= SearchFlags.ReplaceExpanded
-    }
-    if ('matchWholeWord' in savedState && savedState.matchWholeWord === true) {
-      flags |= SearchFlags.MatchWholeWord
-    }
-    if ('matchCase' in savedState && savedState.matchCase === true) {
-      flags |= SearchFlags.MatchCase
-    }
-    return flags
+  if (savedState && typeof savedState === 'object' && 'flags' in savedState && typeof savedState.flags === 'number') {
+    return savedState.flags
   }
   return 0
 }
 
-const getSavedCollapsedPaths = (savedState: unknown): string[] => {
-  if (
-    savedState &&
-    typeof savedState === 'object' &&
-    'collapsedPaths' in savedState &&
-    Array.isArray(savedState.collapsedPaths) &&
-    savedState.collapsedPaths.every((path: any) => typeof path === 'string')
-  ) {
-    return savedState.collapsedPaths
-  }
-  return []
-}
-
-const getThreads = (): number => {
-  return 1
-}
-
 export const restoreState = (savedState: unknown): RestoredState => {
-  const savedValue = getSavedValue(savedState)
-  const savedCollapsedPaths = getSavedCollapsedPaths(savedState)
-  const threads = getThreads()
-  const replacement = getSavedReplacement(savedState)
-  const flags = getSavedFlags(savedState)
-
   return {
-    savedCollapsedPaths,
-    savedValue,
-    threads,
-    replacement,
-    flags,
+    savedValue: getSavedValue(savedState),
+    replacement: getSavedReplacement(savedState),
+    savedCollapsedPaths: [],
+    threads: 1,
+    flags: getSavedFlags(savedState),
   }
 }
