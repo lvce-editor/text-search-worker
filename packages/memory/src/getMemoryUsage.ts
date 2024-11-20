b60c87ea0c22f7ca188c0fae1c69608f1fb9ed49
Le Vivilet <levivlet@pm.me>
2024-11-20 19:15:15 +0100

    improve

--- packages/memory/src/getMemoryUsage.ts
+++ packages/memory/src/getMemoryUsage.ts
@@ -13,7 +13,9 @@ export const getMemoryUsage = async (page: Page) => {
   }
 
   const client = await page.context().newCDPSession(page)
-  const { workers } = await client.send('ServiceWorker.getAllWorkers')
+  const { targetInfos } = await client.send('Target.getTargets')
+
+  const workers = targetInfos.filter((target) => target.type === 'worker')
 
   if (!workers.length) {
     console.error('[memory] No workers found')
@@ -22,7 +24,9 @@ export const getMemoryUsage = async (page: Page) => {
 
   const results = []
   for (const worker of workers) {
-    const metrics = await client.send('Performance.getMetrics')
+    const workerSession = await client.connection().createSession(worker.targetId)
+    const metrics = await workerSession.send('Performance.getMetrics')
+    await workerSession.detach()
     results.push({
       jsHeapSize: metrics.metrics.find((m) => m.name === 'JSHeapUsedSize')?.value,
       totalHeapSize: metrics.metrics.find((m) => m.name === 'JSHeapTotalSize')?.value,
