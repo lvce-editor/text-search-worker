ba64dd19771d8084209cd91697dec868a7e96c39
Le Vivilet <levivlet@pm.me>
2024-11-20 19:40:31 +0100

    feature: try to get memory usage

--- packages/memory/src/getMemoryUsage.ts
+++ packages/memory/src/getMemoryUsage.ts
@@ -1,10 +1,10 @@
 import type { Page } from 'playwright'
 
 export const getMemoryUsage = async (page: Page) => {
+  await new Promise((r) => setTimeout(r, 1000))
   const client = await page.context().newCDPSession(page)
-  const { targetInfos } = await client.send('Target.getTargets')
 
-  const workers = targetInfos.filter((target) => target.type === 'worker')
+  const workers = page.workers()
 
   if (!workers.length) {
     console.error('[memory] No workers found')
@@ -13,16 +13,20 @@ export const getMemoryUsage = async (page: Page) => {
 
   const results = []
   for (const worker of workers) {
-    // @ts-ignore
-    const workerSession = await client.connection().createSession(worker.targetId)
-    const metrics = await workerSession.send('Performance.getMetrics')
-    await workerSession.detach()
-    results.push({
-      // @ts-ignore
-      jsHeapSize: metrics.metrics.find((m) => m.name === 'JSHeapUsedSize')?.value,
-      // @ts-ignore
-      totalHeapSize: metrics.metrics.find((m) => m.name === 'JSHeapTotalSize')?.value,
-    })
+    console.log('worker', worker.url())
+    console.log(worker)
+    worker.
+    // const metrics = await worker.evaluate((pageFunction, arg) => {
+    // // @ts-ignore
+    // const workerSession = await client.connection().createSession(worker.targetId)
+    // const metrics = await workerSession.send('Performance.getMetrics')
+    // await workerSession.detach()
+    // results.push({
+    //   // @ts-ignore
+    //   jsHeapSize: metrics.metrics.find((m) => m.name === 'JSHeapUsedSize')?.value,
+    //   // @ts-ignore
+    //   totalHeapSize: metrics.metrics.find((m) => m.name === 'JSHeapTotalSize')?.value,
+    // })
   }
   return results
 }
