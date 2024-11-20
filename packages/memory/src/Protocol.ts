5d9f7f4120310fbc4d2423a961de0e5f28f71e42
Le Vivilet <levivlet@pm.me>
2024-11-20 21:23:41 +0100

    remove unused code

--- packages/memory/src/Protocol.ts
+++
@@ -1,6 +0,0 @@
-export interface Protocol {
-  send: (method: string, params?: any, sessionId?: string) => Promise<any>
-  close: () => void
-  addEventListener: (event: string, listener: (event: CustomEvent) => void) => void
-  removeEventListener: (event: string, listener: (event: CustomEvent) => void) => void
-}
