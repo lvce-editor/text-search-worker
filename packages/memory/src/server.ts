15e66986ba1e419727aca2bf14555e88395cf594
Le Vivilet <levivlet@pm.me>
2024-11-20 19:24:47 +0100

    move app

--- packages/memory/src/server.ts
+++ packages/memory/src/server.ts
@@ -1,22 +1,12 @@
-import express from 'express'
 import { fileURLToPath } from 'node:url'
 import { dirname, join } from 'node:path'
+import { createApp } from './createApp.ts'
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const root = join(__dirname, '..')
 
 export const startServer = async (port: number, textSearchWorkerPath: string): Promise<void> => {
-  const app = express()
-
-  // Serve worker file
-  app.get('/dist/textSearchWorkerMain.js', (req, res) => {
-    res.sendFile(textSearchWorkerPath)
-  })
-
-  // Serve index.html from root directory
-  app.get('/', (req, res) => {
-    res.sendFile(join(root, 'index.html'))
-  })
+  const app = createApp(textSearchWorkerPath, root)
 
   const { promise, resolve } = Promise.withResolvers<void>()
   app.listen(port, () => {
