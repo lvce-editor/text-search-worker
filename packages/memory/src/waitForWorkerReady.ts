008d04c4200d9becbd6f7700e88d40b0c26b0d96
Le Vivilet <levivlet@pm.me>
2024-11-20 21:38:34 +0100

    fix measure

--- packages/memory/src/waitForWorkerReady.ts
+++ packages/memory/src/waitForWorkerReady.ts
@@ -4,7 +4,9 @@ import { WorkerInitializationError } from './errors.ts'
 
 export const waitForWorkerReady = async (page: Page): Promise<void> => {
   const workerState = await Promise.race([
+    // @ts-ignore
     page.waitForFunction(() => window.__workerDidLaunch === 1, { timeout: 5000 }).then(() => WorkerState.Launched),
+    // @ts-ignore
     page.waitForFunction(() => window.__workerDidLaunch === 2, { timeout: 5000 }).then(() => WorkerState.Error),
   ])
 
