91c04670e9cc20eb59388f795d18d864d08fd9f9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-21 21:42:30 +0100

    feature: simplify shared input handler (#268)

--- packages/text-search-worker/src/parts/HandleSharedInput/HandleSharedInput.ts
+++ packages/text-search-worker/src/parts/HandleSharedInput/HandleSharedInput.ts
@@ -1,15 +1,8 @@
-import * as HandleReplaceInput from '../HandleReplaceInput/HandleReplaceInput.ts'
+import * as GetInputHandler from '../GetInputHandler/GetInputHandler.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
-import * as HandleInput from '../HandleInput/HandleInput.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
 
-export const handleSharedInput = async (state: SearchState, name: string, value: string, inputSource = InputSource.Script): Promise<SearchState> => {
-  switch (name) {
-    case 'search-value':
-      return HandleInput.handleInput(state, value, inputSource)
-    case 'replacement-value':
-      return HandleReplaceInput.handleReplaceInput(state, value, inputSource)
-    default:
-      return state
-  }
+export const handleSharedInput = (state: SearchState, name: string, value: string, inputSource = InputSource.Script): Promise<SearchState> => {
+  const fn = GetInputHandler.getInputHandler(name)
+  return fn(state, value, inputSource)
 }
