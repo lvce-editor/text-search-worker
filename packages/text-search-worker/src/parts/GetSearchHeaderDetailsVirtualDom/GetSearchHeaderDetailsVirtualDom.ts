c6454f103ff82448071d3c90c6ee31bd7fd8caf9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-21 19:59:37 +0100

    feature: simplify isExpanded code (#257)

--- packages/text-search-worker/src/parts/GetSearchHeaderDetailsVirtualDom/GetSearchHeaderDetailsVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchHeaderDetailsVirtualDom/GetSearchHeaderDetailsVirtualDom.ts
@@ -19,7 +19,7 @@ export const getSearchHeaderDetailsVirtualDom = (flags: number): readonly Virtua
       childCount: 0,
     },
   ]
-  if (flags & DetailsExpanded) {
+  if (isExpanded) {
     dom.push(
       text(SearchStrings.filesToInclude()),
       {
