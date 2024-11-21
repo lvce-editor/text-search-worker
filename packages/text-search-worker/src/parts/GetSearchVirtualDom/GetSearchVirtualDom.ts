49f42d7ff79b54506e120fb2791ab144a6f814d9
Le Vivilet <levivlet@pm.me>
2024-11-21 18:58:46 +0100

    tsc

--- packages/text-search-worker/src/parts/GetSearchVirtualDom/GetSearchVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchVirtualDom/GetSearchVirtualDom.ts
@@ -1,7 +1,5 @@
-import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetSearchHeaderVirtualDom from '../GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.ts'
 import * as GetSearchMessageVirtualDom from '../GetSearchMessageVirtualDom/GetSearchMessageVirtualDom.ts'
 import * as GetSearchResultsVirtualDom from '../GetSearchResultsVirtualDom/GetSearchResultsVirtualDom.ts'
