6f186857bbd9ef1bd91241852f76205f44fc0b06
Le Vivilet <levivlet@pm.me>
2024-11-21 18:28:04 +0100

    feature: split selectIndex function into separate files

--- packages/text-search-worker/src/parts/SelectIndex/SelectIndex.ts
+++ packages/text-search-worker/src/parts/SelectIndex/SelectIndex.ts
@@ -1,51 +1,7 @@
-import * as Assert from '../Assert/Assert.ts'
-import * as OpenUri from '../OpenUri/OpenUri.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
+import * as SelectIndexFile from '../SelectIndexFile/SelectIndexFile.ts'
+import * as SelectIndexPreview from '../SelectIndexPreview/SelectIndexPreview.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
-import * as Workspace from '../Workspace/Workspace.ts'
-
-const getFileIndex = (items: readonly any[], index: number): number => {
-  for (let i = index; i >= 0; i--) {
-    const item = items[i]
-    if (item.type === TextSearchResultType.File) {
-      return i
-    }
-  }
-  return -1
-}
-
-const selectIndexFile = (state: SearchState, searchResult: any, index: number): SearchState => {
-  const { collapsedPaths } = state
-  const path = Workspace.getAbsolutePath(searchResult.text)
-  Assert.string(path)
-  return {
-    ...state,
-    collapsedPaths: [...collapsedPaths, path],
-    listFocusedIndex: index,
-    listFocused: true,
-  }
-}
-
-const selectIndexPreview = async (state: SearchState, searchResult: any, index: number): Promise<SearchState> => {
-  const { items } = state
-  const fileIndex = getFileIndex(items, index)
-  if (fileIndex === -1) {
-    throw new Error('Search result is missing file')
-  }
-  const { lineNumber } = searchResult
-  // console.log({ searchResult })
-  const fileResult = items[fileIndex]
-  const path = Workspace.getAbsolutePath(fileResult.text)
-  Assert.string(path)
-  await OpenUri.openUri(path, true, {
-    selections: new Uint32Array([lineNumber, 0, lineNumber, 0]),
-  })
-  return {
-    ...state,
-    listFocusedIndex: index,
-    listFocused: false,
-  }
-}
 
 export const selectIndex = async (state: SearchState, index: number): Promise<SearchState> => {
   if (index === -1) {
@@ -59,9 +15,9 @@ export const selectIndex = async (state: SearchState, index: number): Promise<Se
   const searchResult = items[index]
   switch (searchResult.type) {
     case TextSearchResultType.File:
-      return selectIndexFile(state, searchResult, index)
+      return SelectIndexFile.selectIndexFile(state, searchResult, index)
     case TextSearchResultType.Match:
-      return selectIndexPreview(state, searchResult, index)
+      return SelectIndexPreview.selectIndexPreview(state, searchResult, index)
     default:
       throw new Error(`unexpected search result type ${searchResult.type}`)
   }
---
+++ packages/text-search-worker/src/parts/SelectIndexFile/SelectIndexFile.ts
@@ -0,0 +1,15 @@
+import * as Assert from '../Assert/Assert.ts'
+import type { SearchState } from '../SearchState/SearchState.ts'
+import * as Workspace from '../Workspace/Workspace.ts'
+
+export const selectIndexFile = (state: SearchState, searchResult: any, index: number): SearchState => {
+  const { collapsedPaths } = state
+  const path = Workspace.getAbsolutePath(searchResult.text)
+  Assert.string(path)
+  return {
+    ...state,
+    collapsedPaths: [...collapsedPaths, path],
+    listFocusedIndex: index,
+    listFocused: true,
+  }
+}
