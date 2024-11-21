2c19434ba1d847d0175cd36b716ebcba56856f59
Le Vivilet <levivlet@pm.me>
2024-11-21 21:09:06 +0100

    feature: simplify search details virtual dom

--- packages/text-search-worker/src/parts/GetSearchHeaderDetailsCollapsedVirtualDom/GetSearchHeaderDetailsCollapsedVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchHeaderDetailsCollapsedVirtualDom/GetSearchHeaderDetailsCollapsedVirtualDom.ts
@@ -11,7 +11,7 @@ export const getSearchHeaderDetailsCollapsedVirtualDom = (message: string): read
       className: ClassNames.SearchHeaderDetails,
       childCount: 2,
     },
-    ...GetSearchDetailsToggleVirtualDom.getSearchDetailsToggleVirtualDom(),
     ...GetSearchMessageVirtualDom.getSearchMessageVirtualDom(message),
+    ...GetSearchDetailsToggleVirtualDom.getSearchDetailsToggleVirtualDom(),
   ]
 }
--- packages/text-search-worker/src/parts/GetSearchHeaderDetailsVirtualDom/GetSearchHeaderDetailsVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchHeaderDetailsVirtualDom/GetSearchHeaderDetailsVirtualDom.ts
@@ -1,37 +1,12 @@
-import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetSearchHeaderDetailsCollapsedVirtualDom from '../GetSearchHeaderDetailsCollapsedVirtualDom/GetSearchHeaderDetailsCollapsedVirtualDom.ts'
+import * as GetSearchHeaderDetailsExpandedVirtualDom from '../GetSearchHeaderDetailsExpandedVirtualDom/GetSearchHeaderDetailsExpandedVirtualDom.ts'
 import { DetailsExpanded } from '../SearchFlags/SearchFlags.ts'
-import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
-export const getSearchHeaderDetailsVirtualDom = (flags: number): readonly VirtualDomNode[] => {
+export const getSearchHeaderDetailsVirtualDom = (flags: number, message: string): readonly VirtualDomNode[] => {
   const isExpanded = flags & DetailsExpanded
-  const dom: VirtualDomNode[] = [
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.SearchHeaderDetails,
-      childCount: isExpanded ? 4 : 1,
-    },
-    {
-      type: VirtualDomElements.Div,
-      className: 'ToggleDetails',
-      childCount: 0,
-    },
-  ]
   if (isExpanded) {
-    dom.push(
-      text(SearchStrings.filesToInclude()),
-      {
-        type: VirtualDomElements.Input,
-        childCount: 0,
-      },
-      text(SearchStrings.filesToExclude()),
-      {
-        type: VirtualDomElements.Input,
-        childCount: 0,
-      },
-    )
+    return GetSearchHeaderDetailsExpandedVirtualDom.getSearchHeaderDetailsExpandedVirtualDom(message)
   }
-  return dom
+  return GetSearchHeaderDetailsCollapsedVirtualDom.getSearchHeaderDetailsCollapsedVirtualDom(message)
 }
