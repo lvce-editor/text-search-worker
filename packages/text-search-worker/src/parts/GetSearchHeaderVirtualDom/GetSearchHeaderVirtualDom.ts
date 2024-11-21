b2276a383cd592aa17404409ab6a596dfdbbedb1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-21 20:55:46 +0100

    Discard changes to packages/text-search-worker/src/parts/GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.ts

--- packages/text-search-worker/src/parts/GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.ts
@@ -3,6 +3,7 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as GetSearchHeaderDetailsVirtualDom from '../GetSearchHeaderDetailsVirtualDom/GetSearchHeaderDetailsVirtualDom.ts'
 import * as GetSearchHeaderTopVirtualDom from '../GetSearchHeaderTopVirtualDom/GetSearchHeaderTopVirtualDom.ts'
+import * as GetSearchMessageVirtualDom from '../GetSearchMessageVirtualDom/GetSearchMessageVirtualDom.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
@@ -12,12 +13,13 @@ export const getSearchHeaderVirtualDom = (flags: number, message: string): reado
       type: VirtualDomElements.Div,
       className: ClassNames.SearchHeader,
       role: AriaRoles.None,
-      childCount: 2,
+      childCount: 3,
       onClick: DomEventListenerFunctions.HandleHeaderClick,
       onFocusIn: DomEventListenerFunctions.HandleHeaderFocusIn,
     },
     ...GetSearchHeaderTopVirtualDom.getSearchHeaderTopVirtualDom(flags),
-    ...GetSearchHeaderDetailsVirtualDom.getSearchHeaderDetailsVirtualDom(flags, message),
+    ...GetSearchHeaderDetailsVirtualDom.getSearchHeaderDetailsVirtualDom(flags),
+    ...GetSearchMessageVirtualDom.getSearchMessageVirtualDom(message),
   ]
   return dom
 }
