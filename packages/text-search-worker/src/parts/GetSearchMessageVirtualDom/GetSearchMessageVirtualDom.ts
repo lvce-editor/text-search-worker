a12b87a56786655856d9093b6ac10a549c227aa9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-21 18:56:58 +0100

    feature: move search message into separate file (#248)

---
+++ packages/text-search-worker/src/parts/GetSearchMessageVirtualDom/GetSearchMessageVirtualDom.ts
@@ -0,0 +1,18 @@
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+
+export const getSearchMessageVirtualDom = (message: string): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.ViewletSearchMessage,
+      role: AriaRoles.Status,
+      tabIndex: 0,
+      childCount: 1,
+    },
+    text(message),
+  ]
+}
--- packages/text-search-worker/src/parts/GetSearchVirtualDom/GetSearchVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchVirtualDom/GetSearchVirtualDom.ts
@@ -1,13 +1,13 @@
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as GetSearchHeaderVirtualDom from '../GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.ts'
-import * as GetSearchResultsVirtualDom from '../GetSearchResultsVirtualDom/GetSearchResultsVirtualDom.ts'
-import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
-import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
-import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as GetSearchHeaderVirtualDom from '../GetSearchHeaderVirtualDom/GetSearchHeaderVirtualDom.ts'
+import * as GetSearchMessageVirtualDom from '../GetSearchMessageVirtualDom/GetSearchMessageVirtualDom.ts'
+import * as GetSearchResultsVirtualDom from '../GetSearchResultsVirtualDom/GetSearchResultsVirtualDom.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getSearchVirtualDom = (
   visibleItems: readonly DisplaySearchResult[],
@@ -22,15 +22,7 @@ export const getSearchVirtualDom = (
       childCount: 2,
     },
     ...GetSearchHeaderVirtualDom.getSearchHeaderVirtualDom(flags),
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.ViewletSearchMessage,
-      role: AriaRoles.Status,
-      tabIndex: 0,
-      childCount: 1,
-    },
-    text(message),
-
+    ...GetSearchMessageVirtualDom.getSearchMessageVirtualDom(message),
     {
       type: VirtualDomElements.Div,
       className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.List, focusOutline ? ClassNames.FocusOutline : ClassNames.Empty),
