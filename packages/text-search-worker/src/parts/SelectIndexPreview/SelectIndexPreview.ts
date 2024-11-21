1e34e219ffecc610a53a4bb794351bb1b9b31713
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-21 18:30:14 +0100

    feature: split selectIndex function into separate files (#245)

---
+++ packages/text-search-worker/src/parts/SelectIndexPreview/SelectIndexPreview.ts
@@ -0,0 +1,36 @@
+import * as Assert from '../Assert/Assert.ts'
+import * as OpenUri from '../OpenUri/OpenUri.ts'
+import type { SearchState } from '../SearchState/SearchState.ts'
+import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
+import * as Workspace from '../Workspace/Workspace.ts'
+
+const getFileIndex = (items: readonly any[], index: number): number => {
+  for (let i = index; i >= 0; i--) {
+    const item = items[i]
+    if (item.type === TextSearchResultType.File) {
+      return i
+    }
+  }
+  return -1
+}
+
+export const selectIndexPreview = async (state: SearchState, searchResult: any, index: number): Promise<SearchState> => {
+  const { items } = state
+  const fileIndex = getFileIndex(items, index)
+  if (fileIndex === -1) {
+    throw new Error('Search result is missing file')
+  }
+  const { lineNumber } = searchResult
+  // console.log({ searchResult })
+  const fileResult = items[fileIndex]
+  const path = Workspace.getAbsolutePath(fileResult.text)
+  Assert.string(path)
+  await OpenUri.openUri(path, true, {
+    selections: new Uint32Array([lineNumber, 0, lineNumber, 0]),
+  })
+  return {
+    ...state,
+    listFocusedIndex: index,
+    listFocused: false,
+  }
+}
