7ec3f7cd1f14cc7d0d42fb760f81fed7c888437b
Le Vivilet <levivlet@pm.me>
2024-11-21 19:33:04 +0100

    feature: remove assertions for clamp to reduce code size and improve performance

--- packages/text-search-worker/src/parts/Clamp/Clamp.ts
+++ packages/text-search-worker/src/parts/Clamp/Clamp.ts
@@ -1,8 +1,3 @@
-import * as Assert from '../Assert/Assert.ts'
-
 export const clamp = (num: number, min: number, max: number): number => {
-  Assert.number(num)
-  Assert.number(min)
-  Assert.number(max)
   return Math.min(Math.max(num, min), max)
 }
