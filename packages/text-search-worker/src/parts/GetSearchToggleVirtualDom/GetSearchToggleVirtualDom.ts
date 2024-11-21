620459a1543f7b505a3908c7abe37596e22d3571
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-21 20:50:27 +0100

    feature: move search toggle into separate file (#259)

---
+++ packages/text-search-worker/src/parts/GetSearchToggleVirtualDom/GetSearchToggleVirtualDom.ts
@@ -0,0 +1,32 @@
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
+import * as SearchFlags from '../SearchFlags/SearchFlags.ts'
+import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+
+export const getSearchToggleVirtualDom = (flags: number): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Button,
+      className: MergeClassNames.mergeClassNames(
+        ClassNames.IconButton,
+        ClassNames.SearchToggleButton,
+        SearchFlags.hasReplaceExpanded(flags) ? ClassNames.SearchToggleButtonExpanded : '',
+      ),
+      title: SearchStrings.toggleReplace(),
+      ariaLabel: SearchStrings.toggleReplace(),
+      ariaExpanded: SearchFlags.hasReplaceExpanded(flags),
+      childCount: 1,
+      'data-command': 'toggleReplace',
+    },
+    {
+      type: VirtualDomElements.Div,
+      className: MergeClassNames.mergeClassNames(
+        ClassNames.MaskIcon,
+        SearchFlags.hasReplaceExpanded(flags) ? ClassNames.MaskIconChevronDown : ClassNames.MaskIconChevronRight,
+      ),
+      childCount: 0,
+    },
+  ]
+}
---
+++ packages/text-search-worker/test/GetSearchToggleVirtualDom.test.ts
@@ -0,0 +1,43 @@
+import { expect, test } from '@jest/globals'
+import * as GetSearchToggleVirtualDom from '../src/parts/GetSearchToggleVirtualDom/GetSearchToggleVirtualDom.ts'
+import * as SearchFlags from '../src/parts/SearchFlags/SearchFlags.ts'
+
+test('getSearchToggleVirtualDom - not expanded', () => {
+  const flags = 0
+  expect(GetSearchToggleVirtualDom.getSearchToggleVirtualDom(flags)).toEqual([
+    {
+      type: 1,
+      className: 'IconButton SearchToggleButton',
+      title: 'Toggle Replace',
+      ariaLabel: 'Toggle Replace',
+      ariaExpanded: false,
+      childCount: 1,
+      'data-command': 'toggleReplace',
+    },
+    {
+      type: 4,
+      className: 'MaskIcon MaskIconChevronRight',
+      childCount: 0,
+    },
+  ])
+})
+
+test('getSearchToggleVirtualDom - expanded', () => {
+  const flags = SearchFlags.ReplaceExpanded
+  expect(GetSearchToggleVirtualDom.getSearchToggleVirtualDom(flags)).toEqual([
+    {
+      type: 1,
+      className: 'IconButton SearchToggleButton SearchToggleButtonExpanded',
+      title: 'Toggle Replace',
+      ariaLabel: 'Toggle Replace',
+      ariaExpanded: true,
+      childCount: 1,
+      'data-command': 'toggleReplace',
+    },
+    {
+      type: 4,
+      className: 'MaskIcon MaskIconChevronDown',
+      childCount: 0,
+    },
+  ])
+})
