a5e31f174323a793873a20a7fc2796395546619b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-21 21:40:45 +0100

    feature: improve search expanded virtual dom (#267)
    * feature: improve search expanded virtual dom
    
    * test

--- packages/text-search-worker/src/parts/GetSearchHeaderDetailsExpandedVirtualDom/GetSearchHeaderDetailsExpandedVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchHeaderDetailsExpandedVirtualDom/GetSearchHeaderDetailsExpandedVirtualDom.ts
@@ -1,8 +1,8 @@
-import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetSearchDetailsToggleVirtualDom from '../GetSearchDetailsToggleVirtualDom/GetSearchDetailsToggleVirtualDom.ts'
+import * as GetSearchFieldVirtualDom from '../GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.ts'
+import * as GetSearchMessageVirtualDom from '../GetSearchMessageVirtualDom/GetSearchMessageVirtualDom.ts'
 import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import * as GetSearchDetailsToggleVirtualDom from '../GetSearchDetailsToggleVirtualDom/GetSearchDetailsToggleVirtualDom.ts'
-import * as GetSearchMessageVirtualDom from '../GetSearchMessageVirtualDom/GetSearchMessageVirtualDom.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
@@ -10,20 +10,14 @@ export const getSearchHeaderDetailsExpandedVirtualDom = (message: string): reado
   return [
     {
       type: VirtualDomElements.Div,
-      className: ClassNames.SearchHeaderDetails,
+      className: 'SearchHeaderDetailsExpanded',
       childCount: 5,
     },
     ...GetSearchDetailsToggleVirtualDom.getSearchDetailsToggleVirtualDom(),
     text(SearchStrings.filesToInclude()),
-    {
-      type: VirtualDomElements.Input,
-      childCount: 0,
-    },
+    ...GetSearchFieldVirtualDom.getSearchFieldVirtualDom('files-to-include-value', 'Include', 'handleIncludeInput', [], []),
     text(SearchStrings.filesToExclude()),
-    {
-      type: VirtualDomElements.Input,
-      childCount: 0,
-    },
+    ...GetSearchFieldVirtualDom.getSearchFieldVirtualDom('files-to-exclude-value', 'Exclude', 'handleExcludeInput', [], []),
     ...GetSearchMessageVirtualDom.getSearchMessageVirtualDom(message),
   ]
 }
