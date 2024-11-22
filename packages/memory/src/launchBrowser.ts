f7dfd1dd79406443a35133e2b0f18d3bcb50945c
Le Vivilet <levivlet@pm.me>
2024-11-22 20:53:44 +0100

    use playwright

--- packages/memory/src/launchBrowser.ts
+++ packages/memory/src/launchBrowser.ts
@@ -1,6 +1,7 @@
+const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
+
 export const launchBrowser = async (headless: boolean, remoteDebuggingPort: string) => {
-  // @ts-ignore
-  const { chromium } = await import('playwright')
+  const { chromium } = await import(playwrightPath)
   const browser = await chromium.launch({
     headless: headless,
     args: [`--remote-debugging-port=${remoteDebuggingPort}`],
