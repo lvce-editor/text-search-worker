f16c7dfe6766e6cfee8c8666b88ab1b2333908a9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-22 22:07:43 +0100

    feature: remove assertions from search strings to reduce code size (#300)

--- packages/text-search-worker/src/parts/SearchStatusMessage/SearchStatusMessage.ts
+++ packages/text-search-worker/src/parts/SearchStatusMessage/SearchStatusMessage.ts
@@ -1,9 +1,6 @@
-import * as Assert from '../Assert/Assert.ts'
 import * as ViewletSearchStrings from '../SearchStrings/SearchStrings.ts'
 
 export const getStatusMessage = (resultCount: number, fileResultCount: number): string => {
-  Assert.number(resultCount)
-  Assert.number(fileResultCount)
   if (resultCount === 0) {
     return ViewletSearchStrings.noResults()
   }
