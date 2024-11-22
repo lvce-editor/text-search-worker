dd3d51c24ed9a66f5b847d10f74090c8f2d9861f
Le Vivilet <levivlet@pm.me>
2024-11-22 19:09:50 +0100

    feature: render search detail headings as heading elements

--- packages/text-search-worker/src/parts/VirtualDomElements/VirtualDomElements.ts
+++ packages/text-search-worker/src/parts/VirtualDomElements/VirtualDomElements.ts
@@ -1,8 +1,9 @@
 export const Button = 1
+export const Del = 21
 export const Div = 4
-export const Span = 8
-export const Text = 12
+export const H4 = 24
 export const Img = 17
 export const Ins = 20
-export const Del = 21
+export const Span = 8
+export const Text = 12
 export const TextArea = 62
