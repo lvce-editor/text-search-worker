fb95c8438ae957457edff30324890a27aba9c62f
Le Vivilet <levivlet@pm.me>
2024-11-23 21:08:24 +0100

    feature: remove console log

--- packages/text-search-worker/src/parts/FocusPreviousInput/FocusPreviousInput.ts
+++ packages/text-search-worker/src/parts/FocusPreviousInput/FocusPreviousInput.ts
@@ -4,7 +4,6 @@ import type { SearchState } from '../SearchState/SearchState.ts'
 
 export const focusPreviousInput = (state: SearchState): SearchState => {
   const previousFocus = GetPreviousFocus.getPreviousFocus(state.focus, state.flags)
-  console.log('prev', state.focus, previousFocus)
   return {
     ...state,
     focus: previousFocus,
