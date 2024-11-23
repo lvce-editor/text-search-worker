3359b6f067b1f8750ddf3c3e369d44ed7f6f5385
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-23 20:38:39 +0100

    feature: add getNextFocus test (#314)

---
+++ packages/text-search-worker/test/GetNextFocus.test.ts
@@ -0,0 +1,82 @@
+import { expect, test } from '@jest/globals'
+import * as GetNextFocus from '../src/parts/GetNextFocus/GetNextFocus.ts'
+import * as WhenExpression from '../src/parts/WhenExpression/WhenExpression.ts'
+import * as SearchFlags from '../src/parts/SearchFlags/SearchFlags.ts'
+
+test('getNextFocus - from search input with replace expanded', () => {
+  const focus = WhenExpression.FocusSearchInput
+  const flags = SearchFlags.ReplaceExpanded
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchReplaceInput)
+})
+
+test('getNextFocus - from search input without replace expanded', () => {
+  const focus = WhenExpression.FocusSearchInput
+  const flags = 0
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchMatchCase)
+})
+
+test('getNextFocus - from replace input', () => {
+  const focus = WhenExpression.FocusSearchReplaceInput
+  const flags = SearchFlags.ReplaceExpanded
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchMatchCase)
+})
+
+test('getNextFocus - from match case', () => {
+  const focus = WhenExpression.FocusSearchMatchCase
+  const flags = 0
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchWholeWord)
+})
+
+test('getNextFocus - from whole word', () => {
+  const focus = WhenExpression.FocusSearchWholeWord
+  const flags = 0
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchRegex)
+})
+
+test('getNextFocus - from regex with replace expanded', () => {
+  const focus = WhenExpression.FocusSearchRegex
+  const flags = SearchFlags.ReplaceExpanded
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchPreserveCase)
+})
+
+test('getNextFocus - from regex without replace expanded', () => {
+  const focus = WhenExpression.FocusSearchRegex
+  const flags = 0
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusToggleDetails)
+})
+
+test('getNextFocus - from preserve case', () => {
+  const focus = WhenExpression.FocusSearchPreserveCase
+  const flags = SearchFlags.ReplaceExpanded
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchReplaceAll)
+})
+
+test('getNextFocus - from replace all', () => {
+  const focus = WhenExpression.FocusSearchReplaceAll
+  const flags = SearchFlags.ReplaceExpanded
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusToggleDetails)
+})
+
+test('getNextFocus - from include input', () => {
+  const focus = WhenExpression.FocusSearchIncludeInput
+  const flags = 0
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchOpenEditors)
+})
+
+test('getNextFocus - from open editors', () => {
+  const focus = WhenExpression.FocusSearchOpenEditors
+  const flags = 0
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusSearchExcludeInput)
+})
+
+test('getNextFocus - from exclude input', () => {
+  const focus = WhenExpression.FocusSearchExcludeInput
+  const flags = 0
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(WhenExpression.FocusIgnoreFiles)
+})
+
+test('getNextFocus - default case returns same focus', () => {
+  const focus = -1
+  const flags = 0
+  expect(GetNextFocus.getNextFocus(focus, flags)).toBe(focus)
+})
