b44cdf86fd0405d73ef5628209cfa55e5582ba08
Le Vivilet <levivlet@pm.me>
2024-11-23 22:11:04 +0100

    tsc

--- packages/text-search-worker/src/parts/Location/Location.ts
+++ packages/text-search-worker/src/parts/Location/Location.ts
@@ -1,21 +1,3 @@
-import * as RendererProcess from '../RendererProcess/RendererProcess.js'
-
-export const getPathName = () => {
-  return RendererProcess.invoke(/* Location.getPathName */ 'Location.getPathName')
-}
-
-export const getHref = () => {
-  return RendererProcess.invoke(/* Location.getHref */ 'Location.getHref')
-}
-
-export const setPathName = (pathName) => {
-  return RendererProcess.invoke(/* Location.setPathName */ 'Location.setPathName', /* pathName */ pathName)
-}
-
-export const hydrate = () => {
-  return RendererProcess.invoke(/* Location.hydrate */ 'Location.hydrate')
-}
-
 export const getOrigin = () => {
   return location.origin
 }
