fa508b844f800055b60738635e90495ecc254714
Le Vivilet <levivlet@pm.me>
2024-11-23 21:03:46 +0100

    feature: make it possible to toggle openEditors and ignoreFiles buttons

--- packages/text-search-worker/src/parts/GetSearchHeaderDetailsExpandedVirtualDom/GetSearchHeaderDetailsExpandedVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchHeaderDetailsExpandedVirtualDom/GetSearchHeaderDetailsExpandedVirtualDom.ts
@@ -2,18 +2,19 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetSearchDetailsToggleVirtualDom from '../GetSearchDetailsToggleVirtualDom/GetSearchDetailsToggleVirtualDom.ts'
 import * as GetSearchFieldVirtualDom from '../GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.ts'
 import * as GetSearchMessageVirtualDom from '../GetSearchMessageVirtualDom/GetSearchMessageVirtualDom.ts'
+import { OpenEditors, UseIgnoreFiles } from '../SearchFlags/SearchFlags.ts'
 import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
-export const getSearchHeaderDetailsExpandedVirtualDom = (message: string): readonly VirtualDomNode[] => {
+export const getSearchHeaderDetailsExpandedVirtualDom = (flags: number, message: string): readonly VirtualDomNode[] => {
   const includeButtons = [
     {
       icon: ClassNames.MaskIconBook,
       title: SearchStrings.searchOnlyOpenEditors(),
       command: 'searchOnlyOpenEditors',
-      checked: false,
+      checked: Boolean(flags & OpenEditors),
     },
   ]
 
@@ -22,7 +23,7 @@ export const getSearchHeaderDetailsExpandedVirtualDom = (message: string): reado
       icon: ClassNames.MaskIconExclude,
       title: SearchStrings.useExcludeSettings(),
       command: 'toggleUseExcludeSettings',
-      checked: false,
+      checked: Boolean(flags & UseIgnoreFiles),
     },
   ]
 
