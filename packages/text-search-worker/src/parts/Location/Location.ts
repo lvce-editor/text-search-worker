58ecf89ea7f495327f9ce9e53b384f6780425000
Le Vivilet <levivlet@pm.me>
2024-11-23 22:19:01 +0100

    remove unused code

--- packages/text-search-worker/src/parts/Location/Location.ts
+++ packages/text-search-worker/src/parts/Location/Location.ts
@@ -1,11 +1,3 @@
-export const getOrigin = (): string => {
-  return location.origin
-}
-
 export const getHost = (): string => {
   return location.host
-}
-
-export const getProtocol = (): string => {
-  return location.protocol
 }
