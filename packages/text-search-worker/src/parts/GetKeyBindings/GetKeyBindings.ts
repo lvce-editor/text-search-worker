d4d6a5836eb4fd968fbdb46475aba035a4e1bdfe
Le Vivilet <levivlet@pm.me>
2024-11-23 17:28:44 +0100

    import

--- packages/text-search-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
+++ packages/text-search-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
@@ -1,7 +1,7 @@
 import type { KeyBinding } from '../KeyBinding/KeyBinding.ts'
 import * as KeyCode from '../KeyCode/KeyCode.ts'
 import * as KeyModifier from '../KeyModifier/KeyModifier.ts'
-import * as WhenExpression from '../WhenExpression/WhenExpression.js'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
 export const getKeyBindings = (): readonly KeyBinding[] => {
   return [
