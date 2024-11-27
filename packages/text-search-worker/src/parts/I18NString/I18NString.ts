5085bbda8455b9531759db35264d718123ce227b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-11-27 20:19:17 +0100

    feature: add i18n dependency (#337)

--- packages/text-search-worker/src/parts/I18NString/I18NString.ts
+++ packages/text-search-worker/src/parts/I18NString/I18NString.ts
@@ -1,14 +1,1 @@
-const emptyObject = {}
-
-const RE_PLACEHOLDER = /\{(PH\d+)\}/g
-
-export const i18nString = (key: string, placeholders = emptyObject): string => {
-  if (placeholders === emptyObject) {
-    return key
-  }
-  const replacer = (match: any, rest: any): any => {
-    // @ts-ignore
-    return placeholders[rest]
-  }
-  return key.replaceAll(RE_PLACEHOLDER, replacer)
-}
+export * from '@lvce-editor/i18n'
