3c6db535ce22bd7ead296e1173f6ff4e382ee4dd
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-01 14:46:05 +0100

    feature: remove unused ipc code (#358)

--- packages/text-search-worker/src/parts/GetData/GetData.ts
+++
@@ -1,4 +0,0 @@
-export const getData = (event: MessageEvent): any => {
-  const { data } = event
-  return data
-}
--- packages/text-search-worker/src/parts/LaunchSearchProcess/LaunchSearchProcess.ts
+++ packages/text-search-worker/src/parts/LaunchSearchProcess/LaunchSearchProcess.ts
@@ -1,14 +1,17 @@
-import * as IpcParent from '../IpcParent/IpcParent.ts'
-import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
+// import * as IpcParent from '../IpcParent/IpcParent.ts'
+// import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 
 export const launchSearchProcess = async (): Promise<any> => {
-  const ipc = await IpcParent.create({
-    method: IpcParentType.NodeAlternate,
-    type: 'search-process',
-    name: 'Search Process',
-    initialCommand: 'HandleMessagePortForSearchProcess.handleMessagePortForSearchProcess',
-  })
-  // TODO
-  // HandleIpc.handleIpc(ipc)
-  return ipc
+  // const ipc = await IpcParent.create({
+  //   method: IpcParentType.NodeAlternate,
+  //   type: 'search-process',
+  //   name: 'Search Process',
+  //   initialCommand: 'HandleMessagePortForSearchProcess.handleMessagePortForSearchProcess',
+  // })
+  // // TODO
+  // // HandleIpc.handleIpc(ipc)
+  // return ipc
+  // @ts-ignore
+  await new Promise(null)
+  return {}
 }
