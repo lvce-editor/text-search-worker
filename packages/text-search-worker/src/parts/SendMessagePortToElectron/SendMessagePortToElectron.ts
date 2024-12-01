76f765e8e95c427a1e4fe8b7a42b249f6ba692a0
Le Vivilet <levivlet@pm.me>
2024-12-01 14:08:49 +0100

    remove unused code

--- packages/text-search-worker/src/parts/SendMessagePortToElectron/SendMessagePortToElectron.ts
+++
@@ -1,8 +0,0 @@
-import * as Assert from '../Assert/Assert.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
-
-export const sendMessagePortToElectron = async (port: MessagePort, initialCommand: string, ipcId: any): Promise<void> => {
-  Assert.object(port)
-  Assert.string(initialCommand)
-  await ParentRpc.invokeAndTransfer(initialCommand, port, ipcId)
-}
