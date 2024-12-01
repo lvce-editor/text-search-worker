900aa0f1f6da31dff8129599761e9ad8dfe9f8e1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-01 09:19:43 +0100

    feature: add integration test package (#339)

---
+++ packages/test-integration-util/src/setup.js
@@ -0,0 +1,46 @@
+import { createWorker } from './createWorker.js'
+
+const workerPath = new URL('../../../.tmp/dist/dist/textSearchWorkerMain.js', import.meta.url).toString()
+
+const getResult = (method, ...params) => {
+  return null
+}
+
+const handleMessage = (event) => {
+  const { data, target } = event
+  if (data.id) {
+    const result = getResult(data.method, ...data.params)
+    target.postMessage({
+      jsonrpc: '2.0',
+      id: data.id,
+      result,
+    })
+  }
+}
+
+const createWrappedRpc = (rpc) => {
+  return rpc
+}
+
+export const setup = async () => {
+  const commandMap = {
+    'SendMessagePortToRendererProcess.sendMessagePortToRendererProcess'(port) {
+      port.addEventListener('message', handleMessage)
+      port.postMessage('ready')
+    },
+    'SendMessagePortToSyntaxHighlightingWorker.sendMessagePortToSyntaxHighlightingWorker'(port) {
+      port.addEventListener('message', handleMessage)
+      port.postMessage('ready')
+    },
+    'SendMessagePortToExtensionHostWorker.sendMessagePortToExtensionHostWorker'(port) {
+      port.addEventListener('message', handleMessage)
+      port.postMessage('ready')
+    },
+  }
+  const rpc = await createWorker(workerPath, commandMap)
+  const syntaxHighlightingEnabled = true
+  const syncIncremental = true
+  await rpc.invoke('Initialize.initialize', syntaxHighlightingEnabled, syncIncremental)
+  const wrapped = createWrappedRpc(rpc)
+  return wrapped
+}
