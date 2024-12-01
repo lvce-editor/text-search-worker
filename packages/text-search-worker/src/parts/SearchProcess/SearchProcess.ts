5b09b872c8a2e80124c00c64dbb8625543598dbe
Le Vivilet <levivlet@pm.me>
2024-12-01 14:27:32 +0100

    fix double rpc

--- packages/text-search-worker/src/parts/SearchProcess/SearchProcess.ts
+++ packages/text-search-worker/src/parts/SearchProcess/SearchProcess.ts
@@ -1,23 +1,22 @@
-import * as JsonRpc from '../JsonRpc/JsonRpc.ts'
 import * as LaunchSearchProcess from '../LaunchSearchProcess/LaunchSearchProcess.ts'
 
 interface State {
-  ipc: any
+  rpc: any
 }
 
 const state: State = {
-  ipc: undefined,
+  rpc: undefined,
 }
 
 export const getOrCreate = (): Promise<any> => {
-  if (!state.ipc) {
+  if (!state.rpc) {
     // @ts-ignore
-    state.ipc = LaunchSearchProcess.launchSearchProcess()
+    state.rpc = LaunchSearchProcess.launchSearchProcess()
   }
-  return state.ipc
+  return state.rpc
 }
 
 export const invoke = async (method: string, ...params: any[]): Promise<any> => {
-  const ipc = await getOrCreate()
-  return JsonRpc.invoke(ipc, method, ...params)
+  const rpc = await getOrCreate()
+  return rpc.invoke(method, ...params)
 }
