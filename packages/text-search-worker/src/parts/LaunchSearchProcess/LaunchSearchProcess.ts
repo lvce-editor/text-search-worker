14d7ac20856b2073afca879daf81485d76c35195
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-01 14:34:46 +0100

    Discard changes to packages/text-search-worker/src/parts/LaunchSearchProcess/LaunchSearchProcess.ts

--- packages/text-search-worker/src/parts/LaunchSearchProcess/LaunchSearchProcess.ts
+++ packages/text-search-worker/src/parts/LaunchSearchProcess/LaunchSearchProcess.ts
@@ -1,15 +1,14 @@
-import { WebSocketRpcParent } from '@lvce-editor/rpc'
-import * as GetWebSocketUrl from '../GetWebSocketUrl/GetWebSocketUrl.ts'
-import * as Location from '../Location/Location.ts'
+import * as IpcParent from '../IpcParent/IpcParent.ts'
+import * as IpcParentType from '../IpcParentType/IpcParentType.ts'
 
 export const launchSearchProcess = async (): Promise<any> => {
-  const host = Location.getHost()
-  const wsUrl = GetWebSocketUrl.getWebSocketUrl('search-process', host)
-  const webSocket = new WebSocket(wsUrl)
-  const rpc = await WebSocketRpcParent.create({
-    webSocket,
-    commandMap: {},
+  const ipc = await IpcParent.create({
+    method: IpcParentType.NodeAlternate,
+    type: 'search-process',
+    name: 'Search Process',
+    initialCommand: 'HandleMessagePortForSearchProcess.handleMessagePortForSearchProcess',
   })
-  console.log({ rpc })
-  return rpc
+  // TODO
+  // HandleIpc.handleIpc(ipc)
+  return ipc
 }
