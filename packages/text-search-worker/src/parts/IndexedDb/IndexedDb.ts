31f68e3187ec462e30c6509e8b4638f72d82c4b7
Le Vivilet <levivlet@pm.me>
2024-12-15 20:49:26 +0100

    feature: move handleDb into separate file

--- packages/text-search-worker/src/parts/IndexedDb/IndexedDb.ts
+++ packages/text-search-worker/src/parts/IndexedDb/IndexedDb.ts
@@ -1,22 +1,7 @@
-// TODO high memory usage in idb because of transactionDoneMap
-
-import { openDB } from '../Idb/Idb.ts'
-import { state } from '../IndexedDbState/IndexedDbState.ts'
-
-const getHandleDb = async (): Promise<any> => {
-  // @ts-ignore
-  const db = await openDB('handle', state.dbVersion, {
-    async upgrade(db: any) {
-      if (!db.objectStoreNames.contains('file-handles-store')) {
-        await db.createObjectStore('file-handles-store', {})
-      }
-    },
-  })
-  return db
-}
+import * as GetHandleDb from '../GetHandleDb/GetHandleDb.ts'
 
 export const getHandle = async (uri: string): Promise<any> => {
-  const handleDb = await getHandleDb()
+  const handleDb = await GetHandleDb.getHandleDb()
   const handle = await handleDb.get('file-handles-store', uri)
   return handle
 }
