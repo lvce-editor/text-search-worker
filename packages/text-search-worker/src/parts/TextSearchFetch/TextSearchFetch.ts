bf222b7a433a83a8f0bfbc1b31c373263d461780
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-18 00:29:06 +0100

    feature: add renderer worker api types (#409)

--- packages/text-search-worker/src/parts/TextSearchFetch/TextSearchFetch.ts
+++ packages/text-search-worker/src/parts/TextSearchFetch/TextSearchFetch.ts
@@ -2,5 +2,5 @@ import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 
 export const textSearch = async (scheme: string, root: string, query: string, options: any, assetDir: string): Promise<readonly SearchResult[]> => {
-  return ParentRpc.invoke('ExtensionHostTextSearch.textSearchHtml', scheme, root, query, options, assetDir)
+  return ParentRpc.invoke('ExtensionHostTextSearch.textSearchFetch', scheme, root, query, options, assetDir)
 }
