130b586f4768ade50adc0b14c19fd268fe639f1d
Le Vivilet <levivlet@pm.me>
2024-12-18 00:14:22 +0100

    feature: move indexeddb code to extension host

--- packages/text-search-worker/src/parts/TextSearchFetch/TextSearchFetch.ts
+++ packages/text-search-worker/src/parts/TextSearchFetch/TextSearchFetch.ts
@@ -1,22 +1,6 @@
-import * as Assert from '../Assert/Assert.ts'
-import * as GetJson from '../GetJson/GetJson.ts'
-import * as GetText from '../GetText/GetText.ts'
-import * as TextSearchInText from '../TextSearchInText/TextSearchInText.ts'
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import type { SearchResult } from '../SearchResult/SearchResult.ts'
 
-export const textSearch = async (scheme: string, root: string, query: string, options: any, assetDir: string): Promise<any> => {
-  Assert.string(scheme)
-  Assert.string(root)
-  Assert.string(query)
-  const fetchUri = `${assetDir}/config/fileMap.json`
-  const fileList = await GetJson.getJson(fetchUri)
-  const allResults: any[] = []
-  const relativeRoot = root.slice('fetch://'.length)
-  for (const uri of fileList) {
-    const fetchUri = `${assetDir}${uri}`
-    const content = await GetText.getText(fetchUri)
-    const relativeUri = uri.slice(relativeRoot.length + 1)
-    const results = TextSearchInText.textSearchInText(relativeUri, content, query)
-    allResults.push(...results)
-  }
-  return allResults
+export const textSearch = async (scheme: string, root: string, query: string, options: any, assetDir: string): Promise<readonly SearchResult[]> => {
+  return ParentRpc.invoke('ExtensionHostTextSearch.textSearchHtml', scheme, root, query, options, assetDir)
 }
--- packages/text-search-worker/src/parts/TextSearchHtml/TextSearchHtml.ts
+++ packages/text-search-worker/src/parts/TextSearchHtml/TextSearchHtml.ts
@@ -1,41 +1,6 @@
-import { FileNotFoundError } from '../FileNotFoundError/FileNotFoundError.ts'
-import * as Assert from '../Assert/Assert.ts'
-import * as FileHandleType from '../FileHandleType/FileHandleType.ts'
-import * as FileSystemHtml from '../FileSystemHtml/FileSystemHtml.ts'
-import * as GetDirectoryHandle from '../GetDirectoryHandle/GetDirectoryHandle.ts'
+import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
-import * as TextSearchInFile from '../TextSearchInFile/TextSearchInFile.ts'
-
-const textSearchRecursively = async (all: SearchResult[], parent: string, handle: FileSystemHandle, query: string): Promise<void> => {
-  const childHandles: readonly FileSystemHandle[] = await FileSystemHtml.getChildHandles(handle)
-  const promises: Promise<void>[] = []
-  for (const childHandle of childHandles) {
-    const absolutePath = parent + '/' + childHandle.name
-    switch (childHandle.kind) {
-      case FileHandleType.Directory:
-        promises.push(textSearchRecursively(all, absolutePath, childHandle, query))
-        break
-      case FileHandleType.File:
-        // @ts-ignore
-        promises.push(TextSearchInFile.textSearchInFile(all, childHandle, absolutePath, query))
-        break
-      default:
-        break
-    }
-  }
-  await Promise.all(promises)
-}
 
 export const textSearch = async (scheme: string, root: string, query: string): Promise<readonly SearchResult[]> => {
-  Assert.string(scheme)
-  Assert.string(root)
-  Assert.string(query)
-  const relativeRoot = root.slice('html://'.length)
-  const handle = await GetDirectoryHandle.getDirectoryHandle(relativeRoot)
-  if (!handle) {
-    throw new FileNotFoundError(`Folder not found: ${relativeRoot}`)
-  }
-  const all: SearchResult[] = []
-  await textSearchRecursively(all, '', handle, query)
-  return all
+  return ParentRpc.invoke('ExtensionHostTextSearch.textSearchHtml', scheme, root, query)
 }
