9f85590836bd08dbc9870bdf7b3397248a7dc8f6
Le Vivilet <levivlet@pm.me>
2024-12-31 14:45:53 +0100

    feature: add no search provider found e2e test

---
+++ packages/e2e/src/search.no-provider-found.js
@@ -0,0 +1,17 @@
+export const name = 'search.no-provider-found'
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Main, Locator, Editor, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+
+  // act
+  await Search.setValue('Doc')
+
+  // assert
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('Error: Failed to execute text search provider: No text search provider for memfs found')
+}
