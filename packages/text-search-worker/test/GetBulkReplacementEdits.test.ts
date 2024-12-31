db639142e387fde649e42ffe624a92bce6d531af
Le Vivilet <levivlet@pm.me>
2024-12-31 16:07:55 +0100

    feature: update bulk replacement function to better work with different filenames

--- packages/text-search-worker/test/GetBulkReplacementEdits.test.ts
+++ packages/text-search-worker/test/GetBulkReplacementEdits.test.ts
@@ -41,3 +41,45 @@ test('getBulkReplacementEdits', () => {
   ]
   expect(GetBulkReplacementEdits.getBulkReplacementEdits(workspacePath, matches)).toEqual({ files: ['/test/file.txt'], ranges: [4, 0, 0, 0, 1] })
 })
+
+test('getBulkReplacementEdits - different file paths', () => {
+  const workspacePath = 'memfs:///test'
+  const matches: readonly DisplaySearchResult[] = [
+    {
+      type: TextSearchResultType.File,
+      text: 'file.txt',
+      lineNumber: 0,
+      matchLength: 0,
+      matchStart: 0,
+      title: '',
+      depth: 0,
+      focused: false,
+      icon: '',
+      matchCount: 0,
+      posInSet: 0,
+      replacement: '',
+      setSize: 0,
+      top: 0,
+    },
+    {
+      type: TextSearchResultType.Match,
+      title: '',
+      lineNumber: 1,
+      matchLength: 1,
+      matchStart: 0,
+      depth: 0,
+      focused: false,
+      icon: '',
+      matchCount: 0,
+      posInSet: 0,
+      replacement: '',
+      setSize: 0,
+      top: 0,
+      text: '',
+    },
+  ]
+  expect(GetBulkReplacementEdits.getBulkReplacementEdits(workspacePath, matches)).toEqual({
+    files: ['memfs:///test/file.txt'],
+    ranges: [4, 0, 0, 0, 1],
+  })
+})
