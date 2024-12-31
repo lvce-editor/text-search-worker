9dc67580445705c59ec238dad3578d7586d66a79
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-31 15:19:43 +0100

    feature: add one result e2e test (#445)

---
+++ packages/e2e/src/search.one-result.js
@@ -0,0 +1,19 @@
+export const name = 'search.one-result'
+
+export const skip = true
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+
+  // act
+  await Search.setValue('ab')
+
+  // assert
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result')
+}
