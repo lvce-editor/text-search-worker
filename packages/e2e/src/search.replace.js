722292892484201350c7a1cd266b882db23fa05f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2024-12-31 16:01:45 +0100

    feature: add replace test (#448)

--- packages/e2e/src/search.replace.js
+++ packages/e2e/src/search.replace.js
@@ -1,5 +1,22 @@
+export const name = 'search.replace'
+
 export const skip = true
 
-export const test = async ({ FileSystem, Workspace, Main, Locator, Editor, expect }) => {
-  // TODO add search replace test
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('ab')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result in 1 file')
+
+  // act
+  await Search.toggleReplace()
+  await Search.setReplaceValue('d')
+  await Search.replaceAll()
+
+  // assert
 }
