7c85f8aa8d6e2d40a41dc829b0204d4aec178889
Le Vivilet <levivlet@pm.me>
2024-12-31 15:28:19 +0100

    feature: add two results in one file test

---
+++ packages/e2e/src/search.two-results-in-one-file.js
@@ -0,0 +1,23 @@
+export const name = 'search.two-results-in-one-file'
+
+export const skip = true
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(
+    `${tmpDir}/test.css`,
+    `abc
+abd`,
+  )
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+
+  // act
+  await Search.setValue('ab')
+
+  // assert
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('2 results in 1 file')
+}
