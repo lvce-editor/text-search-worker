54df70841ee9be95404bb317b360d75d143ebef2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 15:25:28 +0100

    Discard changes to packages/text-search-worker/src/parts/SearchFlags/SearchFlags.ts

--- packages/text-search-worker/src/parts/SearchFlags/SearchFlags.ts
+++ packages/text-search-worker/src/parts/SearchFlags/SearchFlags.ts
@@ -6,3 +6,67 @@ export const MatchCase = 1 << 4 // 16
 export const DetailsExpanded = 1 << 5 // 32
 export const OpenEditors = 1 << 6 // 64
 export const UseIgnoreFiles = 1 << 7 // 128
+
+export const hasPreserveCase = (flags: number): boolean => {
+  return (flags & PreserveCase) === PreserveCase
+}
+
+export const hasUseRegularExpression = (flags: number): boolean => {
+  return (flags & UseRegularExpression) === UseRegularExpression
+}
+
+export const hasReplaceExpanded = (flags: number): boolean => {
+  return (flags & ReplaceExpanded) === ReplaceExpanded
+}
+
+export const hasMatchWholeWord = (flags: number): boolean => {
+  return (flags & MatchWholeWord) === MatchWholeWord
+}
+
+export const hasMatchCase = (flags: number): boolean => {
+  return (flags & MatchCase) === MatchCase
+}
+
+export const hasDetailsExpanded = (flags: number): boolean => {
+  return (flags & DetailsExpanded) === DetailsExpanded
+}
+
+export const hasOpenEditors = (flags: number): boolean => {
+  return (flags & OpenEditors) === OpenEditors
+}
+
+export const hasUseIgnoreFiles = (flags: number): boolean => {
+  return (flags & UseIgnoreFiles) === UseIgnoreFiles
+}
+
+export const togglePreserveCase = (flags: number): number => {
+  return flags ^ PreserveCase
+}
+
+export const toggleUseRegularExpression = (flags: number): number => {
+  return flags ^ UseRegularExpression
+}
+
+export const toggleReplaceExpanded = (flags: number): number => {
+  return flags ^ ReplaceExpanded
+}
+
+export const toggleMatchWholeWord = (flags: number): number => {
+  return flags ^ MatchWholeWord
+}
+
+export const toggleMatchCase = (flags: number): number => {
+  return flags ^ MatchCase
+}
+
+export const toggleDetailsExpanded = (flags: number): number => {
+  return flags ^ DetailsExpanded
+}
+
+export const toggleOpenEditors = (flags: number): number => {
+  return flags ^ OpenEditors
+}
+
+export const toggleUseIgnoreFiles = (flags: number): number => {
+  return flags ^ UseIgnoreFiles
+}
