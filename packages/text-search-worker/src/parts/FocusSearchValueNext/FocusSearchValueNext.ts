ef2fc369f5a624f23e142eaa8bcf85ae1031daac
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 15:24:04 +0100

    Discard changes to packages/text-search-worker/src/parts/FocusSearchValueNext/FocusSearchValueNext.ts

--- packages/text-search-worker/src/parts/FocusSearchValueNext/FocusSearchValueNext.ts
+++ packages/text-search-worker/src/parts/FocusSearchValueNext/FocusSearchValueNext.ts
@@ -4,7 +4,7 @@ import * as FocusReplaceValue from '../FocusReplaceValue/FocusReplaceValue.ts'
 import * as SearchFlags from '../SearchFlags/SearchFlags.ts'
 
 export const focusSearchValueNext = (state: SearchState): SearchState => {
-  if (state.flags & SearchFlags.ReplaceExpanded) {
+  if (SearchFlags.hasReplaceExpanded(state.flags)) {
     return FocusReplaceValue.focusReplaceValue(state)
   }
   return FocusMatchCase.focusMatchCase(state)
