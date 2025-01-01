80d0ea98b52ec8cb10606a707e0d22901faee08a
Le Vivilet <levivlet@pm.me>
2025-01-01 22:27:18 +0100

    feature: add addCollapsedPath helper function

--- packages/text-search-worker/src/parts/SelectIndexFile/SelectIndexFile.ts
+++ packages/text-search-worker/src/parts/SelectIndexFile/SelectIndexFile.ts
@@ -1,16 +1,13 @@
 import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
-import * as Assert from '../Assert/Assert.ts'
+import * as AddCollapsedPath from '../AddCollapsedPath/AddCollapsedPath.ts'
 import * as GetFilteredResults from '../GetFilteredResults/GetFilteredResults.ts'
 import * as Workspace from '../Workspace/Workspace.ts'
 
 export const selectIndexFile = (state: SearchState, searchResult: DisplaySearchResult, index: number): SearchState => {
   const { collapsedPaths, items, maxLineY } = state
   const path = Workspace.getAbsolutePath(searchResult.text)
-  Assert.string(path)
-
-  const pathWithDot = `.${path}`
-  const newCollapsedPaths = [...collapsedPaths, path, pathWithDot]
+  const newCollapsedPaths = AddCollapsedPath.addCollapsedPath(collapsedPaths, path)
   const filteredResults = GetFilteredResults.getFilteredResults(items, collapsedPaths)
   const newMaxLineY = Math.min(maxLineY, filteredResults.length)
   return {
