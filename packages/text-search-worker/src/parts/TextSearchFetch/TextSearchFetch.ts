19c6f4dbe69944c0c5548d5381b784e2412ad24c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 14:43:05 +0100

    feature: update textsearchOptions (#474)

--- packages/text-search-worker/src/parts/TextSearchFetch/TextSearchFetch.ts
+++ packages/text-search-worker/src/parts/TextSearchFetch/TextSearchFetch.ts
@@ -1,6 +1,13 @@
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
+import type { TextSearchOptions } from '../TextSearchOptions/TextSearchOptions.ts'
 import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
-export const textSearch = async (scheme: string, root: string, query: string, options: any, assetDir: string): Promise<readonly SearchResult[]> => {
+export const textSearch = async (
+  scheme: string,
+  root: string,
+  query: string,
+  options: TextSearchOptions,
+  assetDir: string,
+): Promise<readonly SearchResult[]> => {
   return ParentRpc.invoke('ExtensionHostTextSearch.textSearchFetch', scheme, root, query, options, assetDir)
 }
