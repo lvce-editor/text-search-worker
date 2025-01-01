dd5f4db1c4579e08abd13f4f5582c71642ff4a5b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 22:11:45 +0100

    feature: enable two results in one file test (#504)

--- packages/e2e/src/search.two-results-in-one-file.js
+++ packages/e2e/src/search.two-results-in-one-file.js
@@ -1,7 +1,5 @@
 export const name = 'search.two-results-in-one-file'
 
-export const skip = true
-
 export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
