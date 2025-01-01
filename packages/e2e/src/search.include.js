80da341279b8f45efc337e9ef6ee31afdfd21a39
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 14:49:48 +0100

    feature: add include e2e test (#475)

---
+++ packages/e2e/src/search.include.js
@@ -0,0 +1,22 @@
+export const name = 'search.include'
+
+export const skip = false
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('ab')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result in 1 file')
+
+  // act
+  await Search.setIncludeValue('other.css')
+
+  // assert
+  await expect(message).toHaveText('No results found.')
+}
