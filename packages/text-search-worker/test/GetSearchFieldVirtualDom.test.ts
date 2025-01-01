592e67774509f1456502a34eb8cd00763d413ce5
Le Vivilet <levivlet@pm.me>
2025-01-01 19:38:19 +0100

    feature: add error classname to input field in case of input error

--- packages/text-search-worker/test/GetSearchFieldVirtualDom.test.ts
+++ packages/text-search-worker/test/GetSearchFieldVirtualDom.test.ts
@@ -34,3 +34,38 @@ test('NotReadableError', () => {
     },
   ])
 })
+
+test('getSearchFieldVirtualDom - with error', () => {
+  const name = ''
+  const placeholder = ''
+  const onInput = 'handleInput'
+  const insideButtons: any = []
+  const outsideButtons: any = []
+  const onFocus = ''
+  const hasError = true
+  expect(GetSearchFieldVirtualDom.getSearchFieldVirtualDom(name, placeholder, onInput, insideButtons, outsideButtons, onFocus, hasError)).toEqual([
+    {
+      childCount: 2,
+      className: 'SearchField SearchFieldError',
+      role: 'none',
+      type: 4,
+    },
+    {
+      autocapitalize: 'off',
+      autocorrect: 'off',
+      childCount: 0,
+      className: 'MultilineInputBox',
+      name: '',
+      onFocus: '',
+      onInput: 'handleInput',
+      placeholder: '',
+      spellcheck: false,
+      type: 62,
+    },
+    {
+      childCount: 0,
+      className: 'SearchFieldButtons',
+      type: 4,
+    },
+  ])
+})
