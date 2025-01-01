a665d3801e803f82e81e57f8458001c8d694e808
Le Vivilet <levivlet@pm.me>
2025-01-01 12:43:28 +0100

    feature: use rpc registry

--- packages/text-search-worker/src/parts/Listen/Listen.ts
+++ packages/text-search-worker/src/parts/Listen/Listen.ts
@@ -1,10 +1,11 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
+import * as RpcId from '../RpcId/RpcId.ts'
+import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
 
 export const listen = async (): Promise<void> => {
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
-  ParentRpc.setRpc(rpc)
+  RpcRegistry.set(RpcId.RendererWorker, rpc)
 }
