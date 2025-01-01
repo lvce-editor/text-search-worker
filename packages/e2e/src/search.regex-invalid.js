c35cbf2e4caa87615d202b9e6858b27f5a64c7c3
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 18:36:18 +0100

    feature: add search invalid regex test (#489)

---
+++ packages/e2e/src/search.regex-invalid.js
@@ -0,0 +1,22 @@
+export const name = 'search.regex-invalid'
+
+export const skip = 1
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `Abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('(ab')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('No results found')
+
+  // act
+  await Search.toggleUseRegularExpression()
+
+  // assert
+  await expect(message).toHaveText('1 result in 1 file')
+}
