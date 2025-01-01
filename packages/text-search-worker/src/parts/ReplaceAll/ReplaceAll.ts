f1f1177f6a87fb2882e092a397a5ccc3b3d367d5
Le Vivilet <levivlet@pm.me>
2025-01-01 12:35:47 +0100

    test

--- packages/text-search-worker/src/parts/ReplaceAll/ReplaceAll.ts
+++ packages/text-search-worker/src/parts/ReplaceAll/ReplaceAll.ts
@@ -1,7 +1,6 @@
-import type { SearchState } from '../SearchState/SearchState.ts'
-import * as GetBulkReplacementEdits from '../GetBulkReplacementEdits/GetBulkReplacementEdits.ts'
 import * as GetReplaceElements from '../GetReplaceElements/GetReplaceElements.ts'
 import * as Rpc from '../ParentRpc/ParentRpc.ts'
+import type { SearchState } from '../SearchState/SearchState.ts'
 
 export const replaceAll = async (state: SearchState): Promise<SearchState> => {
   const { workspacePath, items, replacement } = state
