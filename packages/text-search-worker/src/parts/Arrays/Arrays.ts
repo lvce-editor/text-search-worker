4e6329406e7aeac17b9ef47a63554af933c8051f
Le Vivilet <levivlet@pm.me>
2025-01-01 12:38:22 +0100

    tsc

--- packages/text-search-worker/src/parts/Arrays/Arrays.ts
+++ packages/text-search-worker/src/parts/Arrays/Arrays.ts
@@ -1,19 +1,5 @@
 // based on https://github.com/microsoft/vscode/blob/c0769274fa136b45799edeccc0d0a2f645b75caf/src/vs/base/common/arrays.ts#L625 (License MIT)
 
-const insertInto = <T>(array: T[], start: number, newItems: readonly T[]): void => {
-  const originalLength = array.length
-  const newItemsLength = newItems.length
-  array.length = originalLength + newItemsLength
-  // Move the items after the start index, start from the end so that we don't overwrite any value.
-  for (let i = originalLength - 1; i >= start; i--) {
-    array[i + newItemsLength] = array[i]
-  }
-
-  for (let i = 0; i < newItemsLength; i++) {
-    array[i + start] = newItems[i]
-  }
-}
-
 export const isLastIndex = <T>(array: readonly T[], index: number): boolean => {
   return index === array.length - 1
 }
