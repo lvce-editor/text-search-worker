68f6aa291bff65081e37fdaf3f395950cf6353d0
Le Vivilet <levivlet@pm.me>
2025-01-01 21:53:12 +0100

    feature: move outside buttons dom into separate file

---
+++ packages/text-search-worker/src/parts/GetOutSideButtonsDom/GetOutSideButtonsDom.ts
@@ -0,0 +1,26 @@
+import type { InputAction } from '../InputAction/InputAction.ts'
+import type { OutSideButtonsDom } from '../OutSideButtonsDom/OutSideButtonsDom.ts'
+import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as GetSearchFieldButtonVirtualDom from '../GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getOutSideButtonsDom = (outsideButtons: readonly InputAction[]): OutSideButtonsDom => {
+  if (outsideButtons.length === 0) {
+    return {
+      preNodes: [],
+      postNodes: [],
+    }
+  }
+  return {
+    preNodes: [
+      {
+        type: VirtualDomElements.Div,
+        className: ClassNames.SearchFieldContainer,
+        role: AriaRoles.None,
+        childCount: 1 + outsideButtons.length,
+      },
+    ],
+    postNodes: outsideButtons.flatMap(GetSearchFieldButtonVirtualDom.getSearchFieldButtonVirtualDom),
+  }
+}
