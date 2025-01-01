0d81ac7df83ec8c3ac7ae433a76041068a5e0717
Le Vivilet <levivlet@pm.me>
2025-01-01 15:40:39 +0100

    feature: add regular expression e2e test

---
+++ packages/e2e/src/search.use-regular-expression.js
@@ -0,0 +1,22 @@
+export const name = 'search.use-regular-expression'
+
+export const skip = false
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `Abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('a.c')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result in 1 file')
+
+  // act
+  await Search.toggleUseRegularExpression()
+
+  // assert
+  await expect(message).toHaveText('No results found.')
+}
