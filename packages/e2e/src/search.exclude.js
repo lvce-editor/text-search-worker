cc0bf8b44872e8ed52bd6947f6e2e8dd4262bd98
Le Vivilet <levivlet@pm.me>
2025-01-01 14:25:31 +0100

    feature: add exclude e2e test

---
+++ packages/e2e/src/search.exclude.js
@@ -0,0 +1,22 @@
+export const name = 'search.exclude'
+
+export const skip = false
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('ab')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result in 1 file')
+
+  // act
+  await Search.setExcludeValue('test.css')
+
+  // assert
+  await expect(message).toHaveText('No Results')
+}
