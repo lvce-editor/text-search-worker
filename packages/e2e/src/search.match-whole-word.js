2947928238c5d85919be4508943d99b9d5fa2e50
Le Vivilet <levivlet@pm.me>
2025-01-01 16:21:39 +0100

    feature: add mach whole word test

---
+++ packages/e2e/src/search.match-whole-word.js
@@ -0,0 +1,22 @@
+export const name = 'search.match-whole-word'
+
+export const skip = true
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `Abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('abc')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result in 1 file')
+
+  // act
+  await Search.toggleMatchWholeWord()
+
+  // assert
+  await expect(message).toHaveText('No results found')
+}
