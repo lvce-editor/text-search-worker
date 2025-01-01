8387c477f98e91c06a72f66dbcd84c5c484934b2
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 19:40:19 +0100

    feature: add error classname to input field in case of input error (#498)

--- packages/text-search-worker/src/parts/GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchFieldVirtualDom/GetSearchFieldVirtualDom.ts
@@ -3,6 +3,7 @@ import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetSearchFieldButtonVirtualDom from '../GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts'
+import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
 interface OutSideButtonsDom {
@@ -37,13 +38,18 @@ export const getSearchFieldVirtualDom = (
   insideButtons: readonly InputAction[],
   outsideButtons: readonly InputAction[],
   onFocus = '',
+  hasError = false,
 ): readonly VirtualDomNode[] => {
   const { preNodes, postNodes } = getOutSideButtonsDom(outsideButtons)
+  const searchFieldClassName = hasError
+    ? MergeClassNames.mergeClassNames(ClassNames.SearchField, ClassNames.SearchFieldError)
+    : ClassNames.SearchField
+
   const dom: readonly VirtualDomNode[] = [
     ...preNodes,
     {
       type: VirtualDomElements.Div,
-      className: ClassNames.SearchField,
+      className: searchFieldClassName,
       role: AriaRoles.None,
       childCount: 2,
     },
