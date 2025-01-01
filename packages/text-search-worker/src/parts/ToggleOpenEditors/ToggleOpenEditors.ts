3813610186fc189486c0f26ac1622e024b56c544
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 15:38:26 +0100

    feature: update when search flags change (#481)

--- packages/text-search-worker/src/parts/ToggleOpenEditors/ToggleOpenEditors.ts
+++ packages/text-search-worker/src/parts/ToggleOpenEditors/ToggleOpenEditors.ts
@@ -1,7 +1,7 @@
-import type { SearchHeader } from '../SearchHeader/SearchHeader.ts'
+import type { SearchState } from '../SearchState/SearchState.ts'
 import * as SearchFlags from '../SearchFlags/SearchFlags.ts'
 import * as ToggleSearchFlag from '../ToggleSearchFlag/ToggleSearchFlag.ts'
 
-export const toggleOpenEditors = (state: SearchHeader): SearchHeader => {
+export const toggleOpenEditors = (state: SearchState): Promise<SearchState> => {
   return ToggleSearchFlag.toggleSearchFlag(state, SearchFlags.OpenEditors)
 }
