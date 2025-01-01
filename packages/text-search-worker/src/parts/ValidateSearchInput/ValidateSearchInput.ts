2dec6cf6b91fdc172b2ded747edb7d3bcdd67abc
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-01 19:04:55 +0100

    feature: update validation function to use flags as parameter (#493)

--- packages/text-search-worker/src/parts/ValidateSearchInput/ValidateSearchInput.ts
+++ packages/text-search-worker/src/parts/ValidateSearchInput/ValidateSearchInput.ts
@@ -1,10 +1,12 @@
 // Based on https://github.com/microsoft/vscode/blob/ad375c92764b72a1f28f3e1626563e9e41e5c740/src/vs/workbench/contrib/search/browser/searchWidget.ts#L393 by Microsoft (License MIT)
 
-export const validateSearchInput = (value: string, useRegularExpression: number): string => {
+import { UseRegularExpression } from '../SearchFlags/SearchFlags.ts'
+
+export const validateSearchInput = (value: string, flags: number): string => {
   if (value.length === 0) {
     return ''
   }
-  if (!useRegularExpression) {
+  if (!(flags & UseRegularExpression)) {
     return ''
   }
   try {
