cba3698bdd4a570dfb1dd160fc4c20d25352818d
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-02 18:02:31 +0100

    feature: make listFocusPrevious function generic (#520)

--- packages/text-search-worker/test/ListFocusPrevious.test.ts
+++ packages/text-search-worker/test/ListFocusPrevious.test.ts
@@ -1,13 +1,19 @@
 import { expect, test } from '@jest/globals'
+import type { List } from '../src/parts/List/List.ts'
 import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
 import * as Create from '../src/parts/Create/Create.ts'
 import * as ListFocusPrevious from '../src/parts/ListFocusPrevious/ListFocusPrevious.ts'
 
 test('focusPrevious - returns same state when focusedIndex is 0', () => {
-  const state: SearchState = {
+  const state: List<string> = {
     ...Create.create(0, 0, 0, 0, 0, '', ''),
     focusedIndex: 0,
     items: ['item1', 'item2'],
+    headerHeight: 0,
+    height: 100,
+    itemHeight: 20,
+    minLineY: 0,
+    maxLineY: 5,
   }
 
   const result = ListFocusPrevious.focusPrevious(state)
@@ -16,10 +22,14 @@ test('focusPrevious - returns same state when focusedIndex is 0', () => {
 })
 
 test('focusPrevious - returns same state when focusedIndex is -1', () => {
-  const state: SearchState = {
-    ...Create.create(0, 0, 0, 0, 0, '', ''),
+  const state: List<string> = {
     focusedIndex: -1,
     items: ['item1', 'item2'],
+    headerHeight: 0,
+    height: 100,
+    itemHeight: 20,
+    minLineY: 0,
+    maxLineY: 5,
   }
 
   const result = ListFocusPrevious.focusPrevious(state)
