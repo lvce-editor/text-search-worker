2cd7ac4be2608e8db78d250797360cb3539ffe9e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-02 18:05:48 +0100

    feature: make listFocusNext function generic (#523)

--- packages/text-search-worker/src/parts/ListFocusNext/ListFocusNext.ts
+++ packages/text-search-worker/src/parts/ListFocusNext/ListFocusNext.ts
@@ -1,8 +1,8 @@
-import type { SearchState } from '../SearchState/SearchState.ts'
+import type { List } from '../List/List.ts'
 import { focusIndex } from '../ListFocusIndex/ListFocusIndex.ts'
 import * as ListIndex from '../ListIndex/ListIndex.ts'
 
-export const focusNext = (state: SearchState): SearchState => {
+export const focusNext = <T, State extends List<T>>(state: State): State => {
   const { focusedIndex, items } = state
   const nextIndex = ListIndex.next(items, focusedIndex)
   return focusIndex(state, nextIndex)
