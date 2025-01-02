949eeb29a76e7676fbdd5af73c4af22100107720
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-02 18:53:09 +0100

    fix: maxLineY not updating correctly when expanding search result (#530)

--- packages/text-search-worker/src/parts/SelectIndexFile/SelectIndexFile.ts
+++ packages/text-search-worker/src/parts/SelectIndexFile/SelectIndexFile.ts
@@ -1,20 +1,24 @@
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
 import * as GetFilteredResults from '../GetFilteredResults/GetFilteredResults.ts'
+import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
 import * as ToggleCollapsedPath from '../ToggleCollapsedPath/ToggleCollapsedPath.ts'
 
 export const selectIndexFile = async (state: SearchState, searchResult: SearchResult, index: number): Promise<SearchState> => {
-  const { collapsedPaths, items, maxLineY } = state
+  const { collapsedPaths, items, itemHeight, headerHeight, height } = state
   const path = searchResult.text
   const newCollapsedPaths = ToggleCollapsedPath.toggleCollapsedPath(collapsedPaths, path)
   const filteredResults = GetFilteredResults.getFilteredResults(items, newCollapsedPaths)
-  const newMaxLineY = Math.min(maxLineY, filteredResults.length)
+  const total = filteredResults.length
+  const listHeight = height - headerHeight
+  const numberOfVisible = GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight)
+  const maxLineY = Math.min(numberOfVisible, total)
   return {
     ...state,
     collapsedPaths: newCollapsedPaths,
     listFocusedIndex: index,
     listFocused: true,
     listItems: filteredResults,
-    maxLineY: newMaxLineY,
+    maxLineY,
   }
 }
