3edb190b516e420e8d8258ebe4c13edda628ec79
Le Vivilet <levivlet@pm.me>
2025-01-02 19:36:23 +0100

    feature: add collapse multiple e2e test

---
+++ packages/e2e/src/search.result-collapse-with-multiple-items.js
@@ -0,0 +1,25 @@
+export const name = 'search.result-collapse'
+
+export const skip = 1
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/a.css`, `abc`)
+  await FileSystem.writeFile(`${tmpDir}/b.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('ab')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('2 results in 2 files')
+  const result = Locator('.TreeItem[aria-label="/a.css"]')
+  await expect(result).toHaveAttribute('aria-expanded', 'true')
+
+  // act
+  await Search.selectIndex(2)
+
+  // assert
+  await expect(result).toHaveAttribute('aria-expanded', 'false')
+}
