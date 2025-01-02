0c4d8a2316bcfc53fbd7b53b0fbae0b8efe4fdb8
Le Vivilet <levivlet@pm.me>
2025-01-02 18:02:41 +0100

    feature: make listFocusNext function generic

--- packages/text-search-worker/test/ListFocusNext.test.ts
+++ packages/text-search-worker/test/ListFocusNext.test.ts
@@ -1,14 +1,17 @@
 import { expect, test } from '@jest/globals'
-import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
-import * as Create from '../src/parts/Create/Create.ts'
+import type { List } from '../src/parts/List/List.ts'
 import * as ListFocusNext from '../src/parts/ListFocusNext/ListFocusNext.ts'
 
 test('focusNext updates focusedIndex to the next index', () => {
   const initialItems = ['item1', 'item2', 'item3']
-  const initialState: SearchState = {
-    ...Create.create(0, 0, 0, 0, 0, '', ''),
+  const initialState: List<string> = {
     focusedIndex: 0,
     items: initialItems,
+    headerHeight: 0,
+    height: 100,
+    itemHeight: 20,
+    maxLineY: 10,
+    minLineY: 0,
   }
   const result = ListFocusNext.focusNext(initialState)
   expect(result.focusedIndex).toBe(1)
