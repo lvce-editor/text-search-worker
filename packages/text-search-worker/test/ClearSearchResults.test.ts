2c6076297a76c73bb394bd28113f7239cfb4b8fb
Le Vivilet <levivlet@pm.me>
2025-01-02 18:05:30 +0100

    feature: update tests to better work with new types

--- packages/text-search-worker/test/ClearSearchResults.test.ts
+++ packages/text-search-worker/test/ClearSearchResults.test.ts
@@ -3,12 +3,16 @@ import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
 import * as ClearSearchResults from '../src/parts/ClearSearchResults/ClearSearchResults.ts'
 import * as Create from '../src/parts/Create/Create.ts'
 import * as InputSource from '../src/parts/InputSource/InputSource.ts'
+import * as TextSearchResultType from '../src/parts/TextSearchResultType/TextSearchResultType.ts'
 import * as WhenExpression from '../src/parts/WhenExpression/WhenExpression.ts'
 
 test('clearSearchResults - clears state and focuses search input', () => {
   const state: SearchState = {
     ...Create.create(0, 0, 0, 0, 0, 'test', ''),
-    items: ['item1', 'item2'],
+    items: [
+      { type: TextSearchResultType.File, text: 'file1.txt', end: 0, start: 0, lineNumber: 0 },
+      { type: TextSearchResultType.Match, text: 'match1', end: 0, start: 0, lineNumber: 0 },
+    ],
     minLineY: 10,
     maxLineY: 20,
     message: 'test message',
