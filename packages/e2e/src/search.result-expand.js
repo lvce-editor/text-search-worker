9f319f10a1b5db6c96db6e0d54a1ab9d364b098e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-02 18:43:17 +0100

    feature: add expand e2e etst (#528)

---
+++ packages/e2e/src/search.result-expand.js
@@ -0,0 +1,28 @@
+export const name = 'search.result-expand'
+
+export const skip = 1
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('ab')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result in 1 file')
+  const result = Locator('.TreeItem[aria-label="/test.css"]')
+  await expect(result).toHaveAttribute('aria-expanded', 'true')
+  const match = Locator('.TreeItem[aria-label="abc"]')
+  await expect(match).toBeVisible()
+  await Search.selectIndex(0)
+  await expect(result).toHaveAttribute('aria-expanded', 'false')
+
+  // act
+  await Search.selectIndex(0)
+
+  // assert
+  await expect(result).toHaveAttribute('aria-expanded', 'true')
+}
