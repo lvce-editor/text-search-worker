3be7946b106e41234f1ad4841de9d69f167d3ad7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-02 18:54:35 +0100

    feature: enable search regex e2e test (#531)

--- packages/e2e/src/search.match-case.js
+++ packages/e2e/src/search.match-case.js
@@ -1,7 +1,5 @@
 export const name = 'search.exclude'
 
-export const skip = true
-
 export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
