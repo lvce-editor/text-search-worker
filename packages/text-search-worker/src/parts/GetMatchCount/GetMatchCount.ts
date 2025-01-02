28ad2d1a63e7a6e2348564a83996570c225445c5
Le Vivilet <levivlet@pm.me>
2025-01-02 23:49:37 +0100

    test

--- packages/text-search-worker/src/parts/GetMatchCount/GetMatchCount.ts
+++ packages/text-search-worker/src/parts/GetMatchCount/GetMatchCount.ts
@@ -2,10 +2,10 @@ import { SearchResult } from '../SearchResult/SearchResult.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
 
 export const getMatchCount = (results: readonly SearchResult[], startIndex: number): number => {
-  for (let i = 0; i < results.length; i++) {
+  for (let i = startIndex + 1; i < results.length; i++) {
     const result = results[i]
     if (result.type === TextSearchResultType.File) {
-      return i - startIndex
+      return i - startIndex - 1
     }
   }
   return results.length - startIndex
