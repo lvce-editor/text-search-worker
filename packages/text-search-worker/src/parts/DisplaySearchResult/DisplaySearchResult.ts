184bbcdfdd64cf7f985b3f7c304d7dcfe424d578
Le Vivilet <levivlet@pm.me>
2025-01-02 23:12:55 +0100

    feature: simplify search result display logic

--- packages/text-search-worker/src/parts/DisplaySearchResult/DisplaySearchResult.ts
+++ packages/text-search-worker/src/parts/DisplaySearchResult/DisplaySearchResult.ts
@@ -1,17 +1,19 @@
 export interface DisplaySearchResult {
   readonly title: string
-  readonly type: number
   readonly text: string
   readonly icon: string
   readonly posInSet: number
   readonly setSize: number
   readonly top: number
-  readonly lineNumber: number
+
+  readonly lineNumber: number // TODO not needed
   readonly matchStart: number
   readonly matchLength: number
   readonly replacement: string
   readonly depth: number
   readonly matchCount: number
+
+  // TODO maybe use a flags number instead of multiple booleans
   readonly focused: boolean
-  readonly expanded: boolean
+  readonly expanded: number
 }
