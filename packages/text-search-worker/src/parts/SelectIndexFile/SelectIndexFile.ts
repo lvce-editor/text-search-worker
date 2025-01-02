627c6059dd718ee0dc997c3f2c1169fc540bdaaa
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-02 18:46:54 +0100

    feature: expand search results (#529)

--- packages/text-search-worker/src/parts/SelectIndexFile/SelectIndexFile.ts
+++ packages/text-search-worker/src/parts/SelectIndexFile/SelectIndexFile.ts
@@ -1,12 +1,12 @@
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
-import * as AddCollapsedPath from '../AddCollapsedPath/AddCollapsedPath.ts'
 import * as GetFilteredResults from '../GetFilteredResults/GetFilteredResults.ts'
+import * as ToggleCollapsedPath from '../ToggleCollapsedPath/ToggleCollapsedPath.ts'
 
 export const selectIndexFile = async (state: SearchState, searchResult: SearchResult, index: number): Promise<SearchState> => {
   const { collapsedPaths, items, maxLineY } = state
   const path = searchResult.text
-  const newCollapsedPaths = AddCollapsedPath.addCollapsedPath(collapsedPaths, path)
+  const newCollapsedPaths = ToggleCollapsedPath.toggleCollapsedPath(collapsedPaths, path)
   const filteredResults = GetFilteredResults.getFilteredResults(items, newCollapsedPaths)
   const newMaxLineY = Math.min(maxLineY, filteredResults.length)
   return {
