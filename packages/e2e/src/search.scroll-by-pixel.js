20324cd4b1d527515f8fd80bce109d3ceaf9250b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-03 21:58:55 +0100

    feature: add scroll by pixel e2e test (#556)
    * feature: add scroll by pixel e2e test
    
    * fix test
    
    * enable test
    
    * skip for now

---
+++ packages/e2e/src/search.scroll-by-pixel.js
@@ -0,0 +1,27 @@
+export const name = 'search.scroll-by-pixel'
+
+export const skip = 1
+
+export const test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  const promises = []
+  for (let i = 0; i < 100; i++) {
+    promises.push(FileSystem.writeFile(`${tmpDir}/${i}.css`, `abc`))
+  }
+  await Promise.all(promises)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('ab')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('100 results in 100 files')
+
+  // act
+  await Search.handleWheel(1, 1)
+
+  // assert
+  const treeItems = Locator('.TreeItems')
+  await expect(treeItems).toHaveCSS('top', '-1px')
+}
