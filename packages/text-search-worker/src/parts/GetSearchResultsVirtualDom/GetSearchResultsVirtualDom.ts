e246ac3d4cd38527daca345e58751433e8110685
Le Vivilet <levivlet@pm.me>
2025-01-03 18:27:17 +0100

    feature: add tree items element to separate scrollbar event listeners from tree item event listeners

--- packages/text-search-worker/src/parts/GetSearchResultsVirtualDom/GetSearchResultsVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchResultsVirtualDom/GetSearchResultsVirtualDom.ts
@@ -1,12 +1,11 @@
-import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
 import * as GetScrollBarVirtualDom from '../GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts'
-import * as GetSearchResultVirtualDom from '../GetSearchResultVirtualDom/GetSearchResultVirtualDom.ts'
+import * as GetTreeItemsVirtualDom from '../GetTreeItemsVirtualDom/GetTreeItemsVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getSearchResultsVirtualDom = (
   visibleItems: readonly DisplaySearchResult[],
@@ -18,15 +17,17 @@ export const getSearchResultsVirtualDom = (
   return [
     {
       type: VirtualDomElements.Div,
-      className: MergeClassNames.mergeClassNames(ClassNames.Viewlet, ClassNames.List, focusOutline ? ClassNames.FocusOutline : ClassNames.Empty),
+      className: MergeClassNames.mergeClassNames(
+        ClassNames.Viewlet,
+        ClassNames.List,
+        ClassNames.Tree,
+        focusOutline ? ClassNames.FocusOutline : ClassNames.Empty,
+      ),
       role: AriaRoles.Tree,
       tabIndex: 0,
-      childCount: visibleItems.length + scrollBarDomNodes,
-      onClick: DomEventListenerFunctions.HandleClick,
-      onBlur: DomEventListenerFunctions.HandleListBlur,
-      onWheel: DomEventListenerFunctions.HandleWheel,
+      childCount: 1 + scrollBarDomNodes,
     },
-    ...visibleItems.flatMap(GetSearchResultVirtualDom.getSearchResultVirtualDom),
+    ...GetTreeItemsVirtualDom.getTreeItemsVirtualDom(visibleItems),
     ...GetScrollBarVirtualDom.getScrollBarVirtualDom(scrollbarHeight, scrollBarY),
   ]
 }
---
+++ packages/text-search-worker/src/parts/GetTreeItemsVirtualDom/GetTreeItemsVirtualDom.ts
@@ -0,0 +1,20 @@
+import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as GetSearchResultVirtualDom from '../GetSearchResultVirtualDom/GetSearchResultVirtualDom.ts'
+import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+
+export const getTreeItemsVirtualDom = (visibleItems: readonly DisplaySearchResult[]): readonly VirtualDomNode[] => {
+  return [
+    {
+      type: VirtualDomElements.Div,
+      className: ClassNames.TreeItems,
+      childCount: visibleItems.length,
+      onClick: DomEventListenerFunctions.HandleClick,
+      onBlur: DomEventListenerFunctions.HandleListBlur,
+      onWheel: DomEventListenerFunctions.HandleWheel,
+    },
+    ...visibleItems.flatMap(GetSearchResultVirtualDom.getSearchResultVirtualDom),
+  ]
+}
