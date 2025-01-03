9ef4626b0798a2b5bf9f6f5e41418c7b7ff41191
Le Vivilet <levivlet@pm.me>
2025-01-03 18:38:54 +0100

    lint

--- packages/text-search-worker/src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts
@@ -1,8 +1,8 @@
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
+import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import * as DomEventListenerFunctions from '../DomEventListenerFunctions/DomEventListenerFunctions.ts'
 
 export const getScrollBarVirtualDom = (scrollBarHeight: number, scrollBarY: number): readonly VirtualDomNode[] => {
   if (scrollBarHeight === 0) {
--- packages/text-search-worker/src/parts/GetSearchResultsVirtualDom/GetSearchResultsVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchResultsVirtualDom/GetSearchResultsVirtualDom.ts
@@ -1,11 +1,11 @@
+import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as AriaRoles from '../AriaRoles/AriaRoles.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
-import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
 import * as GetScrollBarVirtualDom from '../GetScrollBarVirtualDom/GetScrollBarVirtualDom.ts'
 import * as GetTreeItemsVirtualDom from '../GetTreeItemsVirtualDom/GetTreeItemsVirtualDom.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getSearchResultsVirtualDom = (
   visibleItems: readonly DisplaySearchResult[],
