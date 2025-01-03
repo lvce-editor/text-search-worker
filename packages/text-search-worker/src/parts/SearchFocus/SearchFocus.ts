c506a1a4ea76002d0e14e56e4d8b6f4d015f505e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-03 22:31:48 +0100

    feature: move handleFocusIn function into separate file (#562)

--- packages/text-search-worker/src/parts/SearchFocus/SearchFocus.ts
+++ packages/text-search-worker/src/parts/SearchFocus/SearchFocus.ts
@@ -1,8 +1,5 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
-import * as Focus from '../Focus/Focus.ts'
 import * as FocusElement from '../FocusElement/FocusElement.ts'
-import * as GetSearchFocusKey from '../GetSearchFocusKey/GetSearchFocusKey.ts'
-import * as InputSource from '../InputSource/InputSource.ts'
 import * as SearchFlags from '../SearchFlags/SearchFlags.ts'
 import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
 
@@ -64,15 +61,4 @@ export const focusReplaceAll = (state: SearchState): SearchState => {
   return FocusElement.focusElement(state, WhenExpression.FocusSearchReplaceAll)
 }
 
-export const handleFocusIn = (state: SearchState, key: any): SearchState => {
-  const focusKey = GetSearchFocusKey.getSearchFocusKey(key)
-  if (state.focus === focusKey) {
-    return state
-  }
-  void Focus.setFocus(WhenExpression.FocusSearch)
-  return {
-    ...state,
-    focus: focusKey,
-    focusSource: InputSource.User,
-  }
-}
+export * from '../HandleFocusIn/HandleFocusIn.ts'
