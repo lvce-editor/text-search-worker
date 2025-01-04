f9625db42a70ab6cb9bb62dfdae29c97285f65fa
Le Vivilet <levivlet@pm.me>
2025-01-04 15:32:28 +0100

    fix: opening search result by click

--- packages/text-search-worker/src/parts/Workspace/Workspace.ts
+++ packages/text-search-worker/src/parts/Workspace/Workspace.ts
@@ -3,7 +3,7 @@ export const pathBaseName = (path: string): string => {
   return path.slice(path.lastIndexOf('/') + 1)
 }
 
-export const getAbsolutePath = (relativePath: string): string => {
+export const getRelativePath = (relativePath: string): string => {
   if (relativePath.startsWith('./')) {
     return `/${relativePath.slice(2)}` // TODO support windows paths
   }
