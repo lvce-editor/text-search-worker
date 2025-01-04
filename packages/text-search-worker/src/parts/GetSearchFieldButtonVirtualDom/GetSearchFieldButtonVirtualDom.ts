c872571ffeeef6ee499ff2321d84877db5a6659b
Le Vivilet <levivlet@pm.me>
2025-01-04 14:40:10 +0100

    set attribute

--- packages/text-search-worker/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts
@@ -5,12 +5,20 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
 
+const getClassName = (checked: boolean, disabled: boolean | undefined): string => {
+  return MergeClassNames.mergeClassNames(
+    ClassNames.SearchFieldButton,
+    checked ? ClassNames.SearchFieldButtonChecked : '',
+    disabled ? ClassNames.SearchFieldButtonDisabled : '',
+  )
+}
+
 export const getSearchFieldButtonVirtualDom = (button: InputAction): readonly VirtualDomNode[] => {
-  const { icon, checked, title, name } = button
+  const { icon, checked, title, name, disabled } = button
   return [
     {
       type: VirtualDomElements.Button,
-      className: MergeClassNames.mergeClassNames(ClassNames.SearchFieldButton, checked ? ClassNames.SearchFieldButtonChecked : ''),
+      className: getClassName(checked, disabled),
       name,
       title,
       role: AriaRoles.CheckBox,
