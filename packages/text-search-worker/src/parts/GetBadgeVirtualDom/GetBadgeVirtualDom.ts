9ab7905fdf7f1de4d8edf810c3bd810836152731
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-04 14:49:15 +0100

    feature: simplify search result virtual dom (#572)
    * feature: simplify search result virtual dom
    
    * fix

--- packages/text-search-worker/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetBadgeVirtualDom/GetBadgeVirtualDom.ts
@@ -5,6 +5,9 @@ import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts
 import { text } from '../VirtualDomHelpers/VirtualDomHelpers.ts'
 
 export const getBadgeVirtualDom = (className: string, badgeText: string): readonly VirtualDomNode[] => {
+  if (!badgeText) {
+    return []
+  }
   return [
     {
       type: VirtualDomElements.Div,
