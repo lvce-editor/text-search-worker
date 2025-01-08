6d626d8c972165062dc104031bd0171283237031
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-08 22:10:18 +0100

    feature: add rpc registry dependency (#579)

--- packages/text-search-worker/src/parts/RpcRegistry/RpcRegistry.ts
+++ packages/text-search-worker/src/parts/RpcRegistry/RpcRegistry.ts
@@ -1,15 +1,1 @@
-import type { Rpc } from '@lvce-editor/rpc'
-
-const rpcs: Record<number, Rpc> = Object.create(null)
-
-export const set = (id: number, rpc: Rpc): void => {
-  rpcs[id] = rpc
-}
-
-export const get = (id: number): Rpc => {
-  return rpcs[id]
-}
-
-export const remove = (id: number): void => {
-  delete rpcs[id]
-}
+export * from '@lvce-editor/rpc-registry'
