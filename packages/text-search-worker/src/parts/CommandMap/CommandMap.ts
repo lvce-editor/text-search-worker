eade95ad7565304a651d32a9352f6a5895d44b67
Le Vivilet <levivlet@pm.me>
2025-01-12 01:07:51 +0100

    feature: add incremental render function

--- packages/text-search-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/text-search-worker/src/parts/CommandMap/CommandMap.ts
@@ -39,6 +39,7 @@ import * as ListHandleWheel from '../ListHandleWheel/ListHandleWheel.ts'
 import * as LoadContent from '../LoadContent/LoadContent.ts'
 import * as Refresh from '../Refresh/Refresh.ts'
 import * as Render from '../Render/Render.ts'
+import * as RenderIncremental from '../RenderIncremental/RenderIncremental.ts'
 import * as ReplaceAll from '../ReplaceAll/ReplaceAll.ts'
 import * as ReplaceAllAndPrompt from '../ReplaceAllAndPrompt/ReplaceAllAndPrompt.ts'
 import * as SaveState from '../SaveState/SaveState.ts'
@@ -124,6 +125,7 @@ export const commandMap = {
   'TextSearch.getFocus': GetFocus.getFocus,
   'TextSearch.getKeyBindings': GetKeyBindings.getKeyBindings,
   'TextSearch.render': Render.doRender,
+  'TextSearch.renderIncremental': RenderIncremental.renderIncremental,
   'TextSearch.saveState': SaveState.saveState,
 
   // TODO needed?
