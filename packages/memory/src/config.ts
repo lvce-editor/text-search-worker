d0482a46c0ef5f09b0ff242fd2c2627bee40b327
Le Vivilet <levivlet@pm.me>
2025-01-12 01:18:04 +0100

    update max memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const threshold = 475_000
+export const threshold = 500_000
 
 export const workerPath = join(root, '.tmp/dist/dist/textSearchWorkerMain.js')
 
