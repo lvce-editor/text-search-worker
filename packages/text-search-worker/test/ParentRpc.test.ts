afcb6e9bb62ee91f63bb12bdb0578bf48d017653
Le Vivilet <levivlet@pm.me>
2025-01-15 00:34:54 +0100

    lint

--- packages/text-search-worker/test/ParentRpc.test.ts
+++ packages/text-search-worker/test/ParentRpc.test.ts
@@ -43,7 +43,7 @@ test('invoke - handles undefined arguments', async () => {
 })
 
 test('invoke - handles multiple arguments of different types', async () => {
-  let capturedArgs: any[] = []
+  let capturedArgs: readonly any[] = []
   const mockRpc = {
     invoke(command: string, ...args: readonly any[]): string {
       capturedArgs = args
