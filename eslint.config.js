2fe5216348bc2ff9b8d6c30616671feee713d185
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-01-14 20:37:33 +0100

    feature: use npm package for eslint config (#586)

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,7 @@
-import perfectionist from 'eslint-plugin-perfectionist'
-import eslint from '@eslint/js'
-import tseslint from 'typescript-eslint'
+import config from '@lvce-editor/eslint-config'
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
+export default [
+  ...config,
   {
     languageOptions: {
       parserOptions: {
@@ -14,56 +11,6 @@ export default tseslint.config(
     },
   },
   {
-    ignores: [
-      'dist',
-      '.tmp',
-      '**/build/**',
-      '**/coverage/**',
-      '**/server/**',
-      '**/e2e/**',
-      '**/memory/**',
-      '**/test-integration/**',
-      '**/test-integration-util/**',
-      'scripts',
-      'rollup.config.js',
-      'eslint.config.js',
-      'packages/text-search-worker/src/textSearchWorkerMain.ts',
-    ],
+    ignores: ['packages/text-search-worker/src/textSearchWorkerMain.ts'],
   },
-  {
-    rules: {
-      '@typescript-eslint/explicit-function-return-type': 'error',
-      '@typescript-eslint/consistent-type-exports': 'error',
-      '@typescript-eslint/consistent-type-imports': 'error',
-    },
-  },
-  {
-    rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/ban-ts-comment': 'off',
-      '@typescript-eslint/no-unsafe-member-access': 'off',
-      '@typescript-eslint/no-unsafe-assignment': 'off',
-      '@typescript-eslint/no-unsafe-call': 'off',
-      '@typescript-eslint/no-unsafe-return': 'off',
-      'no-case-declarations': 'off',
-      '@typescript-eslint/no-unsafe-argument': 'off',
-      '@typescript-eslint/restrict-template-expressions': 'off',
-      '@typescript-eslint/require-await': 'off',
-    },
-  },
-  {
-    plugins: {
-      perfectionist,
-    },
-    rules: {
-      'perfectionist/sort-imports': [
-        'error',
-        {
-          type: 'natural',
-          order: 'asc',
-          newlinesBetween: 'never',
-        },
-      ],
-    },
-  },
-)
+]
