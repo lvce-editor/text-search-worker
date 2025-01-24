edba99ac4368b5080852570fed59e5d28a2c434d
Le Vivilet <levivlet@pm.me>
2025-01-25 00:25:17 +0100

    feature: enable replace disabled e2e test

--- packages/e2e/src/search.replace-disabled-with-no-results.ts
+++ packages/e2e/src/search.replace-disabled-with-no-results.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.replace-disabled-with-no-results'
 
-export const skip = 1
-
 export const test: Test = async ({ Main, Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
