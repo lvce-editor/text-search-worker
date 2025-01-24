4ba4b9a5399b6a452eb192a1d8523541b0fe108f
Le Vivilet <levivlet@pm.me>
2025-01-25 00:47:22 +0100

    fix

--- packages/text-search-worker/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchFieldButtonVirtualDom/GetSearchFieldButtonVirtualDom.ts
@@ -5,17 +5,43 @@ import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as GetSearchFieldButtonClassName from '../GetSearchFieldButtonClassName/GetSearchFieldButtonClassName.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
+import * as InputActionFlag from '../InputActionFlag/InputActionFlag.ts'
+
+// TODO maybe move logic to viewmodel, which returns ariaChecked 1 | 2 | 3
+const getAriaChecked = (flag: number): boolean | undefined => {
+  switch (flag) {
+    case InputActionFlag.CheckBoxEnabled:
+      return true
+    case InputActionFlag.CheckBoxDisabled:
+      return false
+    default:
+      return undefined
+  }
+}
+
+// TODO have have separate renderers for checkbox and button elements
+const getRole = (flag: number): string | undefined => {
+  switch (flag) {
+    case InputActionFlag.CheckBoxEnabled:
+    case InputActionFlag.CheckBoxDisabled:
+      return AriaRoles.CheckBox
+    default:
+      return undefined
+  }
+}
 
 export const getSearchFieldButtonVirtualDom = (button: InputAction): readonly VirtualDomNode[] => {
   const { icon, title, name, flag } = button
+  const ariaChecked = getAriaChecked(flag)
+  const role = getRole(flag)
   return [
     {
       type: VirtualDomElements.Button,
       className: GetSearchFieldButtonClassName.getSearchFieldButtonClassName(flag),
       name,
       title,
-      role: AriaRoles.CheckBox,
-      ariaChecked: flag,
+      role,
+      ariaChecked,
       tabIndex: 0,
       childCount: 1,
     },
