8662335a6fdfea8b0861f5a7a8b697a440b01094
Le Vivilet <levivlet@pm.me>
2025-01-25 01:01:08 +0100

    feature: add another scroll e2e test

---
+++ packages/e2e/src/search.scrollbar.ts
@@ -0,0 +1,30 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'search.scrollbar'
+
+export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  const promises: any[] = []
+  for (let i = 0; i < 100; i++) {
+    promises.push(FileSystem.writeFile(`${tmpDir}/${i}.css`, `abc`))
+  }
+  await Promise.all(promises)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+
+  // act
+  await Search.setValue('ab')
+
+  // assert
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('100 results in 100 files')
+
+  // act
+  await Search.handleWheel(1, 44)
+
+  // assert
+  const treeItems = Locator('.TreeItems')
+  await expect(treeItems).toHaveCSS('top', '0px')
+}
