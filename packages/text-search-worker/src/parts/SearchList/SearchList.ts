8c4015e234cf89f25cd0056c32b9b6b58d3e0ccb
Le Vivilet <levivlet@pm.me>
2025-01-27 23:46:31 +0100

    feature: make search list interface extend list

--- packages/text-search-worker/src/parts/SearchList/SearchList.ts
+++ packages/text-search-worker/src/parts/SearchList/SearchList.ts
@@ -1,30 +1,24 @@
 import type { DisplaySearchResult } from '../DisplaySearchResult/DisplaySearchResult.ts'
+import type { List } from '../List/List.ts'
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 
-export interface SearchList {
+export interface SearchList extends List<SearchResult> {
   readonly assetDir: string
   readonly collapsedPaths: readonly string[]
   readonly deltaY: number
   readonly disposed: boolean
   readonly fileCount: number
   readonly finalDeltaY: number
-  readonly focusedIndex: number
   readonly handleOffset: number
-  readonly headerHeight: number
-  readonly height: number
   readonly icons: readonly string[]
   readonly inputSource: number
-  readonly itemHeight: number
-  readonly items: readonly SearchResult[]
   readonly listItems: readonly SearchResult[]
   readonly listFocused: boolean
   readonly listFocusedIndex: number
   readonly loaded: boolean
   readonly matchCount: number
-  readonly maxLineY: number
   readonly message: string
   readonly minimumSliderSize: number
-  readonly minLineY: number
   readonly scrollBarActive: boolean
   readonly scrollBarHeight: number
   readonly searchId: number
