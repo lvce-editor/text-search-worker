b1c01eacc5abd02da9217fa8bf7e155649904959
Le Vivilet <levivlet@pm.me>
2025-01-27 23:37:58 +0100

    feature: use list prefix for list functions

--- packages/text-search-worker/src/parts/ListFocusIndex/ListFocusIndex.ts
+++ packages/text-search-worker/src/parts/ListFocusIndex/ListFocusIndex.ts
@@ -1,7 +1,7 @@
 import type { List } from '../List/List.ts'
 import * as Assert from '../Assert/Assert.ts'
-import * as FocusIndexScrollDown from '../FocusIndexScrollDown/FocusIndexScrollDown.ts'
-import * as FocusIndexScrollUp from '../FocusIndexScrollUp/FocusIndexScrollUp.ts'
+import * as FocusIndexScrollDown from '../ListFocusIndexScrollDown/ListFocusIndexScrollDown.ts'
+import * as FocusIndexScrollUp from '../ListFocusIndexScrollUp/ListFocusIndexScrollUp.ts'
 
 export const focusIndex = <T, State extends List<T>>(state: State, index: number): State => {
   const { itemHeight, minLineY, maxLineY, headerHeight, height, items } = state
