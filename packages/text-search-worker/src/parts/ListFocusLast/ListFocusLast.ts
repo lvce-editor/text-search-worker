7a86f2364e1ec98e6cc714d7e97162bf961f51e0
Le Vivilet <levivlet@pm.me>
2025-01-27 23:32:26 +0100

    feature: use generics for focusLast function

--- packages/text-search-worker/src/parts/ListFocusLast/ListFocusLast.ts
+++ packages/text-search-worker/src/parts/ListFocusLast/ListFocusLast.ts
@@ -1,8 +1,8 @@
-import type { SearchState } from '../SearchState/SearchState.ts'
+import type { List } from '../List/List.ts'
 import { focusIndex } from '../ListFocusIndex/ListFocusIndex.ts'
 import * as ListIndex from '../ListIndex/ListIndex.ts'
 
-export const focusLast = (state: SearchState): SearchState => {
+export const focusLast = <T, State extends List<T>>(state: State): State => {
   const { items } = state
   const lastIndex = ListIndex.last(items)
   return focusIndex(state, lastIndex)
--- packages/text-search-worker/test/ListFocusLast.test.ts
+++ packages/text-search-worker/test/ListFocusLast.test.ts
@@ -1,12 +1,17 @@
 import { expect, test } from '@jest/globals'
-import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
-import * as Create from '../src/parts/Create/Create.ts'
+import type { List } from '../src/parts/List/List.ts'
 import { focusLast } from '../src/parts/ListFocusLast/ListFocusLast.ts'
 
 test('focusLast', () => {
-  const initialState: SearchState = {
-    ...Create.create(0, 0, 0, 0, 0, '', ''),
-    focusedIndex: -1,
+  const initialItems: readonly string[] = []
+  const initialState: List<string> = {
+    focusedIndex: 0,
+    items: initialItems,
+    headerHeight: 0,
+    height: 100,
+    itemHeight: 20,
+    maxLineY: 10,
+    minLineY: 0,
   }
   const result = focusLast(initialState)
   expect(result.focusedIndex).toBe(-1)
