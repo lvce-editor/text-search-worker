bbd70815c101780bc1e170c41de7c88faf464015
Le Vivilet <levivlet@pm.me>
2025-01-27 23:33:53 +0100

    feature: use generic list for focusFirst function

--- packages/text-search-worker/test/ListFocusFirst.test.ts
+++ packages/text-search-worker/test/ListFocusFirst.test.ts
@@ -1,12 +1,17 @@
 import { expect, test } from '@jest/globals'
-import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
-import * as Create from '../src/parts/Create/Create.ts'
+import type { List } from '../src/parts/List/List.ts'
 import * as ListFocusFirst from '../src/parts/ListFocusFirst/ListFocusFirst.ts'
 
 test('focusFirst', () => {
-  const initialState: SearchState = {
-    ...Create.create(0, 0, 0, 0, 0, '', ''),
-    focusedIndex: -1,
+  const initialItems = ['item1', 'item2', 'item3']
+  const initialState: List<string> = {
+    focusedIndex: 0,
+    items: initialItems,
+    headerHeight: 0,
+    height: 100,
+    itemHeight: 20,
+    maxLineY: 10,
+    minLineY: 0,
   }
   const result = ListFocusFirst.focusFirst(initialState)
   expect(result.focusedIndex).toBe(-1)
