16affc17f0b63fff2509820e64c12ea9c611c401
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-02 18:42:01 +0100

    feature: improve diff test (#628)
    * feature: improve diff test
    
    * lint

--- packages/text-search-worker/test/Diff.test.ts
+++ packages/text-search-worker/test/Diff.test.ts
@@ -3,9 +3,10 @@ import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
 import * as Create from '../src/parts/Create/Create.ts'
 import * as Diff from '../src/parts/Diff/Diff.ts'
 import * as DiffType from '../src/parts/DiffType/DiffType.ts'
+import * as InputSource from '../src/parts/InputSource/InputSource.ts'
 import * as SearchViewStates from '../src/parts/SearchViewStates/SearchViewStates.ts'
 
-test('doRender - no changes returns empty commands', () => {
+test('diff - no changes', () => {
   const uid = 1
   Create.create(uid, 0, 0, 0, 0, '', '')
   const { oldState } = SearchViewStates.get(uid)
@@ -13,35 +14,38 @@ test('doRender - no changes returns empty commands', () => {
   expect(Diff.diff(oldState, newState)).toEqual([])
 })
 
-test.skip('doRender - value change', () => {
+test('diff - value change', () => {
   const uid = 1
   Create.create(uid, 0, 0, 0, 0, '', '')
   const { oldState } = SearchViewStates.get(uid)
   const newState: SearchState = {
     ...oldState,
     value: 'a',
+    inputSource: InputSource.Script,
   }
   expect(Diff.diff(oldState, newState)).toEqual([DiffType.RenderValue])
 })
 
-test.skip('doRender - include value change', () => {
+test('diff - include value change', () => {
   const uid = 1
   Create.create(uid, 0, 0, 0, 0, '', '')
   const { oldState } = SearchViewStates.get(uid)
   const newState: SearchState = {
     ...oldState,
     includeValue: 'a',
+    inputSource: InputSource.Script,
   }
   expect(Diff.diff(oldState, newState)).toEqual([DiffType.RenderIncludeValue])
 })
 
-test.skip('doRender - exclude value change', () => {
+test('diff - exclude value change', () => {
   const uid = 1
   Create.create(uid, 0, 0, 0, 0, '', '')
   const { oldState } = SearchViewStates.get(uid)
   const newState: SearchState = {
     ...oldState,
     excludeValue: 'a',
+    inputSource: InputSource.Script,
   }
   expect(Diff.diff(oldState, newState)).toEqual([DiffType.RenderExcludeValue])
 })
