cb846284f09f14cb20f0c2348c26aa32fae99b59
Le Vivilet <levivlet@pm.me>
2025-02-08 22:56:16 +0100

    feature: move diff modules into separate file

---
+++ packages/text-search-worker/src/parts/DiffModules/DiffModules.ts
@@ -0,0 +1,8 @@
+import * as DiffExcludeValue from '../DiffExcludeValue/DiffExcludeValue.ts'
+import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
+import * as DiffIncludeValue from '../DiffIncludeValue/DiffIncludeValue.ts'
+import * as DiffItems from '../DiffItems/DiffItems.ts'
+import * as DiffReplaceValue from '../DiffReplaceValue/DiffReplaceValue.ts'
+import * as DiffValue from '../DiffValue/DiffValue.ts'
+
+export const modules = [DiffItems, DiffFocus, DiffIncludeValue, DiffExcludeValue, DiffReplaceValue, DiffValue]
