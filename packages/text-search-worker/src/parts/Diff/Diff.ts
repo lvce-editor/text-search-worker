38b50b6dade2c96e9d5dec34bf397554d8044861
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-08 22:59:59 +0100

    feature: move diff modules into separate file (#635)

--- packages/text-search-worker/src/parts/Diff/Diff.ts
+++ packages/text-search-worker/src/parts/Diff/Diff.ts
@@ -1,16 +1,9 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
-import * as DiffExcludeValue from '../DiffExcludeValue/DiffExcludeValue.ts'
-import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
-import * as DiffIncludeValue from '../DiffIncludeValue/DiffIncludeValue.ts'
-import * as DiffItems from '../DiffItems/DiffItems.ts'
-import * as DiffReplaceValue from '../DiffReplaceValue/DiffReplaceValue.ts'
-import * as DiffValue from '../DiffValue/DiffValue.ts'
-
-const modules = [DiffItems, DiffFocus, DiffIncludeValue, DiffExcludeValue, DiffReplaceValue, DiffValue]
+import * as DiffModules from '../DiffModules/DiffModules.ts'
 
 export const diff = (oldState: SearchState, newState: SearchState): readonly number[] => {
   const diffResult: number[] = []
-  for (const module of modules) {
+  for (const module of DiffModules.modules) {
     if (!module.isEqual(oldState, newState)) {
       diffResult.push(module.diffType)
     }
