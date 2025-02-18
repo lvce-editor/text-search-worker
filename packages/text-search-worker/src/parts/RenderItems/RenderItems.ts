13851cf7dc770d2415247bf89c96cdf7c4ef1090
Le Vivilet <levivlet@pm.me>
2025-02-18 19:53:12 +0100

    feature: use new viewmodel

--- packages/text-search-worker/src/parts/RenderItems/RenderItems.ts
+++ packages/text-search-worker/src/parts/RenderItems/RenderItems.ts
@@ -1,42 +1,21 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
-import * as GetSearchDisplayResults from '../GetSearchDisplayResults/GetSearchDisplayResults.ts'
+import * as CreateViewModel from '../CreateViewModel/CreateViewModel.ts'
 import * as GetSearchVirtualDom from '../GetSearchVirtualDom/GetSearchVirtualDom.ts'
-import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
 
 export const renderItems = (oldState: SearchState, newState: SearchState): any => {
-  const displayResults = GetSearchDisplayResults.getDisplayResults(
-    newState.listItems,
-    newState.itemHeight,
-    newState.fileCount,
-    newState.value,
-    newState.minLineY,
-    newState.maxLineY,
-    newState.replacement,
-    newState.icons,
-    newState.listFocusedIndex,
-    newState.collapsedPaths,
-    newState.items,
-  )
-  const focusOutline = newState.listFocused && newState.listFocusedIndex === -1
-  const scrollBarValue = (newState.deltaY / newState.finalDeltaY) * 100
-  const scrollBarY = ScrollBarFunctions.getScrollBarY(
-    newState.deltaY,
-    newState.finalDeltaY,
-    newState.height - newState.headerHeight,
-    newState.scrollBarHeight,
-  )
+  const viewModel = CreateViewModel.createViewModel(newState)
   const dom = GetSearchVirtualDom.getSearchVirtualDom(
-    displayResults,
-    newState.flags,
-    newState.message,
-    focusOutline,
-    newState.searchInputErrorMessage,
-    newState.scrollBarHeight,
-    scrollBarY,
-    scrollBarValue,
-    newState.deltaY,
-    newState.itemHeight,
-    newState.matchCount,
+    viewModel.displayResults,
+    viewModel.flags,
+    viewModel.message,
+    viewModel.focusOutline,
+    viewModel.searchInputErrorMessage,
+    viewModel.scrollBarHeight,
+    viewModel.scrollBarY,
+    viewModel.scrollBarValue,
+    viewModel.deltaY,
+    viewModel.itemHeight,
+    viewModel.matchCount,
   )
   return ['Viewlet.setDom2', newState.uid, dom]
 }
