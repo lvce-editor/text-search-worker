93cd84c97777f842d3b750090c9d25248638e625
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-18 19:47:01 +0100

    feature: add function to create view model (#642)

---
+++ packages/text-search-worker/src/parts/CreateViewModel/CreateViewModel.ts
@@ -0,0 +1,59 @@
+import type { SearchState } from '../SearchState/SearchState.ts'
+import type { ViewModel } from '../ViewModel/ViewModel.ts'
+import * as GetSearchDisplayResults from '../GetSearchDisplayResults/GetSearchDisplayResults.ts'
+import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
+
+export const createViewModel = (state: SearchState): ViewModel => {
+  const {
+    listItems,
+    itemHeight,
+    fileCount,
+    value,
+    minLineY,
+    maxLineY,
+    replacement,
+    icons,
+    listFocusedIndex,
+    collapsedPaths,
+    items,
+    listFocused,
+    deltaY,
+    finalDeltaY,
+    height,
+    headerHeight,
+    flags,
+    message,
+    searchInputErrorMessage,
+    scrollBarHeight,
+    matchCount,
+  } = state
+  const displayResults = GetSearchDisplayResults.getDisplayResults(
+    listItems,
+    itemHeight,
+    fileCount,
+    value,
+    minLineY,
+    maxLineY,
+    replacement,
+    icons,
+    listFocusedIndex,
+    collapsedPaths,
+    items,
+  )
+  const focusOutline = listFocused && listFocusedIndex === -1
+  const scrollBarValue = (deltaY / finalDeltaY) * 100
+  const scrollBarY = ScrollBarFunctions.getScrollBarY(deltaY, finalDeltaY, height - headerHeight, scrollBarHeight)
+  return {
+    displayResults,
+    flags,
+    message,
+    focusOutline,
+    searchInputErrorMessage,
+    scrollBarHeight,
+    scrollBarY,
+    scrollBarValue,
+    deltaY,
+    itemHeight,
+    matchCount,
+  }
+}
