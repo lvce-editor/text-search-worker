5831b7789541d80429682bee3d054fa0fedf231a
Le Vivilet <levivlet@pm.me>
2025-02-19 20:06:12 +0100

    fix

--- packages/text-search-worker/src/parts/RenderActions/RenderActions.ts
+++ packages/text-search-worker/src/parts/RenderActions/RenderActions.ts
@@ -1,6 +1,6 @@
 import type { Action } from '../Action/Action.ts'
-import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.js'
-import * as SearchViewStates from '../SearchViewStates/SearchViewStates.js'
+import * as GetActionsVirtualDom from '../GetActionsVirtualDom/GetActionsVirtualDom.ts'
+import * as SearchViewStates from '../SearchViewStates/SearchViewStates.ts'
 
 export const renderActions = (uid: number): readonly any[] => {
   const { oldState, newState } = SearchViewStates.get(uid)
