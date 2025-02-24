250689642f644b8431552e00ffd664e5048644a8
Le Vivilet <levivlet@pm.me>
2025-02-24 20:20:06 +0100

    feature: add render test

---
+++ packages/text-search-worker/test/RenderItems.test.ts
@@ -0,0 +1,53 @@
+import { expect, test } from '@jest/globals'
+import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
+import * as Create from '../src/parts/Create/Create.ts'
+import * as InputSource from '../src/parts/InputSource/InputSource.ts'
+import * as RenderItems from '../src/parts/RenderItems/RenderItems.ts'
+import * as TextSearchResultType from '../src/parts/TextSearchResultType/TextSearchResultType.ts'
+test('renderItems - returns correct command structure', () => {
+  const oldState: SearchState = {
+    ...Create.create(0, 0, 0, 0, 0, '', ''),
+    items: [],
+    minLineY: 0,
+    maxLineY: 0,
+    uid: 1,
+    value: '',
+    includeValue: '',
+    excludeValue: '',
+    focus: '',
+    focusSource: InputSource.Script,
+    inputSource: InputSource.Script,
+  }
+
+  const newState: SearchState = {
+    ...oldState,
+    items: [{ type: TextSearchResultType.File, text: 'item1', start: 0, end: 0, lineNumber: 0 }],
+    uid: 1,
+    itemHeight: 22,
+    fileCount: 1,
+    value: 'test',
+    listItems: [{ type: TextSearchResultType.File, text: 'item1', start: 0, end: 0, lineNumber: 0 }],
+  }
+
+  const result = RenderItems.renderItems(oldState, newState)
+  expect(result).toEqual(['Viewlet.setDom2', 1, expect.any(Array)])
+})
+
+test('renderItems - handles empty items', () => {
+  const oldState: SearchState = {
+    ...Create.create(0, 0, 0, 0, 0, '', ''),
+    items: [],
+    minLineY: 0,
+    maxLineY: 0,
+    uid: 1,
+  }
+
+  const newState = {
+    ...oldState,
+    items: [],
+    uid: 1,
+  }
+
+  const result = RenderItems.renderItems(oldState, newState)
+  expect(result).toEqual(['Viewlet.setDom2', 1, expect.any(Array)])
+})
