76c7de107f8b0ea63574e74683d570dd8a7b7f9d
Le Vivilet <levivlet@pm.me>
2025-02-24 19:42:48 +0100

    lint

--- packages/text-search-worker/test/GetRenderer.test.ts
+++ packages/text-search-worker/test/GetRenderer.test.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@jest/globals'
-import * as GetRenderer from '../src/parts/GetRenderer/GetRenderer.ts'
 import * as DiffType from '../src/parts/DiffType/DiffType.ts'
+import * as GetRenderer from '../src/parts/GetRenderer/GetRenderer.ts'
 import * as RenderFocus from '../src/parts/RenderFocus/RenderFocus.ts'
 import * as RenderItems from '../src/parts/RenderItems/RenderItems.ts'
 import * as RenderValue from '../src/parts/RenderValue/RenderValue.ts'
