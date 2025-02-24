826bdc724241d9491e2c9ab52ccfaa18f72ae2ee
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-24 19:40:01 +0100

    feature: add event listeners test (#661)
    * feature: add event listeners test
    
    * lint

---
+++ packages/text-search-worker/test/RenderEventListeners.test.ts
@@ -0,0 +1,55 @@
+import { expect, test } from '@jest/globals'
+import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
+import * as InputSource from '../src/parts/InputSource/InputSource.ts'
+import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
+
+test('renderEventListeners', () => {
+  expect(RenderEventListeners.renderEventListeners()).toEqual([
+    {
+      name: DomEventListenerFunctions.HandleExcludeInput,
+      params: ['handleExcludeInput', 'event.target.value'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleIncludeInput,
+      params: ['handleIncludeInput', 'event.target.value'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleReplaceInput,
+      params: ['handleReplaceInput', 'event.target.value'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleInput,
+      params: ['handleInput', 'event.target.value', InputSource.User],
+    },
+    {
+      name: DomEventListenerFunctions.HandleClick,
+      params: ['handleClickAt', 'event.clientX', 'event.clientY'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleToggleButtonClick,
+      params: ['handleClickAt', 'event.clientX', 'event.clientY'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleListFocus,
+      params: ['handleClickAt', 'event.clientX', 'event.clientY'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleListBlur,
+      params: ['handleListBlur'],
+    },
+    {
+      name: DomEventListenerFunctions.HandleContextMenu,
+      params: ['handleContextMenu', 'event.button', 'event.clientX', 'event.clientY'],
+      preventDefault: true,
+    },
+    {
+      name: DomEventListenerFunctions.HandleWheel,
+      params: ['handleContextMenu', 'event.deltaMode', 'event.deltaY'],
+      passive: true,
+    },
+    {
+      name: DomEventListenerFunctions.HandleHeaderClick2,
+      params: ['handleHeaderClick', 'event.target.name'],
+    },
+  ])
+})
