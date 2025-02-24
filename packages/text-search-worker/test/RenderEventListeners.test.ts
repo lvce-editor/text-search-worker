a03b661d36fbf0030175c3a317daa1b2121083b2
Le Vivilet <levivlet@pm.me>
2025-02-24 19:36:54 +0100

    lint

--- packages/text-search-worker/test/RenderEventListeners.test.ts
+++ packages/text-search-worker/test/RenderEventListeners.test.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@jest/globals'
-import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 import * as DomEventListenerFunctions from '../src/parts/DomEventListenerFunctions/DomEventListenerFunctions.ts'
 import * as InputSource from '../src/parts/InputSource/InputSource.ts'
+import * as RenderEventListeners from '../src/parts/RenderEventListeners/RenderEventListeners.ts'
 
 test('renderEventListeners', () => {
   expect(RenderEventListeners.renderEventListeners()).toEqual([
