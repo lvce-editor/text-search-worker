cc7cd0ee1e2686007c80bd4b0b7157230e2e979f
Le Vivilet <levivlet@pm.me>
2025-02-24 20:00:28 +0100

    feature: add selectIndex test

---
+++ packages/text-search-worker/test/SelectIndexFile.test.ts
@@ -0,0 +1,76 @@
+import { beforeEach, expect, jest, test } from '@jest/globals'
+import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
+import * as Create from '../src/parts/Create/Create.ts'
+import * as RpcId from '../src/parts/RpcId/RpcId.ts'
+import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
+import { selectIndexFile } from '../src/parts/SelectIndexFile/SelectIndexFile.ts'
+import * as TextSearchResultType from '../src/parts/TextSearchResultType/TextSearchResultType.ts'
+
+const mockIcon = 'file-icon'
+const mockRpc = {
+  // @ts-ignore
+  invoke: jest.fn().mockResolvedValue(mockIcon),
+} as any
+
+RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+
+beforeEach(() => {
+  mockRpc.invoke.mockReset()
+})
+
+test('selectIndexFile - toggles collapsed path and updates state', async () => {
+  const state: SearchState = {
+    ...Create.create(0, 0, 0, 0, 0, '', ''),
+    items: [
+      { type: TextSearchResultType.File, text: 'file1.txt', start: 0, end: 0, lineNumber: 0 },
+      { type: TextSearchResultType.Match, text: 'match1', start: 0, end: 0, lineNumber: 1 },
+      { type: TextSearchResultType.File, text: 'file2.txt', start: 0, end: 0, lineNumber: 2 },
+    ],
+    collapsedPaths: [],
+    itemHeight: 22,
+    headerHeight: 40,
+    height: 500,
+  }
+
+  const result = await selectIndexFile(state, state.items[0], 0)
+
+  expect(result).toEqual({
+    ...state,
+    collapsedPaths: ['file1.txt'],
+    listFocusedIndex: 0,
+    listFocused: true,
+    listItems: [
+      { type: TextSearchResultType.File, text: 'file1.txt', start: 0, end: 0, lineNumber: 0 },
+      { type: TextSearchResultType.File, text: 'file2.txt', start: 0, end: 0, lineNumber: 2 },
+    ],
+    maxLineY: 2,
+    icons: [undefined, undefined],
+  })
+})
+
+test('selectIndexFile - uncollapse path when already collapsed', async () => {
+  const state: SearchState = {
+    ...Create.create(0, 0, 0, 0, 0, '', ''),
+    items: [
+      { type: TextSearchResultType.File, text: 'file1.txt', start: 0, end: 0, lineNumber: 0 },
+      { type: TextSearchResultType.Match, text: 'match1', start: 0, end: 0, lineNumber: 1 },
+      { type: TextSearchResultType.File, text: 'file2.txt', start: 0, end: 0, lineNumber: 2 },
+    ],
+    collapsedPaths: ['file1.txt'],
+    itemHeight: 22,
+    headerHeight: 40,
+    height: 500,
+  }
+
+  const result = await selectIndexFile(state, state.items[0], 0)
+
+  expect(result).toEqual({
+    ...state,
+    collapsedPaths: [],
+    listFocusedIndex: 0,
+    listFocused: true,
+    listItems: state.items,
+    maxLineY: 3,
+    icons: [undefined, '', undefined],
+  })
+})
