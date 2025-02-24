b84b1a44f89c1d61652b7b031047607af8443c10
Le Vivilet <levivlet@pm.me>
2025-02-24 19:22:00 +0100

    feature: move renderFocus into separate file

--- packages/text-search-worker/src/parts/GetRenderer/GetRenderer.ts
+++ packages/text-search-worker/src/parts/GetRenderer/GetRenderer.ts
@@ -1,20 +1,15 @@
 import type { Renderer } from '../Renderer/Renderer.ts'
-import type { SearchState } from '../SearchState/SearchState.ts'
 import * as DiffType from '../DiffType/DiffType.ts'
-import * as InputSource from '../InputSource/InputSource.ts'
+import * as RenderFocus from '../RenderFocus/RenderFocus.ts'
 import * as RenderItems from '../RenderItems/RenderItems.ts'
 import * as RenderValue from '../RenderValue/RenderValue.ts'
-
-const renderFocus = (oldState: SearchState, newState: SearchState): boolean => {
-  return oldState.focus === newState.focus || newState.focusSource === InputSource.User
-}
 
 export const getRenderer = (diffType: number): Renderer => {
   switch (diffType) {
     case DiffType.RenderValue:
       return RenderValue.renderValue
     case DiffType.RenderFocus:
-      return renderFocus
+      return RenderFocus.renderFocus
     case DiffType.RenderReplaceValue:
       return RenderValue.renderReplacement
     case DiffType.RenderIncludeValue:
