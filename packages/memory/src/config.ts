bed6472abccac689b786b1ad508302355755f658
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-02 16:41:45 +0100

    feature: add instantiations (#691)

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,6 +3,10 @@ import { root } from './root.ts'
 
 export const threshold = 500_000
 
+export const instantiations = 7000
+
+export const instantiationsPath = join(root, 'packages', 'text-search-worker')
+
 export const workerPath = join(root, '.tmp/dist/dist/textSearchWorkerMain.js')
 
 export const playwrightPath = new URL('../../e2e/node_modules/playwright/index.mjs', import.meta.url).toString()
