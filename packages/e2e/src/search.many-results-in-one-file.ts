c9119cd04faee4e28be6afff155c4441ea36a8a1
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-02 15:01:47 +0100

    feature: add many results e2e test (#680)

--- packages/e2e/src/search.many-results-in-one-file.ts
+++ packages/e2e/src/search.many-results-in-one-file.ts
@@ -1,11 +1,11 @@
 import type { Test } from '@lvce-editor/test-with-playwright'
 
-export const name = 'search.one-result'
+export const name = 'search.many-results-in-one-file'
 
 export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
-  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc\nabc`)
   await Workspace.setPath(tmpDir)
   await SideBar.open('Search')
 
@@ -15,5 +15,5 @@ export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locat
   // assert
   const viewletSearch = Locator('.Search')
   const message = viewletSearch.locator('[role="status"]')
-  await expect(message).toHaveText('1 result in 1 file')
+  await expect(message).toHaveText('2 results in 1 file')
 }
