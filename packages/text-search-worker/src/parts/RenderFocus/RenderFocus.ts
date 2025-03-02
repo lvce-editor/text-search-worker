5806d934dd299b7e3ee35c6deefe9b7a1c9812eb
Le Vivilet <levivlet@pm.me>
2025-03-02 15:37:38 +0100

    lint

--- packages/text-search-worker/src/parts/RenderFocus/RenderFocus.ts
+++ packages/text-search-worker/src/parts/RenderFocus/RenderFocus.ts
@@ -1,5 +1,5 @@
-import * as GetFocusSelector from '../GetFocusSelector/GetFocusSelector.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
+import * as GetFocusSelector from '../GetFocusSelector/GetFocusSelector.ts'
 
 export const renderFocus = (oldState: SearchState, newState: SearchState): readonly any[] => {
   const selector = GetFocusSelector.getFocusSelector(newState.focus)
