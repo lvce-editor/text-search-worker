3ac2b0745bf42c4b1049ccf5d59695bd86457a44
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-03-29 11:04:26 +0100

    fix: static build (#718)

--- packages/build/src/build-static.js
+++ packages/build/src/build-static.js
@@ -1,7 +1,7 @@
 import { join } from 'node:path'
 import { pathToFileURL } from 'node:url'
 import { root } from './root.js'
-import { cp } from 'node:fs/promises'
+import { cp, readFile, writeFile } from 'node:fs/promises'
 
 const sharedProcessPath = join(root, 'packages', 'server', 'node_modules', '@lvce-editor', 'shared-process', 'index.js')
 
@@ -15,9 +15,23 @@ const { commitHash } = await sharedProcess.exportStatic({
   extensionPath: '',
 })
 
-await cp(
-  join(root, '.tmp', 'dist', 'dist', 'textSearchWorkerMain.js'),
-  join(root, 'dist', commitHash, 'packages', 'text-search-worker', 'dist', 'textSearchWorkerMain.js'),
-)
+const rendererWorkerPath = join(root, 'dist', commitHash, 'packages', 'renderer-worker', 'dist', 'rendererWorkerMain.js')
+
+export const getRemoteUrl = (path) => {
+  const url = pathToFileURL(path).toString().slice(8)
+  return `/remote/${url}`
+}
+
+const content = await readFile(rendererWorkerPath, 'utf8')
+const workerPath = join(root, '.tmp/dist/dist/textSearchWorkerMain.js')
+const remoteUrl = getRemoteUrl(workerPath)
+
+if (content.includes('// const textSearchWorkerUrl = ')) {
+  const occurrence = `// const textSearchWorkerUrl = \`\${assetDir}/packages/text-search-worker/dist/textSearchWorkerMain.js\`
+  const textSearchWorkerUrl = \`${remoteUrl}\``
+  const replacement = `const textSearchWorkerUrl = \`\${assetDir}/packages/text-search-worker/dist/textSearchWorkerMain.js\``
+  const newContent = content.replace(occurrence, replacement)
+  await writeFile(rendererWorkerPath, newContent)
+}
 
 await cp(join(root, 'dist'), join(root, '.tmp', 'static'), { recursive: true })
