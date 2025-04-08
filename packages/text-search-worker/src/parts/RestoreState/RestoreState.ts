384c27c851cb4ca48d99e188976fc84480d8c74e
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-08 20:52:47 +0200

    feature: move getSavedValues into separate file (#736)

--- packages/text-search-worker/src/parts/RestoreState/RestoreState.ts
+++ packages/text-search-worker/src/parts/RestoreState/RestoreState.ts
@@ -1,39 +1,5 @@
 import type { RestoredState } from '../RestoredState/RestoredState.ts'
-
-const getSavedValue = (savedState: unknown): string => {
-  if (savedState && typeof savedState === 'object' && 'value' in savedState && typeof savedState.value === 'string') {
-    return savedState.value
-  }
-  return ''
-}
-
-const getSavedReplacement = (savedState: unknown): string => {
-  if (savedState && typeof savedState === 'object' && 'replacement' in savedState && typeof savedState.replacement === 'string') {
-    return savedState.replacement
-  }
-  return ''
-}
-
-const getSavedFlags = (savedState: unknown): number => {
-  if (savedState && typeof savedState === 'object' && 'flags' in savedState && typeof savedState.flags === 'number') {
-    return savedState.flags
-  }
-  return 0
-}
-
-const getSavedIncludeValue = (savedState: unknown): string => {
-  if (savedState && typeof savedState === 'object' && 'includeValue' in savedState && typeof savedState.includeValue === 'string') {
-    return savedState.includeValue
-  }
-  return ''
-}
-
-const getSavedExcludeValue = (savedState: unknown): string => {
-  if (savedState && typeof savedState === 'object' && 'excludeValue' in savedState && typeof savedState.excludeValue === 'string') {
-    return savedState.excludeValue
-  }
-  return ''
-}
+import { getSavedValue, getSavedReplacement, getSavedFlags, getSavedIncludeValue, getSavedExcludeValue } from '../GetSavedValues/GetSavedValues.ts'
 
 export const restoreState = (savedState: unknown): RestoredState => {
   return {
