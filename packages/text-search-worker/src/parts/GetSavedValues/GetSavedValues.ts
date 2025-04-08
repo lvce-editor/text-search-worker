29588030ad1d0b89caed01a454f150a5b34bb9c3
Le Vivilet <levivlet@pm.me>
2025-04-08 20:49:18 +0200

    feature: move getSavedValues into separate file

---
+++ packages/text-search-worker/src/parts/GetSavedValues/GetSavedValues.ts
@@ -0,0 +1,34 @@
+export const getSavedValue = (savedState: unknown): string => {
+  if (savedState && typeof savedState === 'object' && 'value' in savedState && typeof savedState.value === 'string') {
+    return savedState.value
+  }
+  return ''
+}
+
+export const getSavedReplacement = (savedState: unknown): string => {
+  if (savedState && typeof savedState === 'object' && 'replacement' in savedState && typeof savedState.replacement === 'string') {
+    return savedState.replacement
+  }
+  return ''
+}
+
+export const getSavedFlags = (savedState: unknown): number => {
+  if (savedState && typeof savedState === 'object' && 'flags' in savedState && typeof savedState.flags === 'number') {
+    return savedState.flags
+  }
+  return 0
+}
+
+export const getSavedIncludeValue = (savedState: unknown): string => {
+  if (savedState && typeof savedState === 'object' && 'includeValue' in savedState && typeof savedState.includeValue === 'string') {
+    return savedState.includeValue
+  }
+  return ''
+}
+
+export const getSavedExcludeValue = (savedState: unknown): string => {
+  if (savedState && typeof savedState === 'object' && 'excludeValue' in savedState && typeof savedState.excludeValue === 'string') {
+    return savedState.excludeValue
+  }
+  return ''
+}
