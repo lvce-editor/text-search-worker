88e56767470a1066272d2c889083b034a2055947
Le Vivilet <levivlet@pm.me>
2025-04-08 20:50:35 +0200

    feature: move bulkreplacement into separate file

--- packages/text-search-worker/src/parts/ReplaceAll/ReplaceAll.ts
+++ packages/text-search-worker/src/parts/ReplaceAll/ReplaceAll.ts
@@ -1,12 +1,12 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import * as ApplyBulkReplacement from '../ApplyBulkReplacement/ApplyBulkReplacement.ts'
 import * as GetReplacedMessage from '../GetReplacedMessage/GetReplacedMessage.ts'
 import * as GetReplaceElements from '../GetReplaceElements/GetReplaceElements.ts'
-import * as ParentRpc from '../ParentRpc/ParentRpc.ts'
 
 export const replaceAll = async (state: SearchState): Promise<SearchState> => {
   const { workspacePath, items, replacement, matchCount } = state
   const bulkEdits = GetReplaceElements.getReplaceElements(items, workspacePath, replacement)
-  await ParentRpc.invoke('BulkReplacement.applyBulkReplacement', bulkEdits)
+  await ApplyBulkReplacement.applyBulkReplacement(bulkEdits)
   const fileCount = bulkEdits.length
   const message = GetReplacedMessage.getReplacedMessage(fileCount, matchCount, replacement)
   return {
