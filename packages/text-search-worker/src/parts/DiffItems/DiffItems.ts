20f4354cf17808aae8516248baeb376378a0bc1f
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-13 18:03:33 +0200

    feature: rerender header when focus changes (#771)

--- packages/text-search-worker/src/parts/DiffItems/DiffItems.ts
+++ packages/text-search-worker/src/parts/DiffItems/DiffItems.ts
@@ -5,18 +5,19 @@ export const diffType = DiffType.RenderItems
 
 export const isEqual = (oldState: SearchState, newState: SearchState): boolean => {
   return (
-    oldState.items === newState.items &&
-    oldState.minLineY === newState.minLineY &&
-    oldState.maxLineY === newState.maxLineY &&
-    oldState.replacement === newState.replacement &&
-    oldState.flags === newState.flags &&
-    oldState.message === newState.message &&
-    oldState.loaded === newState.loaded &&
     oldState.collapsedPaths === newState.collapsedPaths &&
-    oldState.listFocusedIndex === newState.listFocusedIndex &&
-    oldState.listFocused === newState.listFocused &&
+    oldState.deltaY === newState.deltaY &&
+    oldState.flags === newState.flags &&
+    oldState.focus === newState.focus &&
     oldState.icons === newState.icons &&
-    oldState.searchInputErrorMessage === newState.searchInputErrorMessage &&
-    oldState.deltaY === newState.deltaY
+    oldState.items === newState.items &&
+    oldState.listFocused === newState.listFocused &&
+    oldState.listFocusedIndex === newState.listFocusedIndex &&
+    oldState.loaded === newState.loaded &&
+    oldState.maxLineY === newState.maxLineY &&
+    oldState.message === newState.message &&
+    oldState.minLineY === newState.minLineY &&
+    oldState.replacement === newState.replacement &&
+    oldState.searchInputErrorMessage === newState.searchInputErrorMessage
   )
 }
