27caac851d79843b1aaf30e50976361aaad1c9eb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-13 19:57:20 +0200

    Discard changes to packages/text-search-worker/src/parts/DiffModules/DiffModules.ts

--- packages/text-search-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/text-search-worker/src/parts/DiffModules/DiffModules.ts
@@ -3,25 +3,22 @@ import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
 import * as DiffIncludeValue from '../DiffIncludeValue/DiffIncludeValue.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffReplaceValue from '../DiffReplaceValue/DiffReplaceValue.ts'
-import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
 export const modules = [
   DiffItems.isEqual,
-  DiffExcludeValue.isEqual,
-  DiffFocus.isEqual,
   DiffFocus.isEqual,
   DiffIncludeValue.isEqual,
+  DiffExcludeValue.isEqual,
   DiffReplaceValue.isEqual,
   DiffValue.isEqual,
 ]
 
 export const numbers = [
   DiffItems.diffType,
-  DiffExcludeValue.diffType,
   DiffFocus.diffType,
-  DiffType.RenderFocusContext,
   DiffIncludeValue.diffType,
+  DiffExcludeValue.diffType,
   DiffReplaceValue.diffType,
   DiffValue.diffType,
 ]
