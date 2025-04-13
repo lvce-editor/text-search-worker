0fbaca8eaa5c9ec2d0d415d47725eeec62e3adc6
Le Vivilet <levivlet@pm.me>
2025-04-13 18:15:52 +0200

    feature: update history on arrowDown

--- packages/text-search-worker/src/parts/NextHistoryResult/NextHistoryResult.ts
+++ packages/text-search-worker/src/parts/NextHistoryResult/NextHistoryResult.ts
@@ -1,7 +1,18 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import { getNextHistoryData } from '../GetNextHistoryData/GetNextHistoryData.ts'
+import * as HandleUpdate from '../HandleUpdate/HandleUpdate.ts'
+import * as InputSource from '../InputSource/InputSource.ts'
 
 export const nextHistoryResult = async (state: SearchState): Promise<SearchState> => {
-  return {
-    ...state,
+  const { history, historyIndex, value } = state
+  const { newValue, newHistoryIndex } = getNextHistoryData(history, historyIndex)
+  if (newValue === value) {
+    return state
   }
+
+  return HandleUpdate.handleUpdate(state, {
+    value: newValue,
+    inputSource: InputSource.Script,
+    historyIndex: newHistoryIndex,
+  })
 }
