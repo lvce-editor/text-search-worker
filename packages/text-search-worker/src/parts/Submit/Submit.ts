d73812599983c529466f4ff91d7fa5411c616ea8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-13 18:06:08 +0200

    feature: update history on submit (#775)

--- packages/text-search-worker/src/parts/Submit/Submit.ts
+++ packages/text-search-worker/src/parts/Submit/Submit.ts
@@ -2,6 +2,18 @@ import type { SearchState } from '../SearchState/SearchState.ts'
 import * as ViewletSearchHandleUpdate from '../HandleUpdate/HandleUpdate.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
+const getNewHistory = (history: readonly string[], newValue: string): readonly string[] => {
+  const newHistory = [...history, newValue]
+  const maxHistoryLength = 100
+  const cutHistory = newHistory.slice(0, maxHistoryLength)
+  return cutHistory
+}
+
 export const submit = (state: SearchState): Promise<SearchState> => {
-  return ViewletSearchHandleUpdate.handleUpdate(state, { value: state.value, inputSource: InputSource.User })
+  const newHistory = getNewHistory(state.history, state.value)
+  return ViewletSearchHandleUpdate.handleUpdate(state, {
+    value: state.value,
+    inputSource: InputSource.User,
+    history: newHistory,
+  })
 }
