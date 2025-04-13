b5adf5c336d19fe86c16e4808669385798cf4836
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-13 18:04:45 +0200

    feature: reset focus state on focusOut (#774)

--- packages/text-search-worker/src/parts/HandleHeaderFocusOut/HandleHeaderFocusOut.ts
+++ packages/text-search-worker/src/parts/HandleHeaderFocusOut/HandleHeaderFocusOut.ts
@@ -1,5 +1,12 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
 
 export const handleHeaderFocusOut = (state: SearchState): SearchState => {
-  return state
+  if (!state.focus) {
+    return state
+  }
+  return {
+    ...state,
+    focused: false,
+    focus: 0,
+  }
 }
