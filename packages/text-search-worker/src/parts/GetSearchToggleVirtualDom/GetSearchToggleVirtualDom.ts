7b83086a1a60171c7da6df3bcf8e2ac6c3dcf083
Le Vivilet <levivlet@pm.me>
2025-04-16 22:33:18 +0200

    lint

--- packages/text-search-worker/src/parts/GetSearchToggleVirtualDom/GetSearchToggleVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchToggleVirtualDom/GetSearchToggleVirtualDom.ts
@@ -1,9 +1,9 @@
+import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 import * as ClassNames from '../ClassNames/ClassNames.ts'
 import * as InputName from '../InputName/InputName.ts'
 import * as MergeClassNames from '../MergeClassNames/MergeClassNames.ts'
 import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
 import * as VirtualDomElements from '../VirtualDomElements/VirtualDomElements.ts'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
 
 export const getSearchToggleVirtualDom = (replaceExpanded: number): readonly VirtualDomNode[] => {
   return [
