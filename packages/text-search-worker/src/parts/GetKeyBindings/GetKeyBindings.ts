b7357fe3d3fd37cb061ca644d55735d7736b6b7c
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-26 20:48:06 +0200

    feature: split search header and list keybindings into seperate files (#810)

--- packages/text-search-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
+++ packages/text-search-worker/src/parts/GetKeyBindings/GetKeyBindings.ts
@@ -1,74 +1,7 @@
 import type { KeyBinding } from '../KeyBinding/KeyBinding.ts'
-import * as KeyCode from '../KeyCode/KeyCode.ts'
-import * as KeyModifier from '../KeyModifier/KeyModifier.ts'
-import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
+import * as GetSearchHeaderKeyBindings from '../GetSearchHeaderKeyBindings/GetSearchHeaderKeyBindings.ts'
+import * as GetSearchResultsKeyBindings from '../GetSearchResultsKeyBindings/GetSearchResultsKeyBindings.ts'
 
 export const getKeyBindings = (): readonly KeyBinding[] => {
-  return [
-    {
-      key: KeyModifier.CtrlCmd | KeyModifier.Alt | KeyCode.Enter,
-      command: 'Search.replaceAll',
-      when: WhenExpression.FocusSearch,
-    },
-    {
-      key: KeyCode.Tab,
-      command: 'Search.focusNextInput',
-      when: WhenExpression.FocusSearch,
-    },
-    {
-      key: KeyModifier.Shift | KeyCode.Tab,
-      command: 'Search.focusPreviousInput',
-      when: WhenExpression.FocusSearch,
-    },
-    {
-      key: KeyCode.UpArrow,
-      command: 'Search.previousHistoryResult',
-      when: WhenExpression.FocusSearchInput,
-    },
-    {
-      key: KeyCode.DownArrow,
-      command: 'Search.nextHistoryResult',
-      when: WhenExpression.FocusSearchInput,
-    },
-    {
-      key: KeyCode.DownArrow,
-      command: 'Search.focusNext',
-      when: WhenExpression.FocusSearchResults,
-    },
-    {
-      key: KeyCode.UpArrow,
-      command: 'Search.focusPrevious',
-      when: WhenExpression.FocusSearchResults,
-    },
-    {
-      key: KeyCode.Delete,
-      command: 'Search.dismissItem',
-      when: WhenExpression.FocusSearchResults,
-    },
-    {
-      key: KeyCode.Home,
-      command: 'Search.focusFirst',
-      when: WhenExpression.FocusSearchResults,
-    },
-    {
-      key: KeyCode.End,
-      command: 'Search.focusLast',
-      when: WhenExpression.FocusSearchResults,
-    },
-    {
-      key: KeyModifier.CtrlCmd | KeyCode.KeyC,
-      command: 'Search.copy',
-      when: WhenExpression.FocusSearchResults,
-    },
-    {
-      key: KeyCode.Enter,
-      command: 'Search.submit',
-      when: WhenExpression.FocusSearchInput,
-    },
-    {
-      key: KeyCode.Enter,
-      command: 'Search.submit',
-      when: WhenExpression.FocusSearchReplaceInput,
-    },
-  ]
+  return [...GetSearchHeaderKeyBindings.getSearchHeaderKeyBindings(), ...GetSearchResultsKeyBindings.getSearchResultKeyBindings()]
 }
---
+++ packages/text-search-worker/src/parts/GetSearchResultsKeyBindings/GetSearchResultsKeyBindings.ts
@@ -0,0 +1,39 @@
+import type { KeyBinding } from '../KeyBinding/KeyBinding.ts'
+import * as KeyCode from '../KeyCode/KeyCode.ts'
+import * as KeyModifier from '../KeyModifier/KeyModifier.ts'
+import * as WhenExpression from '../WhenExpression/WhenExpression.ts'
+
+export const getSearchResultKeyBindings = (): readonly KeyBinding[] => {
+  return [
+    {
+      key: KeyCode.DownArrow,
+      command: 'Search.focusNext',
+      when: WhenExpression.FocusSearchResults,
+    },
+    {
+      key: KeyCode.UpArrow,
+      command: 'Search.focusPrevious',
+      when: WhenExpression.FocusSearchResults,
+    },
+    {
+      key: KeyCode.Delete,
+      command: 'Search.dismissItem',
+      when: WhenExpression.FocusSearchResults,
+    },
+    {
+      key: KeyCode.Home,
+      command: 'Search.focusFirst',
+      when: WhenExpression.FocusSearchResults,
+    },
+    {
+      key: KeyCode.End,
+      command: 'Search.focusLast',
+      when: WhenExpression.FocusSearchResults,
+    },
+    {
+      key: KeyModifier.CtrlCmd | KeyCode.KeyC,
+      command: 'Search.copy',
+      when: WhenExpression.FocusSearchResults,
+    },
+  ]
+}
