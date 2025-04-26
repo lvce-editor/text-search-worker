73cb45b32cc45a5086ce01c1e60de03e6a336f2d
Le Vivilet <levivlet@pm.me>
2025-04-26 20:57:17 +0200

    render focus

--- packages/text-search-worker/src/parts/DiffModules/DiffModules.ts
+++ packages/text-search-worker/src/parts/DiffModules/DiffModules.ts
@@ -1,8 +1,10 @@
 import * as DiffExcludeValue from '../DiffExcludeValue/DiffExcludeValue.ts'
 import * as DiffFocus from '../DiffFocus/DiffFocus.ts'
+import * as DiffFocusContext from '../DiffFocusContext/DiffFocusContext.ts'
 import * as DiffIncludeValue from '../DiffIncludeValue/DiffIncludeValue.ts'
 import * as DiffItems from '../DiffItems/DiffItems.ts'
 import * as DiffReplaceValue from '../DiffReplaceValue/DiffReplaceValue.ts'
+import * as DiffType from '../DiffType/DiffType.ts'
 import * as DiffValue from '../DiffValue/DiffValue.ts'
 
 export const modules = [
@@ -12,6 +14,7 @@ export const modules = [
   DiffExcludeValue.isEqual,
   DiffReplaceValue.isEqual,
   DiffValue.isEqual,
+  DiffFocusContext.isEqual,
 ]
 
 export const numbers = [
@@ -21,4 +24,5 @@ export const numbers = [
   DiffExcludeValue.diffType,
   DiffReplaceValue.diffType,
   DiffValue.diffType,
+  DiffType.RenderFocusContext,
 ]
