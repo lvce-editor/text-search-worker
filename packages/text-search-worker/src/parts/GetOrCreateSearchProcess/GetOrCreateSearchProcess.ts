afc7aa1fb60fabdf49c0dcb9fe906b0c34db8b73
Le Vivilet <levivlet@pm.me>
2025-05-04 15:25:14 +0200

    feature: use rpc type

--- packages/text-search-worker/src/parts/GetOrCreateSearchProcess/GetOrCreateSearchProcess.ts
+++ packages/text-search-worker/src/parts/GetOrCreateSearchProcess/GetOrCreateSearchProcess.ts
@@ -1,14 +1,15 @@
+import type { Rpc } from '@lvce-editor/rpc'
 import * as LaunchSearchProcess from '../LaunchSearchProcess/LaunchSearchProcess.ts'
 
 interface State {
-  rpc: any
+  rpc: Promise<Rpc> | undefined
 }
 
 const state: State = {
   rpc: undefined,
 }
 
-export const getOrCreate = (): Promise<any> => {
+export const getOrCreate = (): Promise<Rpc> => {
   if (!state.rpc) {
     // @ts-ignore
     state.rpc = LaunchSearchProcess.launchSearchProcess()
