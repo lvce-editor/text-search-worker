5d5d304a074f5df34231779679fed3a0bf47e35e
Le Vivilet <levivlet@pm.me>
2025-05-19 22:51:44 +0200

    lint

--- packages/text-search-worker/src/parts/EnsureDenseTree/EnsureDenseTree.ts
+++ packages/text-search-worker/src/parts/EnsureDenseTree/EnsureDenseTree.ts
@@ -1,6 +1,6 @@
-import * as Path from '../Path/Path.ts'
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import type { Tree } from '../Tree/Tree.ts'
+import * as Path from '../Path/Path.ts'
 
 export const createFolderTree = (folders: readonly string[]): Tree => {
   const denseTree: Record<string, SearchResult[]> = Object.create(null)
