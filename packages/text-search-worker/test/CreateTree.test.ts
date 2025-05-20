d82a8b41f0cbf1ce0dbc36d87198c24d70ca99c0
Le Vivilet <levivlet@pm.me>
2025-05-20 18:48:55 +0200

    test

--- packages/text-search-worker/test/CreateTree.test.ts
+++ packages/text-search-worker/test/CreateTree.test.ts
@@ -121,3 +121,75 @@ test('createTree handles single file with no matches', () => {
     'file1.ts': [],
   })
 })
+
+test('createParentFolderTree creates tree for deeply nested folders', () => {
+  const results = [
+    { type: TextSearchResultType.File, text: 'src/folder1/folder2/folder3/file.ts', start: 0, end: 0, lineNumber: 0 },
+    { type: TextSearchResultType.Match, text: 'match1', start: 0, end: 6, lineNumber: 1 },
+  ]
+
+  const tree = createParentFolderTree(results)
+
+  expect(tree).toEqual({
+    '': [],
+    'src/folder1/folder2/folder3': [
+      {
+        end: 0,
+        lineNumber: 0,
+        start: 0,
+        text: 'src/folder1/folder2/folder3/file.ts',
+        type: 1,
+      },
+    ],
+    'src/folder1/folder2/folder3/file.ts': [
+      {
+        end: 6,
+        lineNumber: 1,
+        start: 0,
+        text: 'match1',
+        type: 2,
+      },
+    ],
+  })
+})
+
+test.skip('createParentFolderTree creates tree for multiple files in same folder', () => {
+  const results = [
+    { type: TextSearchResultType.File, text: 'src/folder/file1.ts', start: 0, end: 0, lineNumber: 0 },
+    { type: TextSearchResultType.Match, text: 'match1', start: 0, end: 6, lineNumber: 1 },
+    { type: TextSearchResultType.File, text: 'src/folder/file2.ts', start: 0, end: 0, lineNumber: 2 },
+    { type: TextSearchResultType.Match, text: 'match2', start: 0, end: 6, lineNumber: 3 },
+  ]
+
+  const tree = createParentFolderTree(results)
+
+  expect(tree['']).toEqual([{ type: TextSearchResultType.File, text: 'src', start: 0, end: 0, lineNumber: 0 }])
+  expect(tree['src']).toEqual([{ type: TextSearchResultType.File, text: 'src/folder', start: 0, end: 0, lineNumber: 0 }])
+  expect(tree['src/folder']).toEqual([
+    { type: TextSearchResultType.File, text: 'src/folder/file1.ts', start: 0, end: 0, lineNumber: 0 },
+    { type: TextSearchResultType.File, text: 'src/folder/file2.ts', start: 0, end: 0, lineNumber: 2 },
+  ])
+  expect(tree['src/folder/file1.ts']).toEqual([{ type: TextSearchResultType.Match, text: 'match1', start: 0, end: 6, lineNumber: 1 }])
+  expect(tree['src/folder/file2.ts']).toEqual([{ type: TextSearchResultType.Match, text: 'match2', start: 0, end: 6, lineNumber: 3 }])
+})
+
+test.skip('createParentFolderTree creates tree for files in different folders', () => {
+  const results = [
+    { type: TextSearchResultType.File, text: 'src/folder1/file1.ts', start: 0, end: 0, lineNumber: 0 },
+    { type: TextSearchResultType.Match, text: 'match1', start: 0, end: 6, lineNumber: 1 },
+    { type: TextSearchResultType.File, text: 'src/folder2/file2.ts', start: 0, end: 0, lineNumber: 2 },
+    { type: TextSearchResultType.Match, text: 'match2', start: 0, end: 6, lineNumber: 3 },
+  ]
+
+  const tree = createParentFolderTree(results)
+
+  expect(tree['']).toEqual([{ type: TextSearchResultType.File, text: 'src', start: 0, end: 0, lineNumber: 0 }])
+  expect(tree['src']).toEqual([
+    { type: TextSearchResultType.File, text: 'src/folder1', start: 0, end: 0, lineNumber: 0 },
+    { type: TextSearchResultType.File, text: 'src/folder2', start: 0, end: 0, lineNumber: 2 },
+  ])
+  expect(tree['src/folder1']).toEqual([{ type: TextSearchResultType.File, text: 'src/folder1/file1.ts', start: 0, end: 0, lineNumber: 0 }])
+  expect(tree['src/folder2']).toEqual([{ type: TextSearchResultType.File, text: 'src/folder2/file2.ts', start: 0, end: 0, lineNumber: 2 }])
+  expect(tree['src/folder1/file1.ts']).toEqual([{ type: TextSearchResultType.Match, text: 'match1', start: 0, end: 6, lineNumber: 1 }])
+  expect(tree['src/folder2/file2.ts']).toEqual([{ type: TextSearchResultType.Match, text: 'match2', start: 0, end: 6, lineNumber: 3 }])
+})
