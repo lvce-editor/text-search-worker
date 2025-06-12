6a03f253c6e009711bea06e320075e0c20bb2a99
Le Vivilet <levivlet@pm.me>
2025-06-12 19:43:01 +0200

    memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,7 +3,7 @@ import { root } from './root.ts'
 
 export const threshold = 475_000
 
-export const instantiations = 7000
+export const instantiations = 30_000
 
 export const instantiationsPath = join(root, 'packages', 'text-search-worker')
 
