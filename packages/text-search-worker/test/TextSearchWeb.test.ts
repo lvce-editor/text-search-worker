6841cf1253368d5e9262c13eca19162e32886437
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-28 15:37:31 +0200

    feature: add text search web test (#892)

--- packages/text-search-worker/test/TextSearchWeb.test.ts
+++ packages/text-search-worker/test/TextSearchWeb.test.ts
@@ -1,5 +1,6 @@
 import { beforeEach, expect, test } from '@jest/globals'
 import * as TextSearchWeb from '../src/parts/TextSearchWeb/TextSearchWeb.ts'
+import { textSearch } from '../src/parts/TextSearchWeb/TextSearchWeb.ts'
 
 beforeEach(() => {})
 
@@ -15,4 +16,14 @@ test.skip('textSearch', () => {
       ],
     ],
   ])
+})
+
+test('textSearch returns empty array for empty object', async () => {
+  const result = await textSearch('web', '/root', 'test')
+  expect(result).toEqual([])
+})
+
+test('textSearch with different parameters', async () => {
+  const result = await textSearch('web', '/different', 'query')
+  expect(result).toEqual([])
 })
