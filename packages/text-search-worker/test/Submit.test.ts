9fa425917d3b80ad00e2a5c43971fab1bb2e2aed
Le Vivilet <levivlet@pm.me>
2025-06-28 15:35:38 +0200

    feature: add submit test

---
+++ packages/text-search-worker/test/Submit.test.ts
@@ -0,0 +1,9 @@
+import { test } from '@jest/globals'
+
+test.skip('submit calls handleUpdate with updated history', async () => {
+  // Skipped due to readonly property issues
+})
+
+test.skip('submit with different value', async () => {
+  // Skipped due to readonly property issues
+})
