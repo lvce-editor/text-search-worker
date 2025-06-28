31de75db9cdf13bada41f3f579ca1dc75b8d5a3b
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-28 16:56:05 +0200

    feature: simplify render test (#918)
    * feature: simplify render test
    
    * feature: simplify render test
    
    * test
    
    * lint

--- packages/text-search-worker/test/Render.test.ts
+++ packages/text-search-worker/test/Render.test.ts
@@ -1,160 +1,69 @@
-import { expect, jest, test, beforeEach } from '@jest/globals'
+import { expect, test } from '@jest/globals'
+import * as Create from '../src/parts/Create/Create.ts'
 import * as InputSource from '../src/parts/InputSource/InputSource.ts'
-
-const mockGetSearchDisplayResults = {
-  getDisplayResults: jest.fn(),
-}
-
-const mockGetSearchVirtualDom = {
-  getSearchVirtualDom: jest.fn(),
-}
-
-const mockGetFocusSelector = {
-  getFocusSelector: jest.fn(),
-}
-
-const mockSearchViewStates = {
-  get: jest.fn(),
-}
-
-jest.unstable_mockModule('../src/parts/GetSearchDisplayResults/GetSearchDisplayResults.ts', () => mockGetSearchDisplayResults)
-jest.unstable_mockModule('../src/parts/GetSearchVirtualDom/GetSearchVirtualDom.ts', () => mockGetSearchVirtualDom)
-jest.unstable_mockModule('../src/parts/GetFocusSelector/GetFocusSelector.ts', () => mockGetFocusSelector)
-jest.unstable_mockModule('../src/parts/SearchViewStates/SearchViewStates.ts', () => mockSearchViewStates)
-
-const Render = await import('../src/parts/Render/Render.ts')
-
-beforeEach(() => {
-  jest.resetAllMocks()
-})
+import * as Render from '../src/parts/Render/Render.ts'
+import * as SearchViewStates from '../src/parts/SearchViewStates/SearchViewStates.ts'
+import * as TextSearchResultType from '../src/parts/TextSearchResultType/TextSearchResultType.ts'
+import * as WhenExpression from '../src/parts/WhenExpression/WhenExpression.ts'
 
 test('doRender - no changes returns empty commands', () => {
-  const oldState = {
-    items: [],
-    minLineY: 0,
-    maxLineY: 0,
-    replacement: '',
-    flags: 0,
-    message: '',
-    loaded: false,
-    collapsedPaths: [],
-    listFocusedIndex: -1,
-    listFocused: false,
-    icons: [],
-    value: '',
-    includeValue: '',
-    excludeValue: '',
-    focus: '',
-    focusSource: InputSource.Script,
-    inputSource: InputSource.Script,
-  }
-  mockSearchViewStates.get.mockReturnValue({ oldState, newState: oldState })
+  const oldState = Create.create(1, 0, 0, 0, 0, '', '')
+  SearchViewStates.set(1, oldState, oldState)
 
   const commands = Render.doRender(1)
   expect(commands).toEqual([])
 })
 
-test.skip('doRender - renders items when changed', () => {
-  const oldState = {
-    items: [],
-    minLineY: 0,
-    maxLineY: 0,
-    replacement: '',
-    flags: 0,
-    message: '',
-    loaded: false,
-    collapsedPaths: [],
-    listFocusedIndex: -1,
-    listFocused: false,
-    icons: [],
-    value: '',
-    includeValue: '',
-    excludeValue: '',
-    focus: '',
-    focusSource: InputSource.Script,
-    inputSource: InputSource.Script,
-  }
+test('doRender - renders items when changed', () => {
+  const oldState = Create.create(1, 0, 0, 0, 0, '', '')
   const newState = {
     ...oldState,
-    items: ['item1'],
-    uid: 1,
-    itemHeight: 22,
-    fileCount: 1,
+    items: [{ type: TextSearchResultType.File, text: 'item1', start: 0, lineNumber: 0, end: 0 }],
+    listItems: [{ type: TextSearchResultType.File, text: 'item1', start: 0, lineNumber: 0, end: 0 }],
     value: 'test',
-    listItems: ['item1'],
+    fileCount: 1,
   }
-  mockSearchViewStates.get.mockReturnValue({ oldState, newState })
-  mockGetSearchDisplayResults.getDisplayResults.mockReturnValue(['displayResult1'])
-  mockGetSearchVirtualDom.getSearchVirtualDom.mockReturnValue(['virtualDom1'])
+  SearchViewStates.set(1, oldState, newState)
 
   const commands = Render.doRender(1)
+  // Only one command is expected: Viewlet.setDom2 with the dom structure
   expect(commands).toEqual([
-    ['Viewlet.setDom2', 1, ['virtualDom1']],
-    ['Viewlet.send', 1, 'setValue', 'test', '[name="SearchValue"]'],
+    [
+      'Viewlet.setDom2',
+      1,
+      expect.any(Array), // dom structure, don't check deeply
+    ],
   ])
 })
 
-test.skip('doRender - renders focus when changed', () => {
-  const oldState = {
-    items: [],
-    minLineY: 0,
-    maxLineY: 0,
-    replacement: '',
-    flags: 0,
-    message: '',
-    loaded: false,
-    collapsedPaths: [],
-    listFocusedIndex: -1,
-    listFocused: false,
-    icons: [],
-    value: '',
-    includeValue: '',
-    excludeValue: '',
-    focus: '',
-    focusSource: InputSource.Script,
-    inputSource: InputSource.Script,
-    uid: 1,
-  }
+test('doRender - renders focus when changed', () => {
+  const oldState = Create.create(1, 0, 0, 0, 0, '', '')
   const newState = {
     ...oldState,
-    focus: 'newFocus',
+    focus: WhenExpression.FocusSearchInput,
     focusSource: InputSource.Script,
   }
-  mockSearchViewStates.get.mockReturnValue({ oldState, newState })
-  mockGetFocusSelector.getFocusSelector.mockReturnValue('focusSelector')
+  SearchViewStates.set(1, oldState, newState)
 
   const commands = Render.doRender(1)
-  expect(commands).toEqual([['Viewlet.focusElementByName', 1, 'focusSelector']])
+  expect(commands).toHaveLength(3)
+  expect(commands[1][0]).toBe('Viewlet.focusElementByName')
+  expect(commands[1][1]).toBe(1)
+  expect(commands[1][2]).toBe('SearchValue')
 })
 
-test.skip('doRender - renders value when changed', () => {
-  const oldState = {
-    items: [],
-    minLineY: 0,
-    maxLineY: 0,
-    replacement: '',
-    flags: 0,
-    message: '',
-    loaded: false,
-    collapsedPaths: [],
-    listFocusedIndex: -1,
-    listFocused: false,
-    icons: [],
-    value: '',
-    includeValue: '',
-    excludeValue: '',
-    focus: '',
-    focusSource: InputSource.Script,
-    inputSource: InputSource.Script,
-  }
+test('doRender - renders value when changed', () => {
+  const oldState = Create.create(1, 0, 0, 0, 0, '', '')
   const newState = {
     ...oldState,
     value: 'newValue',
-    uid: 1,
     inputSource: InputSource.Script,
   }
-  mockSearchViewStates.get.mockReturnValue({ oldState, newState })
+  SearchViewStates.set(1, oldState, newState)
 
   const commands = Render.doRender(1)
-  expect(commands).toEqual([['Viewlet.send', 1, 'setValue', 'newValue', '[name="SearchValue"]']])
+  expect(commands).toHaveLength(1)
+  expect(commands[0][0]).toBe('Viewlet.setValueByName')
+  expect(commands[0][1]).toBe('SearchValue')
+  expect(commands[0][2]).toBe('newValue')
 })
