5fb2b47eb6d8c5df7ab0db2ef5d85f51fec690a9
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-06-28 22:53:53 +0200

    feature: add createLazyRpc function (#930)
    * feature: add createLazyRpc function
    
    * lint
    
    * lint

---
+++ packages/text-search-worker/src/parts/CreateLazyRpc/CreateLazyRpc.ts
@@ -0,0 +1,32 @@
+import type { Rpc } from '@lvce-editor/rpc'
+import * as RpcRegistry from '../RpcRegistry/RpcRegistry.ts'
+
+interface LazyRpc {
+  setFactory: (value: () => Promise<Rpc>) => void
+  invoke: (method: string, ...params: readonly any[]) => Promise<any>
+}
+
+export const createLazyRpc = (rpcId: number): LazyRpc => {
+  let rpcPromise: Promise<void> | undefined
+  let factory: () => Promise<Rpc>
+  const createRpc = async (): Promise<void> => {
+    const rpc = await factory()
+    RpcRegistry.set(rpcId, rpc)
+  }
+  const ensureRpc = async (): Promise<void> => {
+    if (!rpcPromise) {
+      rpcPromise = createRpc()
+    }
+    await rpcPromise
+  }
+  return {
+    setFactory(value: () => Promise<Rpc>): void {
+      factory = value
+    },
+    async invoke(method: string, ...params: readonly any[]): Promise<any> {
+      await ensureRpc()
+      // @ts-ignore
+      return invoke(method, ...params)
+    },
+  }
+}
