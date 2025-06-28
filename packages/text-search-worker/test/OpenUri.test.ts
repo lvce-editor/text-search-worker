acd7ec880c392a4d178d4d54a00601846384c2f3
Le Vivilet <levivlet@pm.me>
2025-06-28 15:58:50 +0200

    feature: simplify openUri test

--- packages/text-search-worker/test/OpenUri.test.ts
+++ packages/text-search-worker/test/OpenUri.test.ts
@@ -1,51 +1,68 @@
-import { beforeEach, expect, jest, test } from '@jest/globals'
-
-beforeEach(() => {
-  jest.resetAllMocks()
-})
-
-jest.unstable_mockModule('../src/parts/RendererWorker/RendererWorker.ts', () => {
-  return {
-    invoke: jest.fn(() => {
-      throw new Error('not implemented')
-    }),
-  }
-})
-
-const OpenUri = await import('../src/parts/OpenUri/OpenUri.ts')
-const Rpc = await import('../src/parts/RendererWorker/RendererWorker.ts')
+import { expect, test } from '@jest/globals'
+import { MockRpc } from '@lvce-editor/rpc'
+import * as OpenUri from '../src/parts/OpenUri/OpenUri.ts'
+import * as RpcId from '../src/parts/RpcId/RpcId.ts'
+import * as RpcRegistry from '../src/parts/RpcRegistry/RpcRegistry.ts'
 
 test('openUri - without options', async () => {
-  // @ts-ignore
-  Rpc.invoke.mockResolvedValue(undefined)
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Main.openUri') {
+        return Promise.resolve(undefined)
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+
   await OpenUri.openUri('/test/file.txt')
-  expect(Rpc.invoke).toHaveBeenCalledTimes(1)
-  expect(Rpc.invoke).toHaveBeenCalledWith('Main.openUri', '/test/file.txt', true, {})
 })
 
 test('openUri - with preview', async () => {
-  // @ts-ignore
-  Rpc.invoke.mockImplementation(() => {})
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Main.openUri') {
+        return Promise.resolve(undefined)
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+
   await OpenUri.openUri('/test/file.txt', true)
-  expect(Rpc.invoke).toHaveBeenCalledTimes(1)
-  expect(Rpc.invoke).toHaveBeenCalledWith('Main.openUri', '/test/file.txt', true, {})
 })
 
 test('openUri - with options', async () => {
-  // @ts-ignore
-  Rpc.invoke.mockImplementation(() => {})
+  const mockRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Main.openUri') {
+        return Promise.resolve(undefined)
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
+  })
+  RpcRegistry.set(RpcId.RendererWorker, mockRpc)
+
   const options = {
     selections: new Uint32Array([1, 0, 1, 0]),
   }
   await OpenUri.openUri('/test/file.txt', false, options)
-  expect(Rpc.invoke).toHaveBeenCalledTimes(1)
-  expect(Rpc.invoke).toHaveBeenCalledWith('Main.openUri', '/test/file.txt', false, options)
 })
 
 test('openUri - error', async () => {
-  // @ts-ignore
-  Rpc.invoke.mockImplementation(() => {
-    throw new Error('Failed to open file')
+  const errorRpc = MockRpc.create({
+    commandMap: {},
+    invoke: (method: string) => {
+      if (method === 'Main.openUri') {
+        throw new Error('Failed to open file')
+      }
+      throw new Error(`unexpected method ${method}`)
+    },
   })
+  RpcRegistry.set(RpcId.RendererWorker, errorRpc)
+
   await expect(OpenUri.openUri('/test/file.txt')).rejects.toThrow('Failed to open file')
 })
