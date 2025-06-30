364f890a81178921cbcbca1ffc43afed8d148e1a
Le Vivilet <levivlet@pm.me>
2025-06-30 11:22:13 +0200

    feature: register text search providers

--- packages/text-search-worker/src/parts/Listen/Listen.ts
+++ packages/text-search-worker/src/parts/Listen/Listen.ts
@@ -1,10 +1,13 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
-import * as ParentRpc from '../RendererWorker/RendererWorker.ts'
+import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
+import * as TextSearchProviderMap from '../TextSearchProviderMap/TextSearchProviderMap.ts'
+import * as TextSearchProviders from '../TextSearchProviders/TextSearchProviders.ts'
 
 export const listen = async (): Promise<void> => {
+  TextSearchProviders.add(TextSearchProviderMap.textSearchProviderMap)
   const rpc = await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
-  ParentRpc.set(rpc)
+  RendererWorker.set(rpc)
 }
