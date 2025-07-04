ac70918502d02c8469bc28f5faa5ce68cb1ddf36
Le Vivilet <levivlet@pm.me>
2025-07-04 12:25:28 +0200

    feature: remove unused code

--- packages/text-search-worker/src/parts/Arrays/Arrays.ts
+++ packages/text-search-worker/src/parts/Arrays/Arrays.ts
@@ -1,13 +1,5 @@
 // based on https://github.com/microsoft/vscode/blob/c0769274fa136b45799edeccc0d0a2f645b75caf/src/vs/base/common/arrays.ts#L625 (License MIT)
 
-export const isLastIndex = <T>(array: readonly T[], index: number): boolean => {
-  return index === array.length - 1
-}
-
-export const lastIndex = <T>(array: readonly T[]): number => {
-  return array.length - 1
-}
-
 export const remove = <T>(array: readonly T[], index: number, deleteCount: number): readonly T[] => {
   return array.toSpliced(index, deleteCount)
 }
--- packages/text-search-worker/src/parts/Path/Path.ts
+++ packages/text-search-worker/src/parts/Path/Path.ts
@@ -1,4 +1,4 @@
-export const dirname = (pathSeparator: string, path: string): string => {
+const dirname = (pathSeparator: string, path: string): string => {
   const index = path.lastIndexOf(pathSeparator)
   if (index === -1) {
     return path
@@ -8,14 +8,6 @@ export const dirname = (pathSeparator: string, path: string): string => {
 
 export const dirname2 = (path: string): string => {
   return dirname('/', path)
-}
-
-export const join = (pathSeparator: string, ...parts: readonly string[]): string => {
-  return parts.join(pathSeparator)
-}
-
-export const getBaseName = (pathSeparator: string, path: string): string => {
-  return path.slice(path.lastIndexOf(pathSeparator) + 1)
 }
 
 export const join2 = (path: string, childPath: string): string => {
