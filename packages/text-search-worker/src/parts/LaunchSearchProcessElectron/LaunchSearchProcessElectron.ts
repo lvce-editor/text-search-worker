a0ee6dde68059048f90b8c73eabb5faaa7dd4adf
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-07-04 12:25:14 +0200

    feature: simplify electron rpc (#958)

--- packages/text-search-worker/src/parts/LaunchSearchProcessElectron/LaunchSearchProcessElectron.ts
+++ packages/text-search-worker/src/parts/LaunchSearchProcessElectron/LaunchSearchProcessElectron.ts
@@ -2,18 +2,10 @@ import type { Rpc } from '@lvce-editor/rpc'
 import { TransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
-const send = async (port: MessagePort): Promise<void> => {
-  await RendererWorker.invokeAndTransfer(
-    'SendMessagePortToElectron.sendMessagePortToElectron',
-    port,
-    'HandleMessagePortForSearchProcess.handleMessagePortForSearchProcess',
-  )
-}
-
 export const launchSearchProcessElectron = async (): Promise<Rpc> => {
   const rpc = await TransferMessagePortRpcParent.create({
     commandMap: {},
-    send,
+    send: RendererWorker.sendMessagePortToSearchProcess,
   })
   return rpc
 }
