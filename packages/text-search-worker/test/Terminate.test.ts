b76033d7bad2a0ad76a98a75a21467c04866d2de
Le Vivilet <levivlet@pm.me>
2025-07-04 12:32:32 +0200

    feature: simplify terminate code

--- packages/text-search-worker/test/Terminate.test.ts
+++
@@ -1,9 +0,0 @@
-import { expect, jest, test } from '@jest/globals'
-import * as Terminate from '../src/parts/Terminate/Terminate.ts'
-
-test('terminate', () => {
-  const mockClose = jest.fn()
-  globalThis.close = mockClose
-  Terminate.terminate()
-  expect(mockClose).toHaveBeenCalled()
-})
