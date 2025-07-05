9d9b9e3d827652c08c26a55c21e6efed3d94b956
Le Vivilet <levivlet@pm.me>
2025-07-05 10:58:19 +0200

    feature: remove unused code

--- packages/text-search-worker/src/parts/GetWebSocketUrl/GetWebSocketUrl.ts
+++
@@ -1,6 +0,0 @@
-import * as WebSocketProtocol from '../WebSocketProtocol/WebSocketProtocol.ts'
-
-export const getWebSocketUrl = (type: string, host: string, locationProtocol: string): string => {
-  const wsProtocol = WebSocketProtocol.getWebSocketProtocol(locationProtocol)
-  return `${wsProtocol}//${host}/websocket/${type}`
-}
--- packages/text-search-worker/src/parts/WebSocketProtocol/WebSocketProtocol.ts
+++
@@ -1,5 +0,0 @@
-import * as Protocol from '../Protocol/Protocol.ts'
-
-export const getWebSocketProtocol = (locationProtocol: string): string => {
-  return locationProtocol === Protocol.Https ? Protocol.Wss : Protocol.Ws
-}
