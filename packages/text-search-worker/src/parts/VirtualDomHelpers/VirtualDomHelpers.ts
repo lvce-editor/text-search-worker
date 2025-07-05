582f944507129ca8ee92556a435ea5478471b51b
Le Vivilet <levivlet@pm.me>
2025-07-05 10:57:35 +0200

    feature: use helper function

--- packages/text-search-worker/src/parts/VirtualDomHelpers/VirtualDomHelpers.ts
+++ packages/text-search-worker/src/parts/VirtualDomHelpers/VirtualDomHelpers.ts
@@ -1,10 +1,1 @@
-import { VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
-import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
-
-export const text = (data: string): VirtualDomNode => {
-  return {
-    type: VirtualDomElements.Text,
-    text: data,
-    childCount: 0,
-  }
-}
+export { text } from '@lvce-editor/virtual-dom-worker'
