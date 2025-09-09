3514b686cd2d3fb542bf4850d070477ed301c30c
Le Vivilet <levivilet@pm.me>
2025-09-09 12:40:42 +0200

    feature: improve limit e2e test

--- packages/e2e/src/search.limit-hit.ts
+++ packages/e2e/src/search.limit-hit.ts
@@ -4,18 +4,22 @@ export const name = 'search.limit-hit'
 
 export const skip = 1
 
-export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+export const test: Test = async ({ Command, Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
-  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc\n`.repeat(1000))
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc\n`.repeat(10))
   await Workspace.setPath(tmpDir)
   await SideBar.open('Search')
-
-  // act
   await Search.setValue('ab')
-
-  // assert
   const viewletSearch = Locator('.Search')
   const message = viewletSearch.locator('[role="status"]')
-  await expect(message).toHaveText('1000 result in 1 file')
+  await expect(message).toHaveText('10 result in 1 file')
+
+  // act
+  await Command.execute('Search.setLimit', 5)
+
+  // assert
+  const warningMessage = Locator('.SearchWarningMessage')
+  await expect(warningMessage).toBeVisible()
+  await expect(warningMessage).toHaveText('Limit was hit')
 }
