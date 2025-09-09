2577c316de1a73c2c915e79f5dec9073d114f884
Le Vivilet <levivilet@pm.me>
2025-09-09 09:28:44 +0200

    feature: add limit hit e2e test

---
+++ packages/e2e/src/search.limit-hit.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'search.limit-hit'
+
+export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+
+  // act
+  await Search.setValue('ab')
+
+  // assert
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result in 1 file')
+}
