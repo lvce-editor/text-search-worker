f90cf97ed38ea37bd85d21563f5d4069803a96c0
Le Vivilet <levivilet@pm.me>
2025-09-15 19:22:43 +0200

    feature: enable limit e2e test

--- packages/e2e/src/search.limit-hit.ts
+++ packages/e2e/src/search.limit-hit.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.limit-hit'
 
-export const skip = 1
-
 export const test: Test = async ({ Command, Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
