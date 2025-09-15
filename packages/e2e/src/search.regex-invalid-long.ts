11b97c694060769ef976a5c48ea880fd112b027e
Le Vivilet <levivilet@pm.me>
2025-09-15 22:08:02 +0200

    tsc

--- packages/e2e/src/search.regex-invalid-long.ts
+++ packages/e2e/src/search.regex-invalid-long.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.regex-invalid-long'
 
-export const test: Test = async ({ Platform, Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
   await FileSystem.writeFile(`${tmpDir}/test.css`, `Abc`)
