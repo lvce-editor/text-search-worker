e116fe183e7bc96dc042cf57f9bc95449417440d
Le Vivilet <levivilet@pm.me>
2025-09-26 21:20:49 +0200

    feature: register blur command (#1064)

---
+++ packages/text-search-worker/src/parts/HandleInputBlur/HandleInputBlur.ts
@@ -0,0 +1,5 @@
+import type { SearchState } from '../SearchState/SearchState.ts'
+
+export const handleInputBlur = async (state: SearchState, name: string): Promise<SearchState> => {
+  return state
+}
