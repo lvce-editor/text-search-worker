4d43498ef9bfce96ba1f8315207a2aa4ab73ba91
Le Vivilet <levivilet@pm.me>
2025-09-26 23:13:05 +0200

    feature: send uid when updating focus context (#1076)

--- packages/text-search-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
+++ packages/text-search-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
@@ -1,5 +1,5 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
 
 export const renderFocusContext = (oldState: SearchState, newState: SearchState): readonly any[] => {
-  return ['Viewlet.setFocusContext', newState.focus]
+  return ['Viewlet.setFocusContext', newState.uid, newState.focus]
 }
