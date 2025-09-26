4e7713325fc85e4cd37a749d6a0d51534294fa61
Le Vivilet <levivilet@pm.me>
2025-09-26 23:15:58 +0200

    feature: use viewlet command

--- packages/text-search-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
+++ packages/text-search-worker/src/parts/RenderFocusContext/RenderFocusContext.ts
@@ -1,5 +1,6 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import type { SearchState } from '../SearchState/SearchState.ts'
 
 export const renderFocusContext = (oldState: SearchState, newState: SearchState): readonly any[] => {
-  return ['Viewlet.setFocusContext', newState.uid, newState.focus]
+  return [ViewletCommand.SetFocusContext, newState.uid, newState.focus]
 }
