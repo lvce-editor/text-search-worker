fb35a386d18855fc120c654f3b3f7d46e7d1d687
Le Vivilet <levivilet@pm.me>
2025-09-26 12:59:41 +0200

    feature: implement basic collapseAll function (#1040)
    * feature: implement basic collapseAll function
    
    * test
    
    * lint

--- packages/text-search-worker/src/parts/CollapseAll/CollapseAll.ts
+++ packages/text-search-worker/src/parts/CollapseAll/CollapseAll.ts
@@ -1,5 +1,25 @@
+import type { SearchResult } from '@lvce-editor/rpc-registry'
 import type { SearchState } from '../SearchState/SearchState.ts'
+import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
+
+const getPaths = (items: readonly SearchResult[]): readonly string[] => {
+  const paths: string[] = []
+  for (const item of items) {
+    if (item.type === TextSearchResultType.File) {
+      paths.push(item.text)
+    }
+  }
+  return paths
+}
 
 export const collapseAll = async (state: SearchState): Promise<SearchState> => {
-  return state
+  const { items } = state
+  if (items.length === 0) {
+    return state
+  }
+  const paths = getPaths(items)
+  return {
+    ...state,
+    collapsedPaths: paths,
+  }
 }
