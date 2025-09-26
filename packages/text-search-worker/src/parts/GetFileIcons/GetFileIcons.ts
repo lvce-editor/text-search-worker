cbb53eb555e3a087783eac7434a73d7552172e7f
Le Vivilet <levivilet@pm.me>
2025-09-26 13:17:05 +0200

    fix: wrong file icons

--- packages/text-search-worker/src/parts/GetFileIcons/GetFileIcons.ts
+++ packages/text-search-worker/src/parts/GetFileIcons/GetFileIcons.ts
@@ -1,17 +1,29 @@
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
+import { IconRequest } from '../IconRequest/IconRequest.ts'
+import { DirentType } from '@lvce-editor/constants'
 
-const getFileIcon = async (result: SearchResult): Promise<string> => {
-  if (result.type === TextSearchResultType.File) {
-    return RendererWorker.getFileIcon({ name: result.text })
+const getFileName = (text: string): string => {
+  if (text.startsWith('./')) {
+    return text.slice(2)
   }
-  return ''
+  return text
 }
 
 export const getFileIcons = async (matches: readonly SearchResult[]): Promise<readonly string[]> => {
+  const requests: IconRequest[] = []
+  for (const item of matches) {
+    if (item.type === TextSearchResultType.File) {
+      const fileName = getFileName(item.text)
+      requests.push({
+        type: DirentType.File,
+        name: fileName,
+        path: `/${fileName}`,
+      })
+    }
+  }
+  const icons = await RendererWorker.getIcons(requests)
   // TODO cache file icons
-  const promises = matches.map(getFileIcon)
-  const icons = await Promise.all(promises)
   return icons
 }
