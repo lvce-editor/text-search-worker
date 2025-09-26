0065e1f7250e9fbf55e3f2f3c4459d69255c8a15
Le Vivilet <levivilet@pm.me>
2025-09-26 16:36:56 +0200

    lint

--- packages/e2e/src/search.copy.ts
+++ packages/e2e/src/search.copy.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.copy'
 
-export const test: Test = async ({ Command, ClipBoard, Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+export const test: Test = async ({ Command, ClipBoard, Search, FileSystem, Workspace, SideBar }) => {
   // arrange
   await ClipBoard.enableMemoryClipBoard()
   const tmpDir = await FileSystem.getTmpDir()
