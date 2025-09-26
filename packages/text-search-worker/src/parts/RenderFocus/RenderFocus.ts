51ab7be64d7e190460aa4300fb0956aa1f12d513
Le Vivilet <levivilet@pm.me>
2025-09-26 23:12:28 +0200

    feature: use viewletCommand

--- packages/text-search-worker/src/parts/RenderFocus/RenderFocus.ts
+++ packages/text-search-worker/src/parts/RenderFocus/RenderFocus.ts
@@ -1,7 +1,8 @@
+import { ViewletCommand } from '@lvce-editor/constants'
 import type { SearchState } from '../SearchState/SearchState.ts'
 import * as GetFocusSelector from '../GetFocusSelector/GetFocusSelector.ts'
 
 export const renderFocus = (oldState: SearchState, newState: SearchState): readonly any[] => {
   const selector = GetFocusSelector.getFocusSelector(newState.focus)
-  return ['Viewlet.focusElementByName', newState.uid, selector]
+  return [ViewletCommand.FocusElementByName, newState.uid, selector]
 }
