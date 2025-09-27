0ba28d4974f06e4d690afc35d121a1010bb3071f
Le Vivilet <levivilet@pm.me>
2025-09-27 10:26:26 +0200

    fix: wrong handleFocus (#1092)

--- packages/text-search-worker/src/parts/FocusNextInput/FocusNextInput.ts
+++ packages/text-search-worker/src/parts/FocusNextInput/FocusNextInput.ts
@@ -4,7 +4,8 @@ import * as GetNextFocus from '../GetNextFocus/GetNextFocus.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
 export const focusNextInput = (state: SearchState): SearchState => {
-  const nextFocus = GetNextFocus.getNextFocus(state.focus, state.flags)
+  const { focus, flags } = state
+  const nextFocus = GetNextFocus.getNextFocus(focus, flags)
   const isListFocus = nextFocus === WhenExpression.FocusSearchResults
   return {
     ...state,
