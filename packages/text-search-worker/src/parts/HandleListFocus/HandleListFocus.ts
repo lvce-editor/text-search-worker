2b81f49492bd93b1597358cc8aff5c78d0b1de1b
Le Vivilet <levivilet@pm.me>
2025-09-27 09:43:15 +0200

    log

--- packages/text-search-worker/src/parts/HandleListFocus/HandleListFocus.ts
+++ packages/text-search-worker/src/parts/HandleListFocus/HandleListFocus.ts
@@ -2,8 +2,6 @@ import type { SearchState } from '../SearchState/SearchState.ts'
 
 export const handleListFocus = async (state: SearchState): Promise<SearchState> => {
   const { listFocused } = state
-
-  console.log({ listFocused })
   if (listFocused) {
     return state
   }
