fb18b6bf088058aa36ece895d1c247aee210e0d4
Le Vivilet <levivilet@pm.me>
2025-11-04 13:41:16 +0100

    feature: add context menu e2e test (#1122)

---
+++ packages/e2e/src/search.result-context-menu.ts
@@ -0,0 +1,36 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'search.result-context-menu'
+
+export const skip = 1
+
+export const test: Test = async ({ Command, Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+  await Search.setValue('ab')
+  await Search.setReplaceValue('')
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('1 result in 1 file')
+  const result = Locator('.TreeItem[aria-label="/test.css"]')
+  await expect(result).toHaveAttribute('aria-expanded', 'true')
+  const match = Locator('.TreeItem[aria-label="abc"]')
+  await expect(match).toBeVisible()
+  await Search.selectIndex(0)
+
+  // act
+  Command.execute('Search.handleContextMenu', 0, 0, 0)
+
+  // assert
+  const menu = Locator('.Menu')
+  await expect(menu).toBeVisible()
+  const menuItems = menu.locator('.MenuItem')
+  await expect(menuItems).toHaveCount(2)
+  const first = menuItems.nth(0)
+  await expect(first).toHaveText('Dismiss')
+  const second = menuItems.nth(1)
+  await expect(second).toHaveText('Copy Path')
+}
