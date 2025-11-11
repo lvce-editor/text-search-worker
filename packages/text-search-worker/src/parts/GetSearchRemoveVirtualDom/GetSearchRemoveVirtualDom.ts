c14240f59a881e16dd21f8a1215b04cf194d2715
Le Vivilet <levivilet@pm.me>
2025-11-11 21:20:31 +0100

    feature: use button for remove

--- packages/text-search-worker/src/parts/GetSearchRemoveVirtualDom/GetSearchRemoveVirtualDom.ts
+++ packages/text-search-worker/src/parts/GetSearchRemoveVirtualDom/GetSearchRemoveVirtualDom.ts
@@ -1,19 +1,23 @@
 import { ClassNames, VirtualDomElements } from '@lvce-editor/virtual-dom-worker'
 import type { VirtualDomNode } from '../VirtualDomNode/VirtualDomNode.ts'
+import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
+
+const closeIcon: VirtualDomNode = {
+  type: VirtualDomElements.Div,
+  className: ClassNames.CloseMaskIcon,
+  childCount: 0,
+}
 
 export const getSearchRemoveVirtualDom = (): readonly VirtualDomNode[] => {
-  // TODO add title/aria label
+  const title = SearchStrings.remove()
   const dom: VirtualDomNode[] = [
     {
-      type: VirtualDomElements.Div,
+      type: VirtualDomElements.Button,
       className: ClassNames.SearchRemove,
       childCount: 1,
+      title,
     },
-    {
-      type: VirtualDomElements.Div,
-      className: ClassNames.CloseMaskIcon,
-      childCount: 0,
-    },
+    closeIcon,
   ]
   return dom
 }
