cbf68e549a5ea0387b2bf9ac43bd6f8a57cd59d1
Le Vivilet <levivilet@pm.me>
2025-11-11 21:37:51 +0100

    fix

--- packages/text-search-worker/src/parts/GetFileName/GetFileName.ts
+++ packages/text-search-worker/src/parts/GetFileName/GetFileName.ts
@@ -1,7 +1,7 @@
 export const getFileName = (path: string): string => {
   const slashIndex = path.lastIndexOf('/')
   if (slashIndex === -1) {
-    return path
+    return ''
   }
   return path.slice(slashIndex + 1)
 }
