25ae1b85b8eee58836b4220f26d018fe8f69e4b4
Le Vivilet <levivilet@pm.me>
2025-11-13 23:58:41 +0100

    lint

--- packages/e2e/src/search.toggle-details.ts
+++ packages/e2e/src/search.toggle-details.ts
@@ -2,7 +2,7 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.toggle-details'
 
-export const skip=1
+export const skip = 1
 
 export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
