814dbbc05dfef7aee13472e6cc33a74e21e4d28b
Le Vivilet <levivilet@pm.me>
2025-11-13 23:33:06 +0100

    feature: add showContextMenu2

--- packages/text-search-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/text-search-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -2,10 +2,13 @@ import * as Assert from '../Assert/Assert.ts'
 import * as RendererWorker from '../RendererWorker/RendererWorker.ts'
 
 export const show = async (x: number, y: number, id: number, ...args: readonly any[]): Promise<void> => {
-  // TODO ask renderer worker to show context menu
   Assert.number(x)
   Assert.number(y)
   Assert.number(id)
   // TODO
   await RendererWorker.showContextMenu(x, y, id, ...args)
+}
+
+export const show2 = async (uid: number, menuId: any, x: number, y: number, args: any): Promise<void> => {
+  await RendererWorker.showContextMenu2(uid, menuId, x, y, args)
 }
