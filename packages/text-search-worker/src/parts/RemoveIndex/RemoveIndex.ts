6b08ed768883054b1ea264e92109189bb9a3538b
Le Vivilet <levivilet@pm.me>
2025-11-14 11:22:56 +0100

    update scrollbar also

--- packages/text-search-worker/src/parts/RemoveIndex/RemoveIndex.ts
+++ packages/text-search-worker/src/parts/RemoveIndex/RemoveIndex.ts
@@ -1,16 +1,26 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
 import { getNewMinMax } from '../GetNewMinMax/GetNewMinMax.ts'
 import { removeItemFromItems } from '../RemoveItemFromItems/RemoveItemFromItems.ts'
+import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
 import * as ViewletSearchStatusMessage from '../SearchStatusMessage/SearchStatusMessage.ts'
 
-export const removeIndex = (state: SearchState, index: number): SearchState => {
-  const { items, fileCount, matchCount, minLineY, maxLineY, deltaY, itemHeight } = state
+export const removeIndex = async (state: SearchState, index: number): Promise<SearchState> => {
+  const { fileIconCache, minimumSliderSize, height, headerHeight, items, fileCount, matchCount, minLineY, maxLineY, deltaY, itemHeight } = state
   if (index === -1) {
     return state
   }
   const { newItems, newFocusedIndex, newMatchCount, newFileCount } = removeItemFromItems(items, index, matchCount, fileCount)
   const message = ViewletSearchStatusMessage.getStatusMessage(newMatchCount, newFileCount)
   const { newMinLineY, newMaxLineY, newDeltaY } = getNewMinMax(newItems.length, minLineY, maxLineY, deltaY, itemHeight)
+  const total = newItems.length
+  const contentHeight = total * itemHeight
+  const listHeight = height - headerHeight
+  const scrollBarHeight = ScrollBarFunctions.getScrollBarSize(height, contentHeight, minimumSliderSize)
+  const finalDeltaY = Math.max(contentHeight - listHeight, 0)
+  const visible = newItems.slice(0, maxLineY)
+  const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, fileIconCache)
+
   return {
     ...state,
     deltaY: newDeltaY,
@@ -22,5 +32,9 @@ export const removeIndex = (state: SearchState, index: number): SearchState => {
     maxLineY: newMaxLineY,
     message,
     minLineY: newMinLineY,
+    icons,
+    fileIconCache: newFileIconCache,
+    finalDeltaY,
+    scrollBarHeight,
   }
 }
