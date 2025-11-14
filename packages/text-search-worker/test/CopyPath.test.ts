71c8456533872eed9ccd89b858566d4ab541a8a3
Le Vivilet <levivilet@pm.me>
2025-11-14 10:49:36 +0100

    feature: add copyPath test (#1183)

---
+++ packages/text-search-worker/test/CopyPath.test.ts
@@ -0,0 +1,35 @@
+import { expect, test } from '@jest/globals'
+import { RendererWorker } from '@lvce-editor/rpc-registry'
+import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
+import * as CopyPath from '../src/parts/CopyPath/CopyPath.ts'
+import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
+
+test('copyPath - no focused item returns same state', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({})
+
+  const state: SearchState = {
+    ...CreateDefaultState.createDefaultState(),
+    focusedIndex: -1,
+    items: [],
+  }
+
+  const result = await CopyPath.copyPath(state)
+  expect(result).toBe(state)
+  expect(mockRpc.invocations).toEqual([])
+})
+
+test('copyPath - copies text from focused item', async () => {
+  const mockRpc = RendererWorker.registerMockRpc({
+    'ClipBoard.writeText': () => undefined,
+  })
+
+  const state: SearchState = {
+    ...CreateDefaultState.createDefaultState(),
+    focusedIndex: 0,
+    items: [{ text: 'test text', end: 0, lineNumber: 0, start: 0, type: 0 }],
+  }
+
+  const result = await CopyPath.copyPath(state)
+  expect(result).toBe(state)
+  expect(mockRpc.invocations).toEqual([['ClipBoard.writeText', 'test text']])
+})
