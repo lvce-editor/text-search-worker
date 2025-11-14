ebf7405838d51871e6479a690fd0e605a61572b1
Le Vivilet <levivilet@pm.me>
2025-11-14 11:16:27 +0100

    feature: call removeIndex from dismiss

--- packages/text-search-worker/src/parts/Dismiss/Dismiss.ts
+++ packages/text-search-worker/src/parts/Dismiss/Dismiss.ts
@@ -1,25 +1,7 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
-import { getNewMinMax } from '../GetNewMinMax/GetNewMinMax.ts'
-import { removeItemFromItems } from '../RemoveItemFromItems/RemoveItemFromItems.ts'
-import * as ViewletSearchStatusMessage from '../SearchStatusMessage/SearchStatusMessage.ts'
+import { removeIndex } from '../RemoveIndex/RemoveIndex.ts'
 
 export const dismissItem = (state: SearchState): SearchState => {
-  const { items, listFocusedIndex, fileCount, matchCount, minLineY, maxLineY, deltaY, itemHeight } = state
-  if (listFocusedIndex === -1) {
-    return state
-  }
-  const { newItems, newFocusedIndex, newMatchCount, newFileCount } = removeItemFromItems(items, listFocusedIndex, matchCount, fileCount)
-  const message = ViewletSearchStatusMessage.getStatusMessage(newMatchCount, newFileCount)
-  const { newMinLineY, newMaxLineY, newDeltaY } = getNewMinMax(newItems.length, minLineY, maxLineY, deltaY, itemHeight)
-  return {
-    ...state,
-    deltaY: newDeltaY,
-    fileCount: newFileCount,
-    items: newItems,
-    listFocusedIndex: newFocusedIndex,
-    matchCount: newMatchCount,
-    maxLineY: newMaxLineY,
-    message,
-    minLineY: newMinLineY,
-  }
+  const { listFocusedIndex } = state
+  return removeIndex(state, listFocusedIndex)
 }
