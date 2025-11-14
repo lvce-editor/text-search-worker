f0d93fdb8ec3f5e8563d2598a0bd64212b89dabe
Le Vivilet <levivilet@pm.me>
2025-11-14 11:04:20 +0100

    feature: move minmax into separate file (#1205)

--- packages/text-search-worker/src/parts/Dismiss/Dismiss.ts
+++ packages/text-search-worker/src/parts/Dismiss/Dismiss.ts
@@ -1,30 +1,8 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import { getNewMinMax } from '../GetNewMinMax/GetNewMinMax.ts'
 import { removeItemFromItems } from '../RemoveItemFromItems/RemoveItemFromItems.ts'
 import * as ViewletSearchStatusMessage from '../SearchStatusMessage/SearchStatusMessage.ts'
 
-interface MinMaxResult {
-  newDeltaY: number
-  newMinLineY: number
-  newMaxLineY: number
-}
-
-const getNewMinMax = (newItemsLength: number, minLineY: number, maxLineY: number, deltaY: number, itemHeight: number): MinMaxResult => {
-  if (maxLineY > newItemsLength) {
-    const diff = maxLineY - minLineY
-    const newMinLineY = Math.max(newItemsLength - diff, 0)
-    const newDeltaY = newMinLineY * itemHeight
-    return {
-      newDeltaY,
-      newMinLineY,
-      newMaxLineY: newItemsLength,
-    }
-  }
-  return {
-    newDeltaY: deltaY,
-    newMinLineY: minLineY,
-    newMaxLineY: maxLineY,
-  }
-}
 export const dismissItem = (state: SearchState): SearchState => {
   const { items, listFocusedIndex, fileCount, matchCount, minLineY, maxLineY, deltaY, itemHeight } = state
   if (listFocusedIndex === -1) {
