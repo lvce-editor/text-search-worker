d18e5be68b7bb7b2fc83c26fbab3b5c2449eac16
Le Vivilet <levivilet@pm.me>
2025-11-14 11:48:14 +0100

    feature: dismiss item at focused index (#1216)

--- packages/text-search-worker/src/parts/Dismiss/Dismiss.ts
+++ packages/text-search-worker/src/parts/Dismiss/Dismiss.ts
@@ -2,6 +2,7 @@ import type { SearchState } from '../SearchState/SearchState.ts'
 import { removeIndex } from '../RemoveIndex/RemoveIndex.ts'
 
 export const dismissItem = (state: SearchState): Promise<SearchState> => {
-  const { listFocusedIndex } = state
-  return removeIndex(state, listFocusedIndex)
+  const { listFocusedIndex, focusedIndex } = state
+  const actualIndex = listFocusedIndex === -1 ? focusedIndex : listFocusedIndex
+  return removeIndex(state, actualIndex)
 }
