a23daf27e393c7a3cb94ab44608842ab184ce7e1
Le Vivilet <levivilet@pm.me>
2025-11-15 19:51:09 +0100

    feature: add input paste e2e test (#1253)
    * feature: add input paste e2e test
    
    * tsc

---
+++ packages/e2e/src/search.input-paste.ts
@@ -0,0 +1,23 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'search.input-paste'
+
+export const skip = 1
+
+export const test: Test = async ({ ClipBoard, Command, Search, FileSystem, Workspace }) => {
+  // arrange
+  await ClipBoard.enableMemoryClipBoard()
+  await Command.execute('ClipBoard.writeText', 'ab')
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await Search.open()
+  await Search.setValue('')
+  await Search.setReplaceValue('')
+
+  // act
+  await Command.execute('Search.handleInputPaste', 'SearchValue')
+
+  // assert
+  // TODO verify that input has pasted text
+}
