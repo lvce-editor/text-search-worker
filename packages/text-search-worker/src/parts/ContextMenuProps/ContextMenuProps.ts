7c87ef7e4ace722c61d01280528856a29ff1632d
Le Vivilet <levivilet@pm.me>
2025-11-15 11:39:18 +0100

    feature: pass params to getmenuEntries (#1225)
    * feature: pass params to getmenuEntries
    
    * tsc

---
+++ packages/text-search-worker/src/parts/ContextMenuProps/ContextMenuProps.ts
@@ -0,0 +1,3 @@
+export interface ContextMenuProps {
+  readonly menuId: number
+}
--- packages/text-search-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
+++ packages/text-search-worker/src/parts/GetMenuEntries/GetMenuEntries.ts
@@ -1,8 +1,10 @@
 import { MenuItemFlags } from '@lvce-editor/constants'
+import type { ContextMenuProps } from '../ContextMenuProps/ContextMenuProps.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
+import type { SearchState } from '../SearchState/SearchState.ts'
 import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
 
-export const getMenuEntries = (): readonly MenuEntry[] => {
+export const getMenuEntries = (state: SearchState, props: ContextMenuProps): readonly MenuEntry[] => {
   return [
     {
       id: 'dismiss',
