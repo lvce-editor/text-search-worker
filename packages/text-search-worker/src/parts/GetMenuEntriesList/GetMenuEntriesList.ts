abe4452c0be3dfc7b1142355407bb92523b539c4
Le Vivilet <levivilet@pm.me>
2025-11-15 23:54:16 +0100

    feature: update list menu entries

--- packages/text-search-worker/src/parts/GetMenuEntriesList/GetMenuEntriesList.ts
+++ packages/text-search-worker/src/parts/GetMenuEntriesList/GetMenuEntriesList.ts
@@ -1,9 +1,16 @@
 import { MenuItemFlags } from '@lvce-editor/constants'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
+import type { SearchState } from '../SearchState/SearchState.ts'
 import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
 
-export const getMenuEntriesList = (): readonly MenuEntry[] => {
+export const getMenuEntriesList = (state: SearchState): readonly MenuEntry[] => {
   return [
+    {
+      id: 'replaceAll',
+      label: SearchStrings.replaceAll(),
+      flags: MenuItemFlags.None,
+      command: 'Search.replaceAll',
+    },
     {
       id: 'dismiss',
       label: SearchStrings.dismiss(),
@@ -11,10 +18,22 @@ export const getMenuEntriesList = (): readonly MenuEntry[] => {
       command: 'Search.removeCurrent',
     },
     {
+      id: 'copy',
+      label: SearchStrings.copy(),
+      flags: MenuItemFlags.None,
+      command: 'Search.copy',
+    },
+    {
       id: 'copyPath',
       label: SearchStrings.copyPath(),
       flags: MenuItemFlags.None,
       command: 'Search.copyPath',
+    },
+    {
+      id: 'copyAll',
+      label: SearchStrings.copyPath(),
+      flags: MenuItemFlags.None,
+      command: 'Search.copyAll',
     },
   ]
 }
