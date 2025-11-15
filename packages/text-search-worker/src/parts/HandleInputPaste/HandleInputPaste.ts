5948bf6ee9a4536f92a3b63e1277623bfc832e1a
Le Vivilet <levivilet@pm.me>
2025-11-15 19:51:37 +0100

    feature: get current value

--- packages/text-search-worker/src/parts/HandleInputPaste/HandleInputPaste.ts
+++ packages/text-search-worker/src/parts/HandleInputPaste/HandleInputPaste.ts
@@ -1,11 +1,26 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
 import type { SelectionState } from '../SelectionState/SelectionState.ts'
 import { getNewText } from '../GetNewText/GetNewText.ts'
+import * as InputName from '../InputName/InputName.ts'
 
 const isSelectionKey = (name: string, selections: SelectionState): name is keyof SelectionState => {
   return name in selections
 }
 
+const getCurrentValue = (state: SearchState, name: string): string => {
+  switch (name) {
+    case InputName.SearchValue:
+      return state.value
+    case InputName.FilesToExclude:
+      return state.excludeValue
+    case InputName.FilesToInclude:
+      return state.includeValue
+    case InputName.ReplaceValue:
+      return state.replacement
+    default:
+      return ''
+  }
+}
 export const handleInputPaste = async (state: SearchState, name: string): Promise<SearchState> => {
   const { selections } = state
   if (!isSelectionKey(name, selections)) {
@@ -13,7 +28,7 @@ export const handleInputPaste = async (state: SearchState, name: string): Promis
   }
   const selection = selections[name]
   const { start, end } = selection
-  const currentText = ''
+  const currentText = getCurrentValue(state, name)
   const insertedText = ''
   const newText = getNewText(currentText, start, end, insertedText)
   if (newText) {
