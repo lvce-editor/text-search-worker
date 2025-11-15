3ee051c5ff7e97e647ea2e208845336d21d1b6f3
Le Vivilet <levivilet@pm.me>
2025-11-15 20:16:51 +0100

    feature: improve input copy (#1266)

--- packages/text-search-worker/src/parts/HandleInputCopy/HandleInputCopy.ts
+++ packages/text-search-worker/src/parts/HandleInputCopy/HandleInputCopy.ts
@@ -1,6 +1,16 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import { writeText } from '../ClipBoard/ClipBoard.ts'
+import { getCurrentValue, isSelectionKey } from '../HandleInputPaste/HandleInputPaste.ts'
 
 export const handleInputCopy = async (state: SearchState, name: string): Promise<SearchState> => {
-  // TODO write selected text to clipboard
+  const { selections } = state
+  if (!isSelectionKey(name, selections)) {
+    return state
+  }
+  const selection = selections[name]
+  const { start, end } = selection
+  const currentText = getCurrentValue(state, name)
+  const toCopy = currentText.slice(start, end)
+  await writeText(toCopy)
   return state
 }
