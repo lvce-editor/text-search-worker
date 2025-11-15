7cdb115037b3bfc168d3dc9b079f3824b1587413
Le Vivilet <levivilet@pm.me>
2025-11-15 23:38:19 +0100

    update

--- packages/text-search-worker/src/parts/MenuEntrySeparator/MenuEntrySeparator.ts
+++ packages/text-search-worker/src/parts/MenuEntrySeparator/MenuEntrySeparator.ts
@@ -1,5 +1,5 @@
+import { MenuItemFlags } from '@lvce-editor/constants'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
-import * as MenuItemFlags from '../MenuItemFlags/MenuItemFlags.ts'
 
 export const menuEntrySeparator: MenuEntry = {
   id: 'separator',
