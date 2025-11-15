67f3b36217c758f66d6f7d47aa49c3c30ebd7021
Le Vivilet <levivilet@pm.me>
2025-11-16 00:18:15 +0100

    feature: implement removeCurrent (#1304)

--- packages/text-search-worker/src/parts/RemoveCurrent/RemoveCurrent.ts
+++ packages/text-search-worker/src/parts/RemoveCurrent/RemoveCurrent.ts
@@ -1,12 +1,11 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import { removeIndex } from '../RemoveIndex/RemoveIndex.ts'
 
 export const removeCurrent = async (state: SearchState): Promise<SearchState> => {
-  // TODO
-  // 1. get focused index
-  // 2. if it is -1, do nothing
-  // 3. get search result at focused index
-  // 4. call removeIndex with state, searchResult, index
-  return {
-    ...state,
+  const { focusedIndex, listFocusedIndex } = state
+  const actualIndex = focusedIndex === -1 ? listFocusedIndex : focusedIndex
+  if (actualIndex === -1) {
+    return state
   }
+  return removeIndex(state, actualIndex)
 }
