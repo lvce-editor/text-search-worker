dbc59df13dc4f9e60c33d54c248aaf45af499f48
Le Vivilet <levivilet@pm.me>
2025-11-15 11:39:59 +0100

    feature: implement input context menu (#1227)

--- packages/text-search-worker/src/parts/HandleInputContextMenu/HandleInputContextMenu.ts
+++ packages/text-search-worker/src/parts/HandleInputContextMenu/HandleInputContextMenu.ts
@@ -1,6 +1,11 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 
-export const handleInputContextMenu = async (state: SearchState): Promise<SearchState> => {
-  // TODO show menu with cut/copy/paste
+export const handleInputContextMenu = async (state: SearchState, button: number, x: number, y: number): Promise<SearchState> => {
+  const { uid } = state
+  const menuId = 91
+  await ContextMenu.show2(uid, menuId, x, y, {
+    menuId,
+  })
   return state
 }
