9d1844489104f7ab2e11c44bc9efe382a535d46b
Le Vivilet <levivilet@pm.me>
2025-11-15 20:17:59 +0100

    feature: enable copy e2e test

--- packages/e2e/src/search.input-copy.ts
+++ packages/e2e/src/search.input-copy.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.input-copy'
 
-export const skip = 1
-
 export const test: Test = async ({ ClipBoard, Command, Search, FileSystem, Workspace }) => {
   // arrange
   await ClipBoard.enableMemoryClipBoard()
@@ -13,9 +11,11 @@ export const test: Test = async ({ ClipBoard, Command, Search, FileSystem, Works
   await Search.open()
   await Search.setValue('ab')
   await Search.setReplaceValue('')
+  await Command.execute('Search.handleInputSelectionChange', 'SearchValue', 0, 2)
 
   // act
   await Command.execute('Search.handleInputCopy', 'SearchValue')
 
   // assert
+  await ClipBoard.shouldHaveText('ab')
 }
