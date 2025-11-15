e4a9308708b98684c364f7363f2e4d146a93f585
Le Vivilet <levivilet@pm.me>
2025-11-15 23:21:05 +0100

    feature: enable copyPath e2e test

--- packages/e2e/src/search.copy-path.ts
+++ packages/e2e/src/search.copy-path.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.copy-path'
 
-export const skip = 1
-
 export const test: Test = async ({ ClipBoard, Search, FileSystem, Workspace, SideBar }) => {
   // arrange
   await ClipBoard.enableMemoryClipBoard()
