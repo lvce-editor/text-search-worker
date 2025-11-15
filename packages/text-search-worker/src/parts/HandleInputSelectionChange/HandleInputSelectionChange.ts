f00714f7cf1f16c86abbebf548af8aea00efbd57
Le Vivilet <levivilet@pm.me>
2025-11-15 13:57:11 +0100

    feature: add selectionChange function

---
+++ packages/text-search-worker/src/parts/HandleInputSelectionChange/HandleInputSelectionChange.ts
@@ -0,0 +1,11 @@
+import type { SearchState } from '../SearchState/SearchState.ts'
+
+export const handleInputSelectionChange = async (
+  state: SearchState,
+  name: string,
+  selectionStart: number,
+  selectionEnd: number,
+): Promise<SearchState> => {
+  // TODO
+  return state
+}
