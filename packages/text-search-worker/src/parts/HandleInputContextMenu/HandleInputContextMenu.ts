c431cc315f8c326155186715572f7f531399e53e
Le Vivilet <levivilet@pm.me>
2025-11-15 11:50:41 +0100

    fix

--- packages/text-search-worker/src/parts/HandleInputContextMenu/HandleInputContextMenu.ts
+++ packages/text-search-worker/src/parts/HandleInputContextMenu/HandleInputContextMenu.ts
@@ -1,11 +1,11 @@
+import { MenuEntryId } from '@lvce-editor/constants'
 import type { SearchState } from '../SearchState/SearchState.ts'
 import * as ContextMenu from '../ContextMenu/ContextMenu.ts'
 
 export const handleInputContextMenu = async (state: SearchState, button: number, x: number, y: number): Promise<SearchState> => {
   const { uid } = state
-  const menuId = 91
-  await ContextMenu.show2(uid, menuId, x, y, {
-    menuId,
+  await ContextMenu.show2(uid, MenuEntryId.InputContextMenu, x, y, {
+    menuId: MenuEntryId.InputContextMenu,
   })
   return state
 }
