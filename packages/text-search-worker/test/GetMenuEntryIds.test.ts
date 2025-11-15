e23e60a6605602076d1dd1ac80bee3c6c96689a8
Le Vivilet <levivilet@pm.me>
2025-11-15 11:36:22 +0100

    tsc

--- packages/text-search-worker/test/GetMenuEntryIds.test.ts
+++ packages/text-search-worker/test/GetMenuEntryIds.test.ts
@@ -1,5 +1,4 @@
 import { expect, test } from '@jest/globals'
-import { MenuEntryId } from '@lvce-editor/constants'
 import { getMenuEntryIds } from '../src/parts/GetMenuEntryIds/GetMenuEntryIds.ts'
 
 test('getMenuEntryIds returns array with MenuEntryId.Search', () => {
