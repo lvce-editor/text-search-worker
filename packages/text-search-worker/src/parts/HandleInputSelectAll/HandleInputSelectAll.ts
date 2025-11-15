acbccddb08a3d433d1ae3dc0bd7dba73a4252ea0
Le Vivilet <levivilet@pm.me>
2025-11-16 00:20:59 +0100

    feature: add selectAll command

---
+++ packages/text-search-worker/src/parts/HandleInputSelectAll/HandleInputSelectAll.ts
@@ -0,0 +1,6 @@
+import type { SearchState } from '../SearchState/SearchState.ts'
+
+export const handleInputSelectAll = async (state: SearchState, name: string): Promise<SearchState> => {
+  // TODO
+  return state
+}
