32a6c335f0cac1d20b0c5a8da069c31558acaa60
Le Vivilet <levivilet@pm.me>
2025-11-15 20:00:01 +0100

    feature: add basic cut (#1262)
    * feature: add basic cut
    
    * feature: add basic cut

--- packages/text-search-worker/src/parts/HandleInputCut/HandleInputCut.ts
+++ packages/text-search-worker/src/parts/HandleInputCut/HandleInputCut.ts
@@ -1,6 +1,21 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import { getNewText } from '../GetNewText/GetNewText.ts'
+import { isSelectionKey, getCurrentValue } from '../HandleInputPaste/HandleInputPaste.ts'
 
 export const handleInputCut = async (state: SearchState, name: string): Promise<SearchState> => {
-  // TODO write selected text to clipboard and cut text
+  const { selections } = state
+  if (!isSelectionKey(name, selections)) {
+    return state
+  }
+  const selection = selections[name]
+  const { start, end } = selection
+  const currentText = getCurrentValue(state, name)
+  const insertedText = ''
+  // TODO get cut text
+  const newText = getNewText(currentText, start, end, insertedText)
+  if (newText) {
+    return state
+  }
+  // TODO cut text
   return state
 }
