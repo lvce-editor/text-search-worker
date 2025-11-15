705d65f3a10445d615e9bbed0d141087d0fd99a9
Le Vivilet <levivilet@pm.me>
2025-11-16 00:14:11 +0100

    fix: copy path menu entry (#1302)

--- packages/text-search-worker/src/parts/CopyPath/CopyPath.ts
+++ packages/text-search-worker/src/parts/CopyPath/CopyPath.ts
@@ -2,11 +2,12 @@ import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { SearchState } from '../SearchState/SearchState.ts'
 
 export const copyPath = async (state: SearchState): Promise<SearchState> => {
-  const { items, focusedIndex } = state
-  if (focusedIndex === -1) {
+  const { items, focusedIndex, listFocusedIndex } = state
+  const actualIndex = focusedIndex === -1 ? listFocusedIndex : focusedIndex
+  if (actualIndex === -1) {
     return state
   }
-  const item = items[focusedIndex]
+  const item = items[actualIndex]
   await RendererWorker.writeClipBoardText(item.text)
   return state
 }
