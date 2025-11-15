85c62adce46027c41942777934dda0a97a43240b
Le Vivilet <levivilet@pm.me>
2025-11-15 19:53:18 +0100

    feature: improve readText

--- packages/text-search-worker/src/parts/ClipBoard/ClipBoard.ts
+++ packages/text-search-worker/src/parts/ClipBoard/ClipBoard.ts
@@ -3,8 +3,7 @@ import { RendererWorker } from '@lvce-editor/rpc-registry'
 export const writeText = async (text: string): Promise<void> => {
   await RendererWorker.writeClipBoardText(text)
 }
-export const readText = async (text: string): Promise<string> => {
-  // TODO
-  // await RendererWorker.invoke(text)
-  return ''
+
+export const readText = async (): Promise<string> => {
+  return await RendererWorker.invoke('ClipBoard.readText')
 }
