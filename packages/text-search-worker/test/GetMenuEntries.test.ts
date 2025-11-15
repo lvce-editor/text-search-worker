5ce347f02eee7c3d819e3b9d60a85384693f5b1f
Le Vivilet <levivilet@pm.me>
2025-11-16 00:06:11 +0100

    test

--- packages/text-search-worker/test/GetMenuEntries.test.ts
+++ packages/text-search-worker/test/GetMenuEntries.test.ts
@@ -10,17 +10,35 @@ test('getMenuEntries returns menu entries with correct structure', () => {
     menuId: MenuEntryId.Search,
   }
   const entries = getMenuEntries(state, params)
-  expect(entries).toHaveLength(2)
+  expect(entries).toHaveLength(5)
   expect(entries[0]).toEqual({
+    id: 'replaceAll',
+    label: SearchStrings.replaceAll(),
+    flags: MenuItemFlags.None,
+    command: 'Search.replaceAll',
+  })
+  expect(entries[1]).toEqual({
     id: 'dismiss',
     label: SearchStrings.dismiss(),
     flags: MenuItemFlags.None,
     command: 'Search.removeCurrent',
   })
-  expect(entries[1]).toEqual({
+  expect(entries[2]).toEqual({
+    id: 'copy',
+    label: SearchStrings.copy(),
+    flags: MenuItemFlags.None,
+    command: 'Search.copy',
+  })
+  expect(entries[3]).toEqual({
     id: 'copyPath',
     label: SearchStrings.copyPath(),
     flags: MenuItemFlags.None,
     command: 'Search.copyPath',
+  })
+  expect(entries[4]).toEqual({
+    id: 'copyAll',
+    label: SearchStrings.copyPath(),
+    flags: MenuItemFlags.None,
+    command: 'Search.copyAll',
   })
 })
