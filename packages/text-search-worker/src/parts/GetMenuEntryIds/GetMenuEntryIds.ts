d16c60249e320a324ee89ebe899649a5eb39a48c
Le Vivilet <levivilet@pm.me>
2025-11-15 11:33:44 +0100

    feature: update menu ids

--- packages/text-search-worker/src/parts/GetMenuEntryIds/GetMenuEntryIds.ts
+++ packages/text-search-worker/src/parts/GetMenuEntryIds/GetMenuEntryIds.ts
@@ -1,5 +1,7 @@
 import { MenuEntryId } from '@lvce-editor/constants'
 
+export const InputContextMenu = 91
+
 export const getMenuEntryIds = (): readonly number[] => {
-  return [MenuEntryId.Search]
+  return [MenuEntryId.Search, InputContextMenu]
 }
