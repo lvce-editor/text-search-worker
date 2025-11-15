e55b77cbd20f260a7a1568daa500f8c2b877c024
Le Vivilet <levivilet@pm.me>
2025-11-15 14:27:51 +0100

    feature: add renderselections (#1243)

---
+++ packages/text-search-worker/src/parts/RenderSelections/RenderSelections.ts
@@ -0,0 +1,8 @@
+import type { SearchState } from '../SearchState/SearchState.ts'
+
+export const renderSelections = (oldState: SearchState, newState: SearchState): readonly any[] => {
+  // const { selections, uid } = newState
+
+  // TODO
+  return []
+}
