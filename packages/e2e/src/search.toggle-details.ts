caa37464e1f69fc0193338527a7fc06406d71599
Le Vivilet <levivilet@pm.me>
2025-11-15 23:43:19 +0100

    feature: enable detail e2e test

--- packages/e2e/src/search.toggle-details.ts
+++ packages/e2e/src/search.toggle-details.ts
@@ -2,8 +2,6 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.toggle-details'
 
-export const skip = 1
-
 export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
