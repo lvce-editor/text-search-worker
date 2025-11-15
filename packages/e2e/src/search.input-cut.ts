b28b5ed9768759f15b27125ceee07a0e55f5cc94
Le Vivilet <levivilet@pm.me>
2025-11-15 19:48:50 +0100

    feature: add cut e2e test

---
+++ packages/e2e/src/search.input-cut.ts
@@ -0,0 +1,21 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'search.input-cut'
+
+export const skip = 1
+
+export const test: Test = async ({ ClipBoard, Command, Search, FileSystem, Workspace }) => {
+  // arrange
+  await ClipBoard.enableMemoryClipBoard()
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc`)
+  await Workspace.setPath(tmpDir)
+  await Search.open()
+  await Search.setValue('ab')
+  await Search.setReplaceValue('')
+
+  // act
+  await Command.execute('Search.handleInputCut', 'SearchValue')
+
+  // assert
+}
