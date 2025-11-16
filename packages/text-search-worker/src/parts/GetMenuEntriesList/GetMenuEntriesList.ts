5d0e13d327fc47fa158cec6cdd0e82f073d7de12
Le Vivilet <levivilet@pm.me>
2025-11-16 18:29:43 +0100

    feature: use context menu items for match or file depending on type (#1310)

--- packages/text-search-worker/src/parts/GetMenuEntriesList/GetMenuEntriesList.ts
+++ packages/text-search-worker/src/parts/GetMenuEntriesList/GetMenuEntriesList.ts
@@ -1,40 +1,22 @@
-import { MenuItemFlags } from '@lvce-editor/constants'
+import { TextSearchResultType } from '@lvce-editor/constants'
 import type { ContextMenuPropsList } from '../ContextMenuProps/ContextMenuProps.ts'
 import type { MenuEntry } from '../MenuEntry/MenuEntry.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
-import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
+import { getMenuEntriesFile } from '../GetMenuEntriesFile/GetMenuEntriesFile.ts'
+import { getMenuEntriesMatch } from '../GetMenuEntriesMatch/GetMenuEntriesMatch.ts'
 
 export const getMenuEntriesList = (state: SearchState, props: ContextMenuPropsList): readonly MenuEntry[] => {
-  return [
-    {
-      id: 'replaceAll',
-      label: SearchStrings.replaceAll(),
-      flags: MenuItemFlags.None,
-      command: 'Search.replaceAll',
-    },
-    {
-      id: 'dismiss',
-      label: SearchStrings.dismiss(),
-      flags: MenuItemFlags.None,
-      command: 'Search.removeCurrent',
-    },
-    {
-      id: 'copy',
-      label: SearchStrings.copy(),
-      flags: MenuItemFlags.None,
-      command: 'Search.copy',
-    },
-    {
-      id: 'copyPath',
-      label: SearchStrings.copyPath(),
-      flags: MenuItemFlags.None,
-      command: 'Search.copyPath',
-    },
-    {
-      id: 'copyAll',
-      label: SearchStrings.copyAll(),
-      flags: MenuItemFlags.None,
-      command: 'Search.copyAll',
-    },
-  ]
+  const { items } = state
+  const { index } = props
+  const item = items[index]
+  if (!item) {
+    return []
+  }
+  if (item.type === TextSearchResultType.Match) {
+    return getMenuEntriesMatch(state, props)
+  }
+  if (item.type === TextSearchResultType.File) {
+    return getMenuEntriesFile(state, props)
+  }
+  return []
 }
