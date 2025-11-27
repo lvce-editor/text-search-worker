151fa3c7b96073356f3e3d5b54503d2f77ff3b03
Le Vivilet <levivilet@pm.me>
2025-11-27 23:25:34 +0100

    feature: update search actions (#1321)

--- packages/text-search-worker/src/parts/GetSearchActions/GetSearchActions.ts
+++ packages/text-search-worker/src/parts/GetSearchActions/GetSearchActions.ts
@@ -1,5 +1,6 @@
 import type { Action } from '../Action/Action.ts'
 import * as ActionType from '../ActionType/ActionType.ts'
+import * as InputName from '../InputName/InputName.ts'
 import * as MaskIcon from '../MaskIcon/MaskIcon.ts'
 import * as SearchStrings from '../SearchStrings/SearchStrings.ts'
 
@@ -7,33 +8,38 @@ export const getActions = (): readonly Action[] => {
   return [
     {
       type: ActionType.Button,
-      id: SearchStrings.refresh(),
+      label: SearchStrings.refresh(),
       icon: MaskIcon.Refresh,
+      id: InputName.Refresh,
       command: 'refresh',
     },
     {
       type: ActionType.Button,
-      id: SearchStrings.clearSearchResults(),
+      label: SearchStrings.clearSearchResults(),
       icon: MaskIcon.ClearAll,
       command: 'clearSearchResults',
+      id: InputName.ClearAll,
     },
     {
       type: ActionType.Button,
-      id: SearchStrings.openNewSearchEditor(),
+      label: SearchStrings.openNewSearchEditor(),
       icon: MaskIcon.NewFile,
       command: '',
+      id: InputName.OpenSearchEditor,
     },
     {
       type: ActionType.Button,
-      id: SearchStrings.viewAsTree(),
+      label: SearchStrings.viewAsTree(),
       icon: MaskIcon.ListFlat,
       command: '',
+      id: InputName.ViewAsTree,
     },
     {
       type: ActionType.Button,
-      id: SearchStrings.collapseAll(),
+      label: SearchStrings.collapseAll(),
       icon: MaskIcon.CollapseAll,
       command: '',
+      id: InputName.CollapseAll,
     },
   ]
 }
