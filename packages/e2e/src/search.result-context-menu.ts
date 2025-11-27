58b9684fe3019bdba5ef3a44651e61042f0936cf
Le Vivilet <levivilet@pm.me>
2025-11-27 23:47:15 +0100

    skip

--- packages/e2e/src/search.result-context-menu.ts
+++ packages/e2e/src/search.result-context-menu.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.result-context-menu'
 
+export const skip = 1
+
 export const test: Test = async ({ Search, FileSystem, Workspace, SideBar, Locator, expect }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
