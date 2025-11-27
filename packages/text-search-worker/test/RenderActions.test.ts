7aaec3be202c8a2e5b41441a17e22f015b647408
Le Vivilet <levivilet@pm.me>
2025-11-27 23:29:19 +0100

    feature: use getter for renderActions (#1324)

--- packages/text-search-worker/test/RenderActions.test.ts
+++ packages/text-search-worker/test/RenderActions.test.ts
@@ -8,6 +8,7 @@ import * as SearchViewStates from '../src/parts/SearchViewStates/SearchViewState
 test.skip('renderActions - returns empty array when states are same', () => {
   const state = { ...CreateDefaultState.createDefaultState(), uid: 1 }
   SearchViewStates.set(1, state, state)
+  // @ts-ignore
   const result = RenderActions.renderActions(1)
   expect(result).toEqual([])
 })
@@ -27,6 +28,7 @@ test.skip('renderActions - returns virtual dom when states differ', () => {
     ],
   }
   SearchViewStates.set(1, oldState, newState)
+  // @ts-ignore
   const result = RenderActions.renderActions(1)
   expect(result).toHaveLength(1)
   expect(result[0]).toEqual({
