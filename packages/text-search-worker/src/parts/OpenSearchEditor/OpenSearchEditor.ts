705204220aa2e6759131c8f591ac9f8d1ce84d73
Le Vivilet <levivilet@pm.me>
2025-11-27 23:23:06 +0100

    feature: add openSearchEditor

---
+++ packages/text-search-worker/src/parts/OpenSearchEditor/OpenSearchEditor.ts
@@ -0,0 +1,8 @@
+import type { SearchState } from '../SearchState/SearchState.ts'
+
+export const openSearchEditor = async (state: SearchState): Promise<SearchState> => {
+  // TODO
+  return {
+    ...state,
+  }
+}
