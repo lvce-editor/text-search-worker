625aeee0428f29c69fed85a688941f1889116c80
Le Vivilet <levivilet@pm.me>
2025-12-03 12:30:56 +0100

    feature: reuse render function

--- packages/text-search-worker/src/parts/RenderIncremental/RenderIncremental.ts
+++ packages/text-search-worker/src/parts/RenderIncremental/RenderIncremental.ts
@@ -1,50 +1,12 @@
 import { diff } from '@lvce-editor/virtual-dom-worker'
-import type { SearchState } from '../SearchState/SearchState.ts'
-import * as GetSearchDisplayResults from '../GetSearchDisplayResults/GetSearchDisplayResults.ts'
-import * as GetSearchVirtualDom from '../GetSearchVirtualDom/GetSearchVirtualDom.ts'
-import * as ScrollBarFunctions from '../ScrollBarFunctions/ScrollBarFunctions.ts'
+import { renderItems } from '../RenderItems/RenderItems.ts'
 import * as SearchViewStates from '../SearchViewStates/SearchViewStates.ts'
-
-const getDom = (state: SearchState): any => {
-  const displayResults = GetSearchDisplayResults.getDisplayResults(
-    state.listItems,
-    state.itemHeight,
-    state.fileCount,
-    state.value,
-    state.minLineY,
-    state.maxLineY,
-    state.replacement,
-    state.icons,
-    state.listFocusedIndex,
-    state.collapsedPaths,
-    state.items,
-  )
-  const focusOutline = state.listFocused && state.listFocusedIndex === -1
-  const scrollBarValue = (state.deltaY / state.finalDeltaY) * 100
-  const scrollBarY = ScrollBarFunctions.getScrollBarY(state.deltaY, state.finalDeltaY, state.height - state.headerHeight, state.scrollBarHeight)
-  const dom = GetSearchVirtualDom.getSearchVirtualDom(
-    displayResults,
-    state.flags,
-    state.message,
-    focusOutline,
-    state.searchInputErrorMessage,
-    state.scrollBarHeight,
-    scrollBarY,
-    scrollBarValue,
-    state.deltaY,
-    state.itemHeight,
-    state.matchCount,
-    state.limitHitWarning,
-    state.focus,
-  )
-  return dom
-}
 
 // TODO cache rendered dom so that it can be used for dom diffing
 export const renderIncremental = (uid: number): readonly any[] => {
   const { oldState, newState } = SearchViewStates.get(uid)
-  const oldDom = getDom(oldState)
-  const newDom = getDom(newState)
+  const oldDom = renderItems(oldState, oldState)
+  const newDom = renderItems(newState, newState)
   const patches = diff(oldDom, newDom)
   return patches
 }
