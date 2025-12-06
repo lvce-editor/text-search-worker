7f5d2641056803c9b61c6814e92918c0ceee61c0
Le Vivilet <levivilet@pm.me>
2025-12-06 17:21:35 +0100

    feature: update icons when collapsing all

--- packages/text-search-worker/src/parts/CollapseAll/CollapseAll.ts
+++ packages/text-search-worker/src/parts/CollapseAll/CollapseAll.ts
@@ -1,5 +1,7 @@
 import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
+import * as GetFileIcons from '../GetFileIcons/GetFileIcons.ts'
+import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
 import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
 
 const getPaths = (items: readonly SearchResult[]): readonly string[] => {
@@ -13,13 +15,21 @@ const getPaths = (items: readonly SearchResult[]): readonly string[] => {
 }
 
 export const collapseAll = async (state: SearchState): Promise<SearchState> => {
-  const { items } = state
+  const { fileIconCache, headerHeight, height, itemHeight, items } = state
   if (items.length === 0) {
     return state
   }
   const paths = getPaths(items)
+  const total = items.length
+  const listHeight = height - headerHeight
+  const numberOfVisible = GetNumberOfVisibleItems.getNumberOfVisibleItems(listHeight, itemHeight)
+  const maxLineY = Math.min(numberOfVisible, total)
+  const visible = items.slice(0, maxLineY)
+  const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(visible, fileIconCache)
   return {
     ...state,
     collapsedPaths: paths,
+    fileIconCache: newFileIconCache,
+    icons,
   }
 }
