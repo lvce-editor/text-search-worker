d51342004a546d5cfbbaa2ec93e43a13cb707e96
Le Vivilet <levivilet@pm.me>
2026-02-03 10:48:43 +0100

    fix

--- packages/text-search-worker/src/parts/ContextMenu/ContextMenu.ts
+++ packages/text-search-worker/src/parts/ContextMenu/ContextMenu.ts
@@ -1,14 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 import type { ContextMenuProps } from '../ContextMenuProps/ContextMenuProps.ts'
-import * as Assert from '../Assert/Assert.ts'
-
-export const show = async (x: number, y: number, id: number, ...args: readonly any[]): Promise<void> => {
-  Assert.number(x)
-  Assert.number(y)
-  Assert.number(id)
-  // TODO
-  await RendererWorker.showContextMenu(x, y, id, ...args)
-}
 
 export const show2 = async <T extends ContextMenuProps>(
   uid: number,
