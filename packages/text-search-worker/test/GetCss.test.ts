94ed38dff755b666dc5f6b5f13eb9171cb71fb30
Le Vivilet <levivilet@pm.me>
2026-02-18 15:53:17 +0100

    fix

--- packages/text-search-worker/test/GetCss.test.ts
+++ packages/text-search-worker/test/GetCss.test.ts
@@ -3,35 +3,26 @@ import { getCss } from '../src/parts/GetCss/GetCss.ts'
 
 test('getCss - returns base css variables when there are no indents', () => {
   const result = getCss(24, [], [], 90, 12, 0)
-  expect(result).toBe(`.Search {
-  --ScrollBarHeight: 90px;
-  --ScrollBarTop: 12px;
-  --TreeItemsTop: 24px;
-}
-   
-.TreeItemsTop-0 {
-  top: 0px;
-}`)
+  expect(result).toContain('.Search {')
+  expect(result).toContain('--ScrollBarHeight: 90px;')
+  expect(result).toContain('--ScrollBarTop: 12px;')
+  expect(result).toContain('--TreeItemsTop: 24px;')
+  expect(result).toContain('.TreeItemsTop-0 {')
+  expect(result).toContain('top: 0px;')
 })
 
 test('getCss - returns css variables and indent rules', () => {
   const result = getCss(0, [16, 28], [12], 44, 6, -3)
-  expect(result).toBe(`.Search {
-  --ScrollBarHeight: 44px;
-  --ScrollBarTop: 6px;
-  --TreeItemsTop: 0px;
-}
-   
-.Indent-16 {
-  padding-left: 16px;
-}
-.Indent-28 {
-  padding-left: 28px;
-}
-.IndentRight-12 {
-  padding-right: 12px;
-}
-.TreeItemsTop--3 {
-  top: -3px;
-}`)
+  expect(result).toContain('.Search {')
+  expect(result).toContain('--ScrollBarHeight: 44px;')
+  expect(result).toContain('--ScrollBarTop: 6px;')
+  expect(result).toContain('--TreeItemsTop: 0px;')
+  expect(result).toContain('.Indent-16 {')
+  expect(result).toContain('padding-left: 16px;')
+  expect(result).toContain('.Indent-28 {')
+  expect(result).toContain('padding-left: 28px;')
+  expect(result).toContain('.IndentRight-12 {')
+  expect(result).toContain('padding-right: 12px;')
+  expect(result).toContain('.TreeItemsTop--3 {')
+  expect(result).toContain('top: -3px;')
 })
