a26f494bdb845d406c21030311cf13246151e3ee
Le Vivilet <levivilet@pm.me>
2026-02-18 11:57:18 +0100

    feat: refactor getCss function to support dynamic indentation rules (#1371)

--- packages/text-search-worker/src/parts/GetCss/GetCss.ts
+++ packages/text-search-worker/src/parts/GetCss/GetCss.ts
@@ -1,8 +1,19 @@
-export const getCss = (top: number): string => {
-  return `.Search {
+const getIndentRule = (indent: number): string => {
+  return `.Indent-${indent} {
+  padding-left: ${indent}px;
+}`
+}
+
+export const getCss = (top: number, uniqueIndents: readonly number[]): string => {
+  const rules = [
+    `.Search {
   --ScrollBarHeight: 20px;
   --ScrollBarTop: 20px;
   --TreeItemsTop: ${top}px;
 }
-  `
+  `,
+
+    ...uniqueIndents.map(getIndentRule),
+  ]
+  return rules.join('\n')
 }
