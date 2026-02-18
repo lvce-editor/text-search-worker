50d1ff15b1ffe14d2997cf28361b35ccb2e5d04f
Le Vivilet <levivilet@pm.me>
2026-02-18 17:27:37 +0100

    feat: add end-to-end test for search functionality with 20k results in a single file

---
+++ packages/e2e/src/search.many-results-20k-in-one-file.ts
@@ -0,0 +1,19 @@
+import type { Test } from '@lvce-editor/test-with-playwright'
+
+export const name = 'search.many-results-20k-in-one-file'
+
+export const test: Test = async ({ expect, FileSystem, Locator, Search, SideBar, Workspace }) => {
+  // arrange
+  const tmpDir = await FileSystem.getTmpDir()
+  await FileSystem.writeFile(`${tmpDir}/test.css`, `abc\n`.repeat(20_000))
+  await Workspace.setPath(tmpDir)
+  await SideBar.open('Search')
+
+  // act
+  await Search.setValue('ab')
+
+  // assert
+  const viewletSearch = Locator('.Search')
+  const message = viewletSearch.locator('[role="status"]')
+  await expect(message).toHaveText('20000 results in 1 file')
+}
