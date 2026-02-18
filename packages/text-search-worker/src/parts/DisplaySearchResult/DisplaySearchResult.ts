b5aad3a39c6b2be3327ce0bb44242752cc9263cc
Le Vivilet <levivilet@pm.me>
2026-02-18 12:00:01 +0100

    refactor: remove TODO comment regarding flags number in DisplaySearchResult interface

--- packages/text-search-worker/src/parts/DisplaySearchResult/DisplaySearchResult.ts
+++ packages/text-search-worker/src/parts/DisplaySearchResult/DisplaySearchResult.ts
@@ -2,14 +2,12 @@ export interface DisplaySearchResult {
   readonly badgeText: string
   readonly depth: number
   readonly expanded: number
-  // TODO maybe use a flags number instead of multiple booleans
   readonly focused: boolean
   readonly icon: string
   readonly matchLength: number
   readonly matchStart: number
   readonly posInSet: number
   readonly replacement: string
-
   readonly setSize: number
   readonly text: string
   readonly title: string
