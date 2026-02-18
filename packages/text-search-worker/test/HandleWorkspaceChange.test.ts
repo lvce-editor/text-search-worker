432c5d2b2839b8abf3c0bb805de3f207747cd260
Le Vivilet <levivilet@pm.me>
2026-02-18 17:33:33 +0100

    feat: update handleWorkspaceChange function and test to clear search, replace, and search results (#1385)

--- packages/text-search-worker/test/HandleWorkspaceChange.test.ts
+++ packages/text-search-worker/test/HandleWorkspaceChange.test.ts
@@ -2,10 +2,18 @@ import { expect, test } from '@jest/globals'
 import type { SearchState } from '../src/parts/SearchState/SearchState.ts'
 import * as CreateDefaultState from '../src/parts/CreateDefaultState/CreateDefaultState.ts'
 import * as HandleWorkspaceChange from '../src/parts/HandleWorkspaceChange/HandleWorkspaceChange.ts'
+import * as TextSearchResultType from '../src/parts/TextSearchResultType/TextSearchResultType.ts'
 
-test('handleWorkspaceChange - clears search and replace values', () => {
+test('handleWorkspaceChange - clears search, replace, and search results', () => {
   const state: SearchState = {
     ...CreateDefaultState.createDefaultState(),
+    deltaY: 10,
+    items: [{ end: 0, lineNumber: 0, start: 0, text: 'file1.txt', type: TextSearchResultType.File }],
+    listItems: [{ end: 0, lineNumber: 0, start: 0, text: 'file1.txt', type: TextSearchResultType.File }],
+    matchCount: 1,
+    maxLineY: 20,
+    message: '1 result in 1 file',
+    minLineY: 5,
     replacement: 'replace-me',
     value: 'search-me',
     workspacePath: 'test',
@@ -15,6 +23,13 @@ test('handleWorkspaceChange - clears search and replace values', () => {
 
   expect(result).toEqual({
     ...state,
+    deltaY: 0,
+    items: [],
+    listItems: [],
+    matchCount: 0,
+    maxLineY: 0,
+    message: '',
+    minLineY: 0,
     replacement: '',
     value: '',
   })
