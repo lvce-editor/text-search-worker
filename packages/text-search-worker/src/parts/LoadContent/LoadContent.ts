f32b5498fae46ffa77ef58575e59c7b4c8620712
Le Vivilet <levivilet@pm.me>
2026-02-18 17:43:10 +0100

    fix

--- packages/text-search-worker/src/parts/LoadContent/LoadContent.ts
+++ packages/text-search-worker/src/parts/LoadContent/LoadContent.ts
@@ -1,5 +1,7 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
 import * as GetUsePullBasedSearch from '../GetUsePullBasedSearch/GetUsePullBasedSearch.ts'
+import * as GetSearchWarningMessageHeight from '../GetSearchWarningMessageHeight/GetSearchWarningMessageHeight.ts'
+import * as GetTopHeight from '../GetTopHeight/GetTopHeight.ts'
 import * as ViewletSearchHandleUpdate from '../HandleUpdate/HandleUpdate.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 import * as RestoreState from '../RestoreState/RestoreState.ts'
@@ -7,12 +9,15 @@ import * as RestoreState from '../RestoreState/RestoreState.ts'
 export const loadContent = async (state: SearchState, savedState: unknown): Promise<SearchState> => {
   const { excludeValue, flags, includeValue, replacement, savedCollapsedPaths, savedValue, threads } = RestoreState.restoreState(savedState)
   const usePullBasedSearch = await GetUsePullBasedSearch.getUsePullBasedSearch()
+  const warningHeight = await GetSearchWarningMessageHeight.getSearchWarningMessageHeight(state.limitHitWarning, state.width)
+  const headerHeight = GetTopHeight.getTopHeight(flags) + warningHeight
 
   const update: Partial<SearchState> = {
     collapsedPaths: savedCollapsedPaths,
     excludeValue,
     flags,
     focus: 0, // TODO
+    headerHeight,
     includeValue,
     inputSource: InputSource.Script,
     replacement,
