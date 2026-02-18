008a2628739a767239f0f9180825cad6c18f94ab
Le Vivilet <levivilet@pm.me>
2026-02-18 14:57:52 +0100

    fix: update instantiations value from 5,000 to 6,000 for improved performance

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -3,7 +3,7 @@ import { root } from './root.ts'
 
 export const threshold = 550_000
 
-export const instantiations = 5_000
+export const instantiations = 6_000
 
 export const instantiationsPath = join(root, 'packages', 'text-search-worker')
 
