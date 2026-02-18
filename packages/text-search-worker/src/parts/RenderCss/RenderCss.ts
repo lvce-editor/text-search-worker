3a4410076d15975532d750fe2688f431aa44a946
Le Vivilet <levivilet@pm.me>
2026-02-18 15:06:59 +0100

    feat: refactor renderCss function to generate dynamic CSS based on unique indents

--- packages/text-search-worker/src/parts/RenderCss/RenderCss.ts
+++ packages/text-search-worker/src/parts/RenderCss/RenderCss.ts
@@ -1,6 +1,20 @@
 import { ViewletCommand } from '@lvce-editor/constants'
 import type { SearchState } from '../SearchState/SearchState.ts'
+import { createViewModel } from '../CreateViewModel/CreateViewModel.ts'
+import { getCss } from '../GetCss/GetCss.ts'
+import { getUniqueIndents } from '../GetUniqueIndents/GetUniqueIndents.ts'
 
 export const renderCss = (oldState: SearchState, newState: SearchState): readonly any[] => {
-  return [ViewletCommand.SetCss, newState.uid, ``]
+  const { focusedIndex, itemHeight, items, minLineY, scrollBarHeight, uid, width } = newState
+  const viewModel = createViewModel(newState)
+  const indents = viewModel.displayResults.map((item) => item.indent)
+  const uniqueIndents = getUniqueIndents(indents)
+  const indent = 8
+  const padding = 10
+  const fileIconWidth = 16
+  const defaultPaddingLeft = 0
+  const chevronSpace = 22
+
+  const css = getCss(0, uniqueIndents)
+  return [ViewletCommand.SetCss, uid, css]
 }
