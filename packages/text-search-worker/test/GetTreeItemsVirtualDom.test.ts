db8d12472c670208a813477d16e550adc700bc75
Le Vivilet <levivilet@pm.me>
2026-02-18 16:21:14 +0100

    fix

--- packages/text-search-worker/test/GetTreeItemsVirtualDom.test.ts
+++ packages/text-search-worker/test/GetTreeItemsVirtualDom.test.ts
@@ -2,7 +2,7 @@ import { expect, test } from '@jest/globals'
 import { getTreeItemsVirtualDom } from '../src/parts/GetTreeItemsVirtualDom/GetTreeItemsVirtualDom.ts'
 
 test('getTreeItemsVirtualDom - rounds subpixel tree items top classname value', () => {
-  const result = getTreeItemsVirtualDom([], 18.609375, 22)
+  const result = getTreeItemsVirtualDom([], 18.609_375, 22)
   expect(result[0]).toMatchObject({
     className: 'TreeItems TreeItemsTop--19',
   })
