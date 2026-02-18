a399144e77d9b5b12b5c2384faf8684ccee044a6
Le Vivilet <levivilet@pm.me>
2026-02-18 17:40:09 +0100

    fix

--- packages/text-search-worker/src/parts/MeasureTextBlockHeight/MeasureTextBlockHeight.ts
+++ packages/text-search-worker/src/parts/MeasureTextBlockHeight/MeasureTextBlockHeight.ts
@@ -1,5 +1,5 @@
 import { RendererWorker } from '@lvce-editor/rpc-registry'
 
 export const measureTextBlockHeight = (text: string, fontFamily: string, fontSize: number, lineHeight: number, width: number): Promise<number> => {
-  return RendererWorker.invoke('MeasureTextHeight.measureTextBlockHeight', text, fontSize, fontFamily, lineHeight, width)
+  return RendererWorker.invoke('MeasureTextBlockHeight.measureTextBlockHeight', text, fontSize, fontFamily, lineHeight, width)
 }
