0d5311d0f518b128d866e296d10a774d6c41998f
Le Vivilet <levivilet@pm.me>
2026-04-30 21:15:35 +0200

    refactor: change error type from 'any' to 'unknown' in handleUpdateError function

--- packages/text-search-worker/src/parts/HandleUpdateError/HandleUpdateError.ts
+++ packages/text-search-worker/src/parts/HandleUpdateError/HandleUpdateError.ts
@@ -1,6 +1,6 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
 
-export const handleUpdateError = async (state: SearchState, update: Partial<SearchState>, error: any): Promise<SearchState> => {
+export const handleUpdateError = async (state: SearchState, update: Partial<SearchState>, error: unknown): Promise<SearchState> => {
   const partialNewState = { ...state, ...update }
   return {
     ...partialNewState,
