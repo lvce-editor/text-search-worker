6564bda0c80188da0ddc6fdaea6d95713cda772a
Le Vivilet <levivilet@pm.me>
2026-05-05 21:45:11 +0000

    e2e

--- packages/e2e/src/search.replace-all-two-results.ts
+++ packages/e2e/src/search.replace-all-two-results.ts
@@ -2,6 +2,8 @@ import type { Test } from '@lvce-editor/test-with-playwright'
 
 export const name = 'search.replace-all-two-results'
 
+export const skip = 1
+
 export const test: Test = async ({ expect, FileSystem, Locator, Main, Search, SideBar, Workspace }) => {
   // arrange
   const tmpDir = await FileSystem.getTmpDir()
