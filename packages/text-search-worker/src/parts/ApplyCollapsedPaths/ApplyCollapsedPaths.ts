27a5801d4879e2637f6dd1453e7e7f22f4629579
Le Vivilet <levivilet@pm.me>
2026-05-05 21:46:55 +0000

    fix

--- packages/text-search-worker/src/parts/ApplyCollapsedPaths/ApplyCollapsedPaths.ts
+++ packages/text-search-worker/src/parts/ApplyCollapsedPaths/ApplyCollapsedPaths.ts
@@ -5,11 +5,7 @@ import * as GetFilteredResults from '../GetFilteredResults/GetFilteredResults.ts
 import * as GetNumberOfVisibleItems from '../GetNumberOfVisibleItems/GetNumberOfVisibleItems.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 
-export const applyCollapsedPaths = async (
-  state: SearchState,
-  collapsedPaths: readonly string[],
-  listFocusedIndex: number,
-): Promise<SearchState> => {
+export const applyCollapsedPaths = async (state: SearchState, collapsedPaths: readonly string[], listFocusedIndex: number): Promise<SearchState> => {
   const { fileIconCache, headerHeight, height, itemHeight, items } = state
   const filteredResults = GetFilteredResults.getFilteredResults(items, collapsedPaths)
   const { icons, newFileIconCache } = await GetFileIcons.getFileIcons(filteredResults, fileIconCache)
