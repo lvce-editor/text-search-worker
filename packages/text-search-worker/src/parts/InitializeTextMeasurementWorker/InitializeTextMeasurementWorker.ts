5fe54b5c49667036523988ba42ced7d159d53bda
Le Vivilet <levivilet@pm.me>
2026-05-28 01:01:11 +0200

    feature: add text measurement worker initialization to the startup process

---
+++ packages/text-search-worker/src/parts/InitializeTextMeasurementWorker/InitializeTextMeasurementWorker.ts
@@ -0,0 +1,14 @@
+import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
+import { RendererWorker, TextMeasurementWorker } from '@lvce-editor/rpc-registry'
+
+const send = async (port: MessagePort): Promise<void> => {
+  await RendererWorker.sendMessagePortToIconThemeWorker(port, 0)
+}
+
+export const initializeTextMeasurementWorker = async (): Promise<void> => {
+  const rpc = await LazyTransferMessagePortRpcParent.create({
+    commandMap: {},
+    send,
+  })
+  TextMeasurementWorker.set(rpc)
+}
