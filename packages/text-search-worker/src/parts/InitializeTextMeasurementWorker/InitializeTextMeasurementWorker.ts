0322eaa5ac91d62101c1fdc40ba53a58c4e88621
Le Vivilet <levivilet@pm.me>
2026-05-30 09:11:21 +0200

    fix

--- packages/text-search-worker/src/parts/InitializeTextMeasurementWorker/InitializeTextMeasurementWorker.ts
+++ packages/text-search-worker/src/parts/InitializeTextMeasurementWorker/InitializeTextMeasurementWorker.ts
@@ -2,7 +2,7 @@ import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { RendererWorker, TextMeasurementWorker } from '@lvce-editor/rpc-registry'
 
 const send = async (port: MessagePort): Promise<void> => {
-  await RendererWorker.sendMessagePortToTextMeasurementWorker(port, 0)
+  await RendererWorker.sendMessagePortToTextMeasurementWorker(port)
 }
 
 export const initializeTextMeasurementWorker = async (): Promise<void> => {
