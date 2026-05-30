babcd4dc792adf4d1c58cf61184609d1912b0c62
Le Vivilet <levivilet@pm.me>
2026-05-30 09:11:16 +0200

    rename

--- packages/text-search-worker/src/parts/InitializeTextMeasurementWorker/InitializeTextMeasurementWorker.ts
+++ packages/text-search-worker/src/parts/InitializeTextMeasurementWorker/InitializeTextMeasurementWorker.ts
@@ -2,7 +2,7 @@ import { LazyTransferMessagePortRpcParent } from '@lvce-editor/rpc'
 import { RendererWorker, TextMeasurementWorker } from '@lvce-editor/rpc-registry'
 
 const send = async (port: MessagePort): Promise<void> => {
-  await RendererWorker.sendMessagePortToIconThemeWorker(port, 0)
+  await RendererWorker.sendMessagePortToTextMeasurementWorker(port, 0)
 }
 
 export const initializeTextMeasurementWorker = async (): Promise<void> => {
