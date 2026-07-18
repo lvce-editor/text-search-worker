d2c1a26a4806f92aebdcba78c002f34808d86223
Le Vivilet <levivilet@pm.me>
2026-07-18 09:02:04 +0000

    feature: open new search editors

--- packages/text-search-worker/src/parts/OpenSearchEditor/OpenSearchEditor.ts
+++ packages/text-search-worker/src/parts/OpenSearchEditor/OpenSearchEditor.ts
@@ -1,8 +1,10 @@
 import type { SearchState } from '../SearchState/SearchState.ts'
+import { openUri } from '../OpenUri/OpenUri.ts'
+
+let searchEditorId = 0
 
 export const openSearchEditor = async (state: SearchState): Promise<SearchState> => {
-  // TODO
-  return {
-    ...state,
-  }
+  const uri = `search-editor://${state.uid}-${++searchEditorId}/Search`
+  await openUri(uri)
+  return state
 }
