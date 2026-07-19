2aa819b6fd8ab9ed1ee06edee15ef686e7522b72
Le Vivilet <levivilet@pm.me>
2026-07-19 13:46:28 +0000

    fix: open search results at match range

--- packages/text-search-worker/src/parts/SelectIndexPreview/SelectIndexPreview.ts
+++ packages/text-search-worker/src/parts/SelectIndexPreview/SelectIndexPreview.ts
@@ -1,22 +1,23 @@
 import { WhenExpression } from '@lvce-editor/virtual-dom-worker'
+import type { SearchResult } from '../SearchResult/SearchResult.ts'
 import type { SearchState } from '../SearchState/SearchState.ts'
 import * as GetFileIndex from '../GetFileIndex/GetFileIndex.ts'
 import * as InputSource from '../InputSource/InputSource.ts'
 import * as OpenUri from '../OpenUri/OpenUri.ts'
 import * as Workspace from '../Workspace/Workspace.ts'
 
-export const selectIndexPreview = async (state: SearchState, searchResult: any, index: number): Promise<SearchState> => {
+export const selectIndexPreview = async (state: SearchState, searchResult: SearchResult, index: number): Promise<SearchState> => {
   const { items, workspacePath } = state
   const fileIndex = GetFileIndex.getFileIndex(items, index)
   if (fileIndex === -1) {
     throw new Error('Search result is missing file')
   }
-  const { lineNumber } = searchResult
+  const { end, endColumnIndex = end, lineNumber, rowIndex = lineNumber, start, startColumnIndex = start } = searchResult
   const fileResult = items[fileIndex]
   const relativePath = Workspace.getRelativePath(fileResult.text)
   const absolutePath = `${workspacePath}${relativePath}`
   await OpenUri.openUri(absolutePath, true, {
-    selections: new Uint32Array([lineNumber, 0, lineNumber, 0]),
+    selections: new Uint32Array([rowIndex, startColumnIndex, rowIndex, endColumnIndex]),
   })
   return {
     ...state,
