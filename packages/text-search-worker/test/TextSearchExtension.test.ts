d1aef4fccf13a2eee3600f29933164f7519caec2
levivilet <72156503+levivilet@users.noreply.github.com>
2026-09-08 21:59:47 +0000

    feature: support extension-owned remote workspace services

--- packages/text-search-worker/test/TextSearchExtension.test.ts
+++ packages/text-search-worker/test/TextSearchExtension.test.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@jest/globals'
 import { RendererWorker } from '@lvce-editor/rpc-registry'
+import { getRipGrepArgs } from '../src/parts/GetTextSearchRipGrepArgs/GetTextSearchRipGrepArgs.ts'
 import * as TextSearchExtension from '../src/parts/TextSearchExtension/TextSearchExtension.ts'
 
 test('textSearch - extension search', async () => {
@@ -44,7 +45,15 @@ test('textSearch - extension search', async () => {
 
   const result = await TextSearchExtension.textSearch('xyz', 'xyz://', 'abc')
   expect(result).toEqual(mockResults)
-  expect(mockRpc.invocations).toEqual([['ExtensionHostTextSearch.executeTextSearchProvider', 'xyz', 'abc']])
+  expect(mockRpc.invocations).toEqual([
+    [
+      'ExtensionHostTextSearch.executeTextSearchProvider',
+      'xyz',
+      'abc',
+      'xyz://',
+      getRipGrepArgs({ isCaseSensitive: false, searchString: 'abc', threads: 1, useRegularExpression: false }),
+    ],
+  ])
 })
 
 test('textSearch - extension search error', async () => {
@@ -55,5 +64,29 @@ test('textSearch - extension search error', async () => {
   })
 
   await expect(TextSearchExtension.textSearch('xyz', 'xyz://', 'abc')).rejects.toThrow(new TypeError('x is not a function'))
-  expect(mockRpc.invocations).toEqual([['ExtensionHostTextSearch.executeTextSearchProvider', 'xyz', 'abc']])
+  expect(mockRpc.invocations).toEqual([
+    [
+      'ExtensionHostTextSearch.executeTextSearchProvider',
+      'xyz',
+      'abc',
+      'xyz://',
+      getRipGrepArgs({ isCaseSensitive: false, searchString: 'abc', threads: 1, useRegularExpression: false }),
+    ],
+  ])
+})
+
+test('preserves remote search roots, options, and result limits', async () => {
+  const result = { limitHit: true, results: [] }
+  using mockRpc = RendererWorker.registerMockRpc({ 'ExtensionHostTextSearch.executeTextSearchProvider': () => result })
+  const options = { exclude: 'node_modules', include: '*.ts', isCaseSensitive: true, threads: 1, useRegularExpression: true }
+  await expect(TextSearchExtension.textSearch('remote-ssh', 'remote-ssh://host/work', 'query', options)).resolves.toEqual(result)
+  expect(mockRpc.invocations).toEqual([
+    [
+      'ExtensionHostTextSearch.executeTextSearchProvider',
+      'remote-ssh',
+      'query',
+      'remote-ssh://host/work',
+      getRipGrepArgs({ ...options, searchString: 'query' }),
+    ],
+  ])
 })
