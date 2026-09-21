ee6a217df237ddc5afcd5770483f1b8b412cb103
Le Vivilet <levivilet@pm.me>
2026-09-20 19:46:39 -0700

    feature: add dev script (#1497)

---
+++ packages/build/src/dev.ts
@@ -0,0 +1,35 @@
+import { execa } from 'execa'
+import { root } from './root.ts'
+
+const watchArgs = [
+  'exec',
+  '--workspace=packages/build',
+  '--',
+  'esbuild',
+  '--format=esm',
+  '--bundle',
+  '--external:node:buffer',
+  '--external:electron',
+  '--external:ws',
+  '--external:node:worker_threads',
+  '--watch',
+  '../text-search-worker/src/textSearchWorkerMain.ts',
+  '--outfile=../../.tmp/dist/dist/textSearchWorkerMain.js',
+]
+
+const main = async (): Promise<void> => {
+  await execa('npm', ['run', 'build'], {
+    cwd: root,
+    stdio: 'inherit',
+  })
+  execa('npm', watchArgs, {
+    cwd: root,
+    stdio: 'inherit',
+  })
+  execa('node', ['node_modules/@lvce-editor/server/bin/server.js', '--link=.tmp/dist'], {
+    cwd: root,
+    stdio: 'inherit',
+  })
+}
+
+main()
