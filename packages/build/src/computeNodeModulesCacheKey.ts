d031388f39f4b9aa5315f8773921c0348752222f
Le Vivilet <levivilet@pm.me>
2026-09-23 17:37:56 -0700

    feature: use linked worker server launcher (#1498)

--- packages/build/src/computeNodeModulesCacheKey.ts
+++ packages/build/src/computeNodeModulesCacheKey.ts
@@ -10,7 +10,6 @@ const locations = [
   '.github/workflows/ci.yml',
   '.github/workflows/release.yml',
   'packages/build/src/computeNodeModulesCacheKey.ts',
-  'packages/server/src/postinstall.js',
 ]
 
 const getAbsolutePath = (relativePath) => {
